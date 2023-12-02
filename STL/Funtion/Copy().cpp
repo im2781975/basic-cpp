bc9ba96b0a1fd1828951c853f51729add7524b8e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-02 19:32:31 +0100

    Update and rename Copy<(),if,n,backward> to Copy().cpp

--- STL/Funtion/Copy().cpp
+++ STL/Funtion/Copy().cpp
@@ -1,4 +1,4 @@
-// working of copy_n() 
+//copy() | copy_n() | copy_if() | copy_backward() 
 #include<iostream> 
 #include<algorithm> 
 #include<vector> 
@@ -13,26 +13,26 @@ int main()
      
    //using copy() to copy 1st 3 elements 
    copy(v1.begin(), v1.begin()+3, v2.begin()); 
-   cout << "After using copy v2[] is : "; 
+   cout << "After using copy array is : "; 
    for(int i=0; i<v2.size(); i++) 
        cout << v2[i] << " ";
      
    // using copy_n() to copy 1st 4 elements 
    copy_n(v1.begin(), 4, v3.begin()); 
-   cout << "\nAfter using copy_n v3[] is : "; 
+   cout << "\nAfter using copy_n array is : "; 
    for(int i=0; i<v3.size(); i++) 
    cout << v3[i] << " "; 
 
 
    copy_if(v1.begin(), v1.end(), v2.begin(), [](int i){return i%2!=0;}); 
-    cout << "\nAfter using copy_if v2[] is: ";
+    cout << "\nAfter using copy_if array is: ";
     for(int i=0; i<v2.size(); i++) 
         cout << v2[i] << " "; 
           
-    //using copy_backward() to copy 1st 4 elements ending at second last position 
+    //using copy_backward() to copy 1st 4 elements ending at second last position. always copied element position is lessthen or equal the position where you copied
+
     copy_backward(v1.begin(), v1.begin() + 4, v3.begin()+5);
-    cout << "\nAfter using copy_backward() v3[] is: ";
+    cout << "\nAfter using copy_backward() array is: ";
     for(int i=0; i<v3.size(); i++) 
     cout << v3[i] << " "; 
-
 } 
