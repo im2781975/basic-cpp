4832dfeeb95f79479518003ba860f8b9b4c3bfec
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-03 18:28:55 +0100

    Update Partition().cpp

--- STL/Funtion/Partition().cpp
+++ STL/Funtion/Partition().cpp
@@ -3,6 +3,9 @@
 #include<algorithm> 
 #include<vector> 
 using namespace std;
+bool odd(int i)
+  { return (i % 2) == 1; } 
+  
 void print(vector<int>&vect, int n)
 {
     for(int i=0; i<vect.size(); i++)
@@ -18,29 +21,24 @@ int main()
     vector<int>vect2;
     
     is_partitioned(vect.begin(), vect.end(),[](int x)
-    {return x%2==0;})?
+    { return x%2==0; })?
     cout <<"\nVector is partitioned":
     cout<<"\nVector is not partitioned";
      
     partition(vect.begin(), vect.end(), [](int x)
-    {return x%2==0;});
+    { return x%2==0; });
 
     cout << "\npartitioned vector is : ";
     print(vect,vect.size());
     
     int n = count_if (vect.begin(), vect.end(), [](int x)
-    {
-        return x%2==0;
-         
-    } );
+    { return x%2==0; } );
     vect1.resize(n); 
     vect2.resize(vect.size()-n);
      
     partition_copy(vect.begin(), vect.end(), vect1.begin(), 
     vect2.begin(), [](int x)
-    {
-        return x%2==0;
-    });
+    { return x%2==0; });
      
     cout << "\nThe elements that return true for condition are : ";
     print(vect1,vect1.size());
@@ -49,24 +47,29 @@ int main()
     print(vect2,vect2.size());
     
     stable_partition(vect.begin(), vect.end(), [](int x)
-    {
-        return x%2 == 0;        
-    });
+    { return x%2 == 0; });
      
     cout << "\nAfter stable_partiton vect is : ";
     print(vect,vect.size());
 
     vector<int>::iterator it;
-     
-    // using partition_point() to get ending position of partition
+    
     auto m= partition_point(vect.begin(), vect.end(), [](int x)
-    {
-        return x%2==0;
-    });
+    { return x%2==0; });
 
     cout << "\nAfter partition_point vect is: ";
     for ( it= vect.begin(); it!=m; it++)
     cout <<*it<<" ";
     
+    vector<int>::iterator pt;
+    pt = stable_partition(vect.begin(), vect.end(), odd);
+    
+    cout << "\nodd numbers:"; 
+    for (it = vect.begin(); it != pt; ++it) 
+        cout << ' ' << *it; 
+  
+    cout << "\nEven numbers:"; 
+    for (it = pt; it != vect.end(); ++it) 
+        cout << ' ' << *it; 
     return 0;
 }
