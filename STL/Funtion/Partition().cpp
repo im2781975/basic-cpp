71bba752cb10273e035ff304f54ab89880e7ba66
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-03 17:47:47 +0100

    Update and rename Partition<copy> to Partition().cpp

---
+++ STL/Funtion/Partition().cpp
@@ -0,0 +1,51 @@
+// partition() | is_partitioned() | partition_copy()
+#include<iostream>
+#include<algorithm> 
+#include<vector> 
+using namespace std;
+void print(vector<int>&vect, int n)
+{
+    for(int i=0; i<vect.size(); i++)
+    {
+        cout<<vect[i]<<" ";
+    }
+}
+int main()
+{
+    vector<int> vect{2,1,5,6,8,7 };
+    
+    vector<int>vect1;
+    vector<int>vect2;
+    
+    is_partitioned(vect.begin(), vect.end(),[](int x)
+    {return x%2==0;})?
+    cout <<"\nVector is partitioned":
+    cout<<"\nVector is not partitioned";
+     
+    partition(vect.begin(), vect.end(), [](int x)
+    {return x%2==0;});
+
+    cout << "\npartitioned vector is : ";
+    print(vect,vect.size());
+    
+    int n = count_if (vect.begin(), vect.end(), [](int x)
+    {
+        return x%2==0;
+         
+    } );
+    vect1.resize(n); 
+    vect2.resize(vect.size()-n);
+     
+    partition_copy(vect.begin(), vect.end(), vect1.begin(), 
+    vect2.begin(), [](int x)
+    {
+        return x%2==0;
+    });
+     
+    cout << "\nThe elements that return true for condition are : ";
+    print(vect1,vect1.size());
+
+    cout << "\nThe elements that return false for condition are : ";
+    print(vect2,vect2.size());
+    return 0;
+}
