86cb4214c7b3f497fc19864e869dd5ba2b423721
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-03 09:04:03 +0100

    Update and rename Move< (),backward> to Move().cpp

---
+++ STL/Funtion/Move().cpp
@@ -0,0 +1,35 @@
+
+//Move() | Move_backward()
+#include<bits/stdc++.h> 
+using namespace std;
+void print(vector<int>&vec,int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout<<vec[i]<<" ";
+    }
+}
+int main() 
+{ 
+    vector <int> vec1{ 1,2,3,4,5 };
+    vector <int> vec2 {7,7,7,7,7}; 
+  
+    cout <<"Elements of vect1 :";
+    print(vec1, vec1.size());
+      
+    cout<<"\nElements of vect2 :";
+    print(vec2, vec2.size());
+      
+    move (vec1.begin(), vec1.begin() + 4, vec2.begin() + 1); 
+      
+    cout << "\nVect2 contains after move() :"; 
+    print(vec2, vec2.size());
+    
+    move_backward (vec2.begin(), vec2.begin() + 3, vec1.begin() + 3); 
+  
+    cout<<"\nVect1 contains after move_backward () : "; 
+    print(vec1, vec1.size());
+    
+    cout<<"\nVect2 contains after move_backward () : "; 
+    print(vec2, vec2.size());
+} 
