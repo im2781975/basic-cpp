8be4970d23a3d60f9bf992582483de0eda0a9713
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-03 09:12:33 +0100

    Update Nth val.cpp

--- STL/Funtion/Nth val.cpp
+++ STL/Funtion/Nth val.cpp
@@ -6,35 +6,34 @@ bool comp(int a, int b)
 { 
     return (a < b); 
 } 
+void print(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+}
 int main() 
 { 
-    int v[]{ 3, 2, 10, 45, 33, 56, 23, 47 }, i; 
+    int v[]{ 3, 2, 10, 45, 33, 56, 23, 47 },i;
     int n=sizeof(v) /sizeof( v[0]);
     
     cout<<"Before sort: ";
-    for (i = 0; i < 8; ++i) 
-        cout << v[i] << " "; 
+    print(v, n);
 
     nth_element(v, v + 4, v + n); 
     // Since, n is 5 so 5th element should be sorted 
     cout<<"\nAfter sort v[5]: ";
-    for (i = 0; i < 8; ++i) 
-        cout << v[i] << " "; 
+    print(v, n);
     
     nth_element(v, v + 5, v + n, comp); 
     cout<<"\nAfter sort v[6]: ";
-    for (i = 0; i < 8; ++i) 
-        cout << v[i] << " "; 
+    print(v, n);
     
     nth_element(v, v + 2, v + n); 
     cout<<"\nAfter sort v[3]: ";
-    for (i = 0; i < 3; ++i)  
-        cout << v[i] << " "; 
+    print(v, 3);
     
     cout<<"\n2nd largest element is : ";
     nth_element(v, v + 1, v + 8, greater<int>()); 
-    for (i = 0; i < 3; ++i)  
-        cout << v[i] << " "; 
-        
+    print(v, 3);
     return 0; 
 } 
