f3aec5a230b82d6fecabf9636526a7de5696e807
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-03 17:25:47 +0100

    Update Count().cpp

--- STL/Funtion/Count().cpp
+++ STL/Funtion/Count().cpp
@@ -8,16 +8,18 @@ bool isEven(int i)
     else
         return false;
 }
-
 int main()
 {
     vector<int> v;
+    vector<int> vec;
     for (int i = 0; i < 10; i++) {
         v.push_back(i);
     }
     int noEven = count_if(v.begin(), v.end(),isEven);
     cout << "After using count_if Even count is: "<< noEven;
     
+    int n=count_if(v.begin(),v.end(),[](int i){return i%2!=0;});
+    cout << "\nAfter using count_if odd count is: "<< n;
     
     string str = "geeksforgeeks";
     cout << "\nAfter using count() 'e' count is : " << count(str.begin(), str.end(), 'e');
