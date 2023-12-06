37c25c94905e973865e0bcc62f7e39a183393c19
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-06 19:13:19 +0100

    Update and rename Priority_heap to Priority heap.cpp

--- STL/Heap/Priority heap.cpp
+++ STL/Heap/Priority heap.cpp
@@ -1,5 +1,4 @@
-// application of make_heap() (max_heap) 
-// priority scheduling 
+// application of make_heap() (max_heap) priority scheduling 
 #include<iostream> 
 #include<algorithm>
 #include<vector> 
@@ -8,20 +7,20 @@ using namespace std;
 int main() 
 { 
     vector<int>vi{15,6,7,9,11,19}; 
-
-    int k = 3; 
-
     make_heap(vi.begin(),vi.end()); 
     int a = 10; 
-      
+    int k = 3; 
+    // k represents the number of additional jobs to be scheduled, and a is the priority of the first additional job.
     for ( int i=0; i<k; i++) 
     { 
         vi.push_back(a); 
-      
     // transform into heap ( using push_heap() ) 
     push_heap(vi.begin(), vi.end()); 
-      
-    cout <<"\nJob with maximum priority is :"<<vi.front();
+    for(int i=0; i<vi.size(); i++)
+    {
+        cout<<vi[i]<<" ";
+    }
+    cout <<"\nJob with maximum priority is : "<<vi.front();
     a = a + 10; 
     } 
 } 
