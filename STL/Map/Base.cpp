ace9ae83b6255ab268d0aa81f20b166239c2fcc1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-07 19:58:39 +0100

    Update and rename Pair <string, int> to Base.cpp

---
+++ STL/Map/Base.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<string, int> mp;
+    map<string, int>::iterator it;
+    mp["A"] = 1;
+    mp["B"] = 2;
+
+    cout << "\nElements of string A & B are: " << mp["A"] << " " << mp["B"];
+
+    mp["B"] = 3;
+    cout << "\nElements of string A & B after change are: " << mp["A"] << " " << mp["B"];
+
+    cout << "\nmap[string, int] elements are: ";
+    for (it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << "\nKey: "<< it->first << " " << "Value: "<< it->second ;
+    }
+
+    cout << "\n";
+    map<int, int> mpp;
+    map<int, int>::iterator pt;
+    mpp[23] = 4;
+    mpp[32] = 5;
+
+    cout << "\nmap[int, int] elements are: ";
+    for (pt = mpp.begin(); pt != mpp.end(); pt++)
+    {
+        cout << "\nKey: "<< pt->first << " " << "value: "<< pt->second;
+    }
+    return 0;
+}
