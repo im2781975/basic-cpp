6c52847bc547622480e6bc248fbaf8c414ee7594
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-08 20:02:41 +0100

    Update Binary rep.cpp

--- STL/Queue/Binary rep.cpp
+++ STL/Queue/Binary rep.cpp
@@ -5,20 +5,24 @@ int main()
 {
     int n;
     cin >> n;
+
+    // Imposed deque binary representation
     deque<string> dq;
     dq.push_back("1");
-    cout<<"Imposed dequeue binary rep: ";
+    cout << "Imposed deque binary rep: ";
     for (int i = 1; i <= n; i++)
     {
-        cout << "\nBinary representation of " << i << ": " <<dq.front();
+        cout << "\nBinary representation of " << i << ": " << dq.front();
         string tmp = dq.front();
         dq.pop_front();
         dq.push_back(tmp + "0");
         dq.push_back(tmp + "1");
     }
+
+    // Based queue binary representation
     queue<string> q;
     q.push("1");
-    cout<<"\nBased queue binary rep: ";
+    cout << "\nBased queue binary rep: ";
     for (int i = 1; i <= n; i++)
     {
         cout << "\nBinary representation of " << i << ": " << q.front();
@@ -27,5 +31,6 @@ int main()
         q.push(tmp + "0");
         q.push(tmp + "1");
     }
+
     return 0;
 }
