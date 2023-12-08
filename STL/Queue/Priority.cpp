3624260c9ca7e132cd5df88ba4b31e8d675de3ab
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-08 20:27:29 +0100

    Update and rename Priority<basic> to Priority.cpp

--- STL/Queue/Priority.cpp
+++ STL/Queue/Priority.cpp
@@ -14,7 +14,11 @@ int main()
 {
     priority_queue<int> pq1;
     priority_queue<int> pq2;
-
+    int arr[6]={10,11,12,13,14,15};
+    for(int i=0; i<6; i++)
+    {
+        pq1.push(arr[i]);
+    }
     pq1.push(1);
     pq1.push(2);
     pq1.push(3);
@@ -24,22 +28,28 @@ int main()
     pq2.push(5);
     pq2.push(7);
     pq2.push(9);
-
     cout << "Before swap() pq1: ";
     print(pq1);
-
-    cout << "\nBefore swap() pq2: ";
+    cout <<"\nBefore swap() pq2: ";
     print(pq2); 
 
     cout << "\npq1.size(): " << pq1.size();
-
     cout << "\npq1.top(): " << pq1.top();
-
     cout << "\npq1.pop(): ";
     pq1.pop();
     print(pq1);
-
-    swap(pq1, pq2);
+    
+    pq2 = priority_queue<int>(); 
+    
+    // Clear the priority queue by 
+    pq2.emplace(15);
+    pq2.emplace(16);
+    pq2.emplace(17);
+    pq2.emplace(18);
+    pq2.emplace(19);
+    pq2.emplace(20);
+    print(pq2);
+    swap(pq1,pq2);
     cout << "\nAfter swap() pq1: ";
     print(pq1);
 
