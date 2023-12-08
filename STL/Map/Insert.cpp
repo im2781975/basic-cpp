c3f658bf734f528b8a18c858d33d0f7999e212c8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-08 06:20:45 +0100

    Update Insert.cpp

--- STL/Map/Insert.cpp
+++ STL/Map/Insert.cpp
@@ -1,17 +1,49 @@
+//insert() | erase() | search() 
 #include<bits/stdc++.h>
-#include<map>
 using namespace std;
+void print(unordered_multimap<string,int>&ump)
+{
+    unordered_multimap<string,int>::iterator it;
+    it=ump.begin();
+    while(it!=ump.end())
+    {
+        cout<<"\nKey: "<<it->first<<" "<<"Value: "<<it->second;
+        it++;
+    }
+}
 int main()
 {
-    map<int,string>mp;
- 
-    mp.insert(pair<int,string>(1,"A"));
-    mp.insert(pair<int,string>(2,"B"));
-    mp.insert(pair<int,string>(3,"C"));
- 
-    map<int,string>::iterator it;
-    for(it=mp.begin(); it!=mp.end(); it++)
+    //unordered_multimap<string,int>ump{{"A",1},{"B",2},{"C",3},{"D",2},{"E",5},{"F",6},{"D",7}};
+    
+    unordered_multimap<string,int>ump;
+    ump.insert(pair<string,int>("A",1));
+    ump.insert(pair<string,int>("B",2));
+    ump.insert(pair<string,int>("C",3));
+    ump.insert(pair<string,int>("D",2));
+    ump.insert(pair<string,int>("E",5));
+    ump.insert(pair<string,int>("F",6));
+    ump.insert(pair<string,int>("D",7));
+    unordered_multimap<string,int>::iterator it;
+    cout<<"\nmultimap elements are: ";
+    print(ump);
+    // Search for an element with value 1
+    int n=1;
+    it=ump.begin();
+    cout<<"\nImposed search for value 1: ";
+    while(it!=ump.end())
     {
-        cout<<"Key: "<< it->first<<" "<<"Value: "<< it->second<<"\n";
+        if(it->second==n)
+        {
+            break;
+        }
+        it++;
     }
+    cout<<it->first;
+    //Erase the element pointed by iterator it
+    if(it!=ump.end())
+    {
+        ump.erase(it);
+    }
+    cout<<"\nImposed erase: ";
+    print(ump);
 }
