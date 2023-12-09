722250aed52a184d2db3dc31517b9adc2a4750fe
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-09 18:14:16 +0100

    Update Multiset.cpp

--- STL/Set/Multiset.cpp
+++ STL/Set/Multiset.cpp
@@ -13,13 +13,24 @@ void print(multiset<int> s)
 int main()
 {
     multiset<int> s;
+    multiset<int> ms({15,13,11,9,7,5,3,1});
     multiset<int>::iterator it;
 
     for (int i = 0; i <= 15; i++)
     {
         s.insert(i);
     }
-
+    ms.insert(0);
+    s.insert(15);
+    if(!ms.empty())
+        cout<<"\nExits element";
+    else
+        cout<<"\ndoesn't exits element";
+    if(s.count(5)!=s.end())
+        cout<<"\n"
+    cout<<"\nSize of ms is: "<<ms.size();
+    cout<<"\nCount 15 is: "<<s.count(15);
+    
     cout << "\nMultiset is: ";
     print(s);
     cout << "\nSize of multiset is: " << s.size();
@@ -41,4 +52,3 @@ int main()
 
     return 0;
 }
-
