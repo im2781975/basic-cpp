6d850498c29088ceb28121e83ed5c16240fca389
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-09 22:01:21 +0100

    Update and rename Union to Union.cpp

--- STL/Set/Union.cpp
+++ STL/Set/Union.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 #include <set>
-
+using namespace std;
 int main() {
-    std::set<int> set1, set2, unionSet;
-
-    // Insert elements into the first set
+    set<int> set1, set2, unionSet;
+    
     set1.insert(1);
     set1.insert(2);
     set1.insert(3);
 
-    // Insert elements into the second set
     set2.insert(2);
     set2.insert(3);
     set2.insert(4);
-
     // Find the union of the two sets
     for (int element : set1) {
         unionSet.insert(element);
     }
     for (int element : set2) {
         unionSet.insert(element);
-    }
-
-    // Print the elements in the union set
+    } 
+    
     for (int element : unionSet) {
-        std::cout << element << " ";
+        cout << element << " ";
     }
-
     return 0;
 }
-
