31cb59ed4d12aeb748ff7c3f891a7c28140b0eb4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-11 17:24:04 +0100

    Update Search 1.cpp

--- STL/Search 1.cpp
+++ STL/Search 1.cpp
@@ -1,24 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
-void print(int arr[],int n)
+
+void print(int arr[], int n)
 {
     for(int i=0; i<n; i++)
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
 }
+
+int binarySearch(int arr[], int low, int high, int x)
+{
+    int mid = low + (high - low) / 2;
+    while(low <= high)
+    {
+        if(arr[mid] == x)
+        {
+            return mid;
+            break;
+        }
+        else if(arr[mid] > x)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     int n;
-    cin>>n;
-    int arr[n+2];
+    cin >> n;
+    int arr[n + 2];
     for(int i=0; i<n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    sort(arr,arr+n);
-    cout<<"\nAfter sort array is: ";
-    print(arr,n);
-    if(binary_search(arr,arr+n,2))
-        cout<<"\nElements exits";
+    sort(arr, arr + n);
+    cout << "\nAfter sort array is: ";
+    print(arr, n);
+
+    int x;
+    cout<<"\nEnter elements for search: ";
+    cin >> x;
+    int result = binarySearch(arr, 0, n - 1, x);
+    
+    (result == -1)?
+        cout << "\nElement doesn't present in the array":
+        cout << "\nElement present at index: " << result;
+
+    if(binary_search(arr, arr + n, 2))
+        cout << "\nElements 2 exists";
     else
-        cout<<"\nElements doesn't exits";
+        cout << "\nElements 2 doesn't exist";
 }
