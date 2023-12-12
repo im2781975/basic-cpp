adc50062d44e6b583157738a63fa2e0c8df36ec0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-12 10:35:34 +0100

    Update and rename Sort<array> to Counting sort.cpp

--- Tabulation/Counting sort.cpp
+++ Tabulation/Counting sort.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 void countingSort(vector<int>& arr, int n) {
     // Create a counting array to track the frequency of each number
     vector<int> count(n, 0);
-
     // Count the frequency of each number in the input array
     for (int i = 0; i < n; i++) {
         count[arr[i]]++;
     }
-
     // Output the sorted numbers based on the counting array
     int index = 0;
     for (int i = 0; i < n; i++) {
@@ -22,24 +19,17 @@ void countingSort(vector<int>& arr, int n) {
         }
     }
 }
-
 int main() {
     int n;
     cin >> n;
-
     vector<int> numbers(n);
 
     for (int i = 0; i < n; i++) {
         cin >> numbers[i];
     }
-
     countingSort(numbers, n);
-
     for (int i = 0; i < n; i++) {
         cout << numbers[i] << " ";
     }
-
-    cout << endl;
-
     return 0;
 }
