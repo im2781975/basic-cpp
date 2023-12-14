ec7e1c24fd091d6c94e31ca9407781092b88d4bd
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-14 14:58:44 +0100

    Update Search().cpp

--- STL/Funtion/Search().cpp
+++ STL/Funtion/Search().cpp
@@ -30,5 +30,26 @@ int main()
         cout << "v2 is not present consecutively 2 times "
              << "in vector v1"; 
     } 
+    
+    vector<int> v3 = {3, 4}; 
+   
+   // vector<int>::iterator i1; 
+   
+    i1 =search(v1.begin(), v1.end(), v3.begin(), v3.end()); 
+   
+    if (i1 != v1.end()) { 
+        cout << "\nvector3 is present firstly at index " 
+             << (i1 - v1.begin()) ;
+    } else { 
+        cout << "\nvector3 is not present in vector1 ";
+    } 
+  
+    i1 =find_end(v1.begin(), v1.end(), v3.begin(), v3.end()); 
+   
+    if (i1 != v1.end()) { 
+        cout << "\nvector3 is present lastly at index "<< (i1 - v1.begin()) ;
+    } else { 
+        cout << "\nvector3 is not present in vector1"; 
+    } 
     return 0; 
 } 
