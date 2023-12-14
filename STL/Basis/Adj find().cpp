d481277314d5ccfd7b8edfc0752640be40a32070
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-14 22:59:51 +0100

    Update Adj find().cpp

--- STL/Basis/Adj find().cpp
+++ STL/Basis/Adj find().cpp
@@ -14,25 +14,17 @@ int main()
  
     // Check if the elements in the range [vec.begin(), vec.begin() + range1) are sorted in descending order.
     it = adjacent_find(vec.begin(),vec.begin() + range1,greater<int>());
-    if (it == vec.begin() + range1) 
-    {
-          cout <<"\nSorted in the range :"<<range1 ;
-    }
+    if (it == vec.begin() + range1)
+        cout <<"\nSorted in the range :"<<range1 ;
     else
-    {
-    cout << "\nNot sorted in the range : " << range1 ;
-    }
+        cout << "\nNot sorted in the range : " << range1 ;
     // Check if the elements in the range [vec.begin(), vec.begin() + range2) are sorted in descending order.
     it =adjacent_find(vec.begin(),vec.begin() + range2, greater<int>());
  
     if (it == vec.begin() + range2) 
-    {
         cout<< "\nSorted in the range : " << range2 ;
-    }
     else
-    {
         cout<<"\nNot sorted in the range : "<<range2 ;
-    }
     int A[]{ 10, 13, 16, 16, 18 };
     int n =sizeof(A) /sizeof(A[0]);
  
