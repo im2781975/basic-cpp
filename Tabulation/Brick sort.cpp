2b54e904110dfb9d9ea552a22d181cb96d64705f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-14 06:57:10 +0100

    Create Brick sort.cpp

---
+++ Tabulation/Brick sort.cpp
@@ -0,0 +1,45 @@
+//implement Odd-Even / Brick Sort
+#include<bits/stdc++.h>
+using namespace std;
+void oddEvenSort(int arr[], int n)
+{
+    bool isSorted = false;
+    while (!isSorted)
+    {
+        isSorted = true;
+        // Perform Bubble sort on odd indexed element
+        for (int i=1; i<=n-2; i=i+2)
+        {
+            if (arr[i] > arr[i+1])
+             {
+                swap(arr[i], arr[i+1]);
+                isSorted = false;
+              }
+        }
+        // Perform Bubble sort on even indexed element
+        for (int i=0; i<=n-2; i=i+2)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                swap(arr[i], arr[i+1]);
+                isSorted = false;
+            }
+        }
+    }
+    return;
+}
+void printArray(int arr[], int n)
+{
+   for (int i=0; i < n; i++)
+       cout << arr[i] << " ";
+   cout << "\n";
+}
+int main()
+{
+    int arr[] = {34, 2, 10, -9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    oddEvenSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
