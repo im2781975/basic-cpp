f66eeedb9dca24f585d22e42bdc556e51d8271ac
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-14 08:54:34 +0100

    Update Cycle.cpp

--- Tabulation/Cycle.cpp
+++ Tabulation/Cycle.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
-
+void cyclicSort(int arr[], int n){
+    int i = 0; 
+    while(i < n)
+    {
+    int correct = arr[i] - 1 ;
+    if(arr[i] != arr[correct]){
+      // if array element should be lesser than size and array element should not be at
+      // its correct position then only swap with its correct position or index value
+      swap(arr[i], arr[correct]) ;
+    }else{
+      // if element is at its correct position just increment i and check for remaining array elements
+      i++ ;
+    }
+  }
+}
 void cycleSort(int arr[], int n)
 {
     // count number of memory writes
@@ -50,7 +64,6 @@ void cycleSort(int arr[], int n)
             }
         }
     }
-    // Number of memory writes or swaps
     // cout << writes << endl ;
 }
 int main()
@@ -58,7 +71,7 @@ int main()
     int arr[] = { 1, 8, 3, 9, 10, 10, 2, 4 };
     int n = sizeof(arr) / sizeof(arr[0]);
     cycleSort(arr, n);
- 
+    cyclicSort(arr, n);
     cout << "\nAfter sort : ";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
