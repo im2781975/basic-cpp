a60501af3a9c3c94acfb8e014fd666c46c50d0db
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-14 07:16:49 +0100

    Update Bubble.cpp

--- Tabulation/Bubble.cpp
+++ Tabulation/Bubble.cpp
@@ -1,6 +1,36 @@
 // Bubble sort
 #include<bits/stdc++.h>
 using namespace std;
+void Bubble_sort(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[i] > arr[j])
+                swap(arr[i], arr[j]);
+        }
+    }
+}
+void BubbleSort(int arr[], int n)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+ 
+        // If no two elements were swapped
+        // by inner loop, then break
+        if (swapped == false)
+            break;
+    }
+}
 void print(int arr[], int n)
 {
     for(int i=0; i<n; i++)
@@ -12,14 +42,11 @@ int main()
 {
     int arr[] = {7, 2, 13, 2, 11, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
-
-    for(int i = 0; i < n - 1; i++)
-    {
-        for(int j = i + 1; j < n; j++)
-        {
-            if(arr[i] > arr[j])
-                swap(arr[i], arr[j]);
-        }
-    }
+    cout<<"\nImposed Bubble sort optimized: ";
+    BubbleSort(arr,n);
+    print(arr,n);
+    
+    cout<<"\nImposed Bubble sort: ";
+    Bubble_sort(arr, n);
     print(arr, n);
 }
