7f08a944d9e5ca039766d1c4eb63996865f919b7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-15 09:44:23 +0100

    Update Base ()2.cpp

--- STL/Funtion/Base ()2.cpp
+++ STL/Funtion/Base ()2.cpp
@@ -1,62 +1,58 @@
-//constructive() | back_inserter() | iterative()
-#include<bits/stdc++.h>  
-using namespace std;  
-  
-int main()  
-{  
-    vector<int> vect1{1, 2, 3, 4};  
-    //constructor method, Deep copy
-    vector<int> vect2(vect1);  
-  
-    cout << "\nElement of Vect1[] is: ";  
-    for (int i=0; i<vect1.size(); i++)  
-        cout << vect1[i] << " ";  
-  
-    cout << "\nAfter copy(constructive) vect2[] is : ";  
-    for (int i=0; i<vect2.size(); i++)  
-        cout << vect2[i] << " ";  
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-    vect1[0] = 2;  
-  
-    cout << "\nFirst element of vect1[] is:" <<vect1[0];
-    
-    cout << "\nFirst element of vect2[] is : "<<vect2[0];
-    
-    vector<int> vect3;  
-  
-    copy(vect1.begin(), vect1.end(), back_inserter(vect3));  
-    cout <<"\nElements of vect1[] are: ";  
-    for (int i=0; i<vect1.size(); i++)  
-        cout << vect1[i] << " ";  
-  
+using namespace std;
+
+void print(vector<int> vec) {
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+}
+
+int main() {
+    vector<int> vect1{1, 2, 3, 4};
+    vector<int> vect2(vect1);
+    vector<int> vect3;
+    vector<int> cev(vect1.size() + vect2.size());  // Resize to accommodate merged elements
+    cout << "\nElement of Vect1[] is: ";
+    print(vect1);
+
+    cout << "\nAfter copy(constructive) vect2[] is : ";
+    print(vect2);
+
+    vect1[0] = 2;
+
+    cout << "\nFirst element of vect1[] is:" << vect1[0] << " " << vect1.front();
+
+    copy(vect1.begin(), vect1.end(), back_inserter(vect3));
+    cout << "\nElements of vect1[] are: ";
+    print(vect1);
+
     cout << "\nAfter copy vect3[] are : ";
-    for (int i=0; i<vect3.size(); i++)  
-        cout << vect3[i] << " ";  
-  
-    vect1[0] = 2;  
-  
-    cout << "\nFirst element of vect1[] is:" <<vect1[0];
-    
-    cout << "\nFirst element of vect3[] is:" <<vect3[0];
-    
-    //copy<iterative>
-    vector<int> vect4;  
-  
-    vect4 = vect1;  
-    
-    cout <<"\nElements of vect1[] are: ";  
-    for (int i=0; i<vect1.size(); i++)  
-        cout << vect1[i] << " ";  
+    print(vect3);
 
-    cout <<"\nAfter copy(iterative) elements of vect4[] are: ";  
-    for (int i=0; i<vect4.size(); i++)  
-        cout << vect4[i] << " ";  
- 
-    vect1[0] = 2;  
-  
-    cout << "\nFirst element of vect1[] is :"<<vect1[0];
+    // copy<iterative>
+    vector<int> vect4;
+    vect4 = vect1;
 
-    cout << "\nFirst element of vect4[] is :"<<vect4[0];
-    
-    return 0;  
-}  
+    cout << "\nElements of vect1[] are: ";
+    print(vect1);
+
+    cout << "\nAfter copy(iterative) elements of vect4[] are: ";
+    print(vect4);
+
+    vect1[0] = 2;
+    cout << "\nFirst element of vect1[] is :" << vect1.front();
+
+    cout << "\nFirst element of vect4[] is :" << vect4.front();
+    vector<int>::iterator it;
+    merge(vect1.begin(), vect1.end(), vect2.begin(), vect2.end(), cev.begin());
+    cout << "\nAfter merge cev is: ";
+    print(cev);
+
+    cout << "\nReversed cev is: ";
+    for (it = cev.end() - 1; it != cev.begin() - 1; --it)
+        cout << *it;
+
+    return 0;
+}
