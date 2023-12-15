0c6020ad3e76afbb26299e4fee91baf59a0db887
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-15 16:44:49 +0100

    Update Count().cpp

--- STL/Funtion/Count().cpp
+++ STL/Funtion/Count().cpp
@@ -10,19 +10,49 @@ bool isEven(int i)
 }
 int main()
 {
-    vector<int> v;
+    vector<int> vect;
     vector<int> vec;
     for (int i = 0; i < 10; i++) {
-        v.push_back(i);
+        vect.push_back(i);
     }
-    int noEven = count_if(v.begin(), v.end(),isEven);
+    for(int i=10; i>0; i--)
+        vec.push_back(i);
+        
+    int noEven = count_if(vect.begin(), vect.end(),isEven);
     cout << "After using count_if Even count is: "<< noEven;
     
-    int n=count_if(v.begin(),v.end(),[](int i){return i%2!=0;});
+    int n=count_if(vect.begin(),vect.end(),[](int i){return i%2!=0;});
     cout << "\nAfter using count_if odd count is: "<< n;
     
     string str = "geeksforgeeks";
     cout << "\nAfter using count() 'e' count is : " << count(str.begin(), str.end(), 'e');
+    
+    cout << "\nOccurrences of 20 in vector : ";
+    
+ //Counts the occurrence of 20
+    cout << count(vec.begin(), vec.end(), 20);
  
+    find(vec.begin(), vec.end(),5) != vec.end()?
+    cout << "\nElement found":
+    cout << "\nElement not found";
+ 
+    vector<int>v{2, 3, 4, 8, 5, 7, 8 };
+    
+    vector<int> ::iterator i1,i2;
+    
+    i1=v.begin();i2=v.end();
+    
+    if(i1!=i2)
+    {
+        cout<<"\n1st & last value aren't same" ;
+    }
+    if (i1 < i2) {
+        cout << "\n1st value is smaller than last";
+    }
+    
+    //count total elements
+    int count = i2 - i1;
+    //count=distance(i1,i2);
+    cout<<"\nTotal element is:"<<count ;
     return 0;
 }
