b9755c191db4567d476c26ed1a673e2afa079108
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-15 17:29:26 +0100

    Update and rename Count().cpp to Count().cpp

--- STL/Basis/Count().cpp
+++ STL/Basis/Count().cpp
@@ -29,30 +29,27 @@ int main()
     
     cout << "\nOccurrences of 20 in vector : ";
     
- //Counts the occurrence of 20
+    //Counts the occurrence of 20
     cout << count(vec.begin(), vec.end(), 20);
  
-    find(vec.begin(), vec.end(),5) != vec.end()?
-    cout << "\nElement found":
-    cout << "\nElement not found";
+    if(find(vec.begin(), vec.end(),5) != vec.end())
+        cout << "\nElement found";
+    else
+        cout << "\nElement not found";
  
     vector<int>v{2, 3, 4, 8, 5, 7, 8 };
-    
     vector<int> ::iterator i1,i2;
     
     i1=v.begin();i2=v.end();
     
     if(i1!=i2)
-    {
         cout<<"\n1st & last value aren't same" ;
-    }
     if (i1 < i2) {
         cout << "\n1st value is smaller than last";
     }
-    
     //count total elements
-    int count = i2 - i1;
-    //count=distance(i1,i2);
+    //int count = i2 - i1;
+    int count=distance(i1,i2);
     cout<<"\nTotal element is:"<<count ;
     return 0;
 }
