a5bcf1a61f5e2befad1e21cbfba29ac6fd9ff153
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-16 15:25:41 +0100

    Update Fill().cpp

--- STL/Funtion/Fill().cpp
+++ STL/Funtion/Fill().cpp
@@ -1,39 +1,30 @@
 //fill() | fill_n()
 #include<bits/stdc++.h>
 using namespace std;
-void print(vector<int>&vect)
+void print(vector<int>vec)
 {
-    int len=vect.size();
-    for(int i=0; i<len; i++)
-    {
-        cout<<vect[i]<<" ";
-    }
+    for(int i=0; i<vec.size(); i++)
+        cout<< vec[i] << " ";
 }
-int main () 
+int main()
 {
-    vector<int> vect(8, 1); 
-    cout<<"Elements of vect is: ";
+    vector<int>vect(8,1);
+    cout<<"Imposed vect(fill): ";
     print(vect);
-
-    vector<int> vect2(8); 
-    fill(vect2.begin() + 2, vect2.end() - 1, 4);
     
-    cout<<"\nElements of vect2 is: ";
-    print(vect2);
-
-// Filling the complete vector with value 10 
-    fill(vect.begin(),vect.end(),10);
-    cout<<"\nAfter impose fill()  vect is: ";
+    cout<<"\nImposed fill: ";
+    fill(vect.begin(), vect.end(), 5);
     print(vect);
-
-   fill_n(vect.begin(), 4, 7);
-   cout<<"\nAfter impose fill_n vect is: ";
-   print(vect);
-    // calling fill to initialize 3 elements from 
-    // "begin()+3" with value 4
-    fill_n(vect.begin() + 3, 3, 4);
-    cout<<"\nAfter impose fill_n(range) vect is: ";
-   print(vect);
-
-    return 0;
+    cout<<"\nImposed fill(range): ";
+    fill(vect.begin()+2, vect.end()-2,5);
+    print(vect);
+    
+    cout<<"\nImposed fill_n: ";
+    fill_n(vect.begin(),4,7);
+    print(vect);
+    
+    cout<<"\nImposed fill_n(range): ";
+    fill_n(vect.begin()+2,2,3);
+    print(vect);
+    
 }
