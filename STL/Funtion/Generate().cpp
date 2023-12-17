c05b8e0080aab584930730b24224738833c78c62
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-17 15:07:51 +0100

    Update Generate().cpp

--- STL/Funtion/Generate().cpp
+++ STL/Funtion/Generate().cpp
@@ -1,4 +1,4 @@
-// generate () | generate_n()
+// generate () | generate_n() | transform() 
 #include <iostream> 
 #include <vector> 
 #include <algorithm> 
@@ -9,6 +9,10 @@ int gen()
     static int i = 0; 
     return ++i; 
 } 
+int increment(int x)
+{  
+    return (x+1); 
+}
 using namespace std; 
 int main() 
 {
@@ -27,5 +31,30 @@ int main()
     {
         cout<<*it<<" ";
     }
-    return 0; 
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+  
+    transform(arr, arr+n, arr, increment);
+    
+    cout<<"\nAfter transform array is: ";
+    for (int i=0; i<n; i++)
+        cout << arr[i] <<" ";
+        
+    int arr2[] = {4, 5, 6};
+    int res[n];
+  
+   transform(arr, arr+n, arr2, res, plus<int>());
+ 
+  cout<<"\nAfter transform result is: ";
+  for (int i=0; i<n; i++)
+    cout << res[i] << " ";
+    
+  // Code to add two arrays
+  for (int i=0; i<n; i++)
+    res[i] = arr[i] + arr2[i];
+ 
+  cout<<"\nSum is: ";
+  for (int i=0; i<3; i++)
+    cout << res[i] << " ";
+    return 0;
 } 
