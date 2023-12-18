2364a0f545294d3a093b38e8f82eb94060e74715
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-18 16:43:17 +0100

    Update and rename Function().cpp to Vector array.cpp

--- Array/Vector array.cpp
+++ Array/Vector array.cpp
@@ -1,15 +1,26 @@
 // working of at() | get() | front() | back()  | size() | max_size() 
+//empty() | fill()
 #include<iostream>
 #include<array> 
-#include<tuple> 
 using namespace std;
+
 int main()
 {
+    array<int,6> a;
+    array<int,0> ar1;
+ 
+    ar1.empty()? cout << "\nArray empty":cout << "\nArray not empty";
+ 
+    a.fill(0);
+    cout << "\nArray after filling operation is : ";
+    for ( int i=0; i<6; i++)
+        cout << a[i] << " ";
+    
     array<int,6> ar = {1, 2, 3, 4, 5, 6};
     int n= sizeof (ar) /sizeof (ar[0]) ;
     
     // Printing array elements using at()
-    cout << "Array elements are (using at()) : ";
+    cout << "\nArray elements are (using at()) : ";
     for ( int i=0; i<n; i++)
     cout << ar.at(i) << " ";
  
@@ -31,14 +42,14 @@ int main()
     cout << ar[i] << " ";
  
     cout << "\nFirst element of array is : ";
-    int &a=ar.front();
-    cout <<a ;
+    int &arr=ar.front();
+    cout <<arr ;
  
     cout << "\nLast element of array is : ";
     int &b=ar.back();
     cout << b ;
       
-    a=10; 
+    arr=10; 
     b=60; 
      
     cout<<"\nArray after updating first and last element :";
