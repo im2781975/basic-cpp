924ec6da6ffb90a1a826e2e725b3a6ac32469734
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-20 17:23:12 +0100

    Update Based column.cpp

--- Vector/Based column.cpp
+++ Vector/Based column.cpp
@@ -6,7 +6,10 @@ bool sizecom(const vector<int>& v1, const vector<int>& v2)
 {
     return v1.size() < v2.size();
 }
- 
+bool Sizecom(const vector<int>& v1,const vector<int>& v2)
+{
+    return v1.size() > v2.size();
+}
 int main()
 {
     vector< vector<int> >vect{{1, 2},{3, 4, 5},{6}};
@@ -18,9 +21,8 @@ int main()
         for (int j=0; j<vect[i].size() ;j++)
             cout <<vect[i][j]<< " ";
     }
- 
-    //sorting on basis of no. of columns in ascending order.
-    sort(vect.begin(), vect.end(), sizecom);
+    //sort(vect.begin(), vect.end(), sizecom);
+    sort(vect.begin(), vect.end(), Sizecom);
  
     cout << "\nThe Matrix after sorting is: ";
     for (int i=0; i<vect.size(); i++)
