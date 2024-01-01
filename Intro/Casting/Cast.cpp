dcdf02a1aa6dfc6b16a367287fe951a61daf7024
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-01 19:41:23 +0100

    Update and rename Cast const.cpp to Cast.cpp

---
+++ Intro/Casting/Cast.cpp
@@ -0,0 +1,26 @@
+// reinterpret_cast 
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+    int number = 10;
+    int* numberPointer = &number; 
+  
+    // Reinterpreting the pointer as a char pointer 
+    char* charPointer 
+        = reinterpret_cast<char*>(numberPointer); 
+    double* doublePointer=reinterpret_cast<double*>(doublePointer);
+    
+    cout << "\nInteger Address: " << numberPointer ; 
+    cout << "\nChar Address: "
+         << reinterpret_cast<void*>(charPointer) ;
+    cout<<"\nDouble pointer: "<<doublePointer ;
+    
+    const int a = 5; 
+    const int* ptr = &a; 
+
+    int* nonConstPtr = const_cast<int*>(ptr); 
+    *nonConstPtr = 10; 
+    cout << "\nModified number: " << *nonConstPtr; 
+    return 0; 
+}
