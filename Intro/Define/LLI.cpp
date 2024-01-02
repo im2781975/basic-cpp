2bdfd8743b6968584d59e8444d56195071d17208
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-02 18:54:40 +0100

    Update LLI.cpp

--- Intro/Define/LLI.cpp
+++ Intro/Define/LLI.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define mod 1000000007
+#define lli long long int
 using namespace std;
 int main()
 {
@@ -9,5 +10,12 @@ int main()
     //z = (z + x*y)%mod; 
     z = ((z%mod) + ((x%mod)*(y%mod))%mod) % mod;
     cout << z;
+    
+    lli a= 1e7;
+    lli b= 1e7;
+    lli c= 1LL*a*b;
+    cout<<"\n" << c;
+    
     return 0;
 }
+
