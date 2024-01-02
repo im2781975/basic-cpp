f880bcec6f9a1c1cee9e8153eb7c0cca4b49aa0b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-02 20:04:31 +0100

    Update and rename Base<range> to Print.cpp

--- Intro/Loop/Print.cpp
+++ Intro/Loop/Print.cpp
@@ -1,23 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
     vector<int> v{0, 1, 2, 3, 4, 5};
     for (auto i : v)
         cout << i << ' ';
-     
     cout << '\n';
 
     for (int n : {0, 1, 2, 3, 4, 5})
         cout << n << ' ';
-  
     cout << '\n';
   
     int a[]{0, 1, 2, 3, 4, 5};
     for (int n : a)
         cout << n << ' ';
-     
     cout << '\n';
   
     string str = "ibrahim";
