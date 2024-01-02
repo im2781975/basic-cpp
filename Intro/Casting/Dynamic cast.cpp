293dffb372911eea636e4b1a643ddaea5bb67d2c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-02 15:23:51 +0100

    Update and rename Class<Derived> to Dynamic cast.cpp

--- Intro/Casting/Dynamic cast.cpp
+++ Intro/Casting/Dynamic cast.cpp
@@ -1,32 +1,27 @@
 //successful dynamic casting and it returns value of type new_type
 #include <iostream>
- 
 using namespace std;
 class Base {
     virtual void print()
     {
-        cout << "Base" << endl;
+        cout << "Base" ;
     }
 };
-
 class Derived1 : public Base {
     void print()
     {
         cout << "Derived1" << "\n";
     }
 };
-
 class Derived2 : public Base {
     void print()
     {
         cout << "Derived2" << "\n";
     }
 };
-
 int main()
 {
     Derived1 d1;
- 
     // Base class pointer holding
     // Derived1 Class object
     Base* bp = dynamic_cast<Base*>(&d1);
@@ -37,6 +32,5 @@ int main()
         cout << "null" << "\n";
     else
         cout << "not null" << "\n";
- 
     return 0;
 }
