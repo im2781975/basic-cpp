052ec1373eb97649d35b1826a0146c322d44ebf0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-02 19:23:04 +0100

    Update and rename Pragma to Pragma.cpp

--- Intro/Define/Pragma.cpp
+++ Intro/Define/Pragma.cpp
@@ -1,23 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
 void func1();
 void func2();
 
 #pragma startup func1
-
 #pragma exit func2
  
 void func1(){cout<<"func1()\n"; }
- 
 void func2(){cout << "func2()\n"; }
  
 int main()
 {
     void func1();
-   void func2();
+    void func2();
     cout << "Inside main()\n";
- 
     return 0;
 }
- 
