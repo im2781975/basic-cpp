d24b5dd88a782451cb3070982ffd2070033c741f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-03 21:27:07 +0100

    Update Build.cpp

--- Intro/Base/Build.cpp
+++ Intro/Base/Build.cpp
@@ -15,6 +15,18 @@ int main()
     cout << "You have entered:\n"; 
     cout << x << "\n"; 
     cout << str << "\n"; 
-  
+    
+    double pi = 3.14159783;
+    cout.precision(5);
+    cout << pi << "\n";
+ 
+    cout.precision(7);
+    cout << pi << "\n";
+    
+    char gfg[]="ibrahim";
+    char ch='e';
+    cout.write(gfg, 6);
+    cout.put(ch);
+    cout<<"\n"<<gfg;
     return 0; 
 }
