991592b2b9c2ed5837b213aba2e233da165785b7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-03 17:00:16 +0100

    Update Nested.cpp

--- Intro/Namespace/Nested.cpp
+++ Intro/Namespace/Nested.cpp
@@ -11,11 +11,20 @@ namespace first
         //using namespace third;
     } 
     //using namespace second;
-} 
+}
 namespace base=first ::second ::third;
+namespace out
+{
+    int val = 5;
+    namespace in
+    {
+        int v =val;
+    }
+}
 int main()
 {
     //cout << first::var;
     cout << base ::var;
+    cout << out :: in::v;
     return 0;
 }
