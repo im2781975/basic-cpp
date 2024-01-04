e314708eefc740f0f9fe1279b499d72fe98bdcab
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-04 20:04:48 +0100

    Update and rename Logical operator to Logical.cpp

--- Intro/Logical.cpp
+++ Intro/Logical.cpp
@@ -1,4 +1,4 @@
-// C++ Program to illustrate the logical AND, OR, NOT Operator 
+//logical AND, OR, NOT Operator 
 #include <iostream> 
 using namespace std; 
   
@@ -8,33 +8,20 @@ int main()
     bool isStudent = true; 
   
     if (age > 18 && isStudent) 
-    { 
         cout <<"\nYou are eligible for a student discount.";
-    } 
     else
-    { 
         cout << "\nYou are not eligible for a student discount.";
-    }
-    
     int num = 7; 
     if (num <= 0 || num >= 10) 
-    { 
         cout  << "\nThe number is outside the range of 0 to 10." ;
-    } 
     else 
-    { 
         cout << "\nThe number is between 0 to 10." ;
-    } 
     bool isLoggedIn = false; 
   
     // using logical not operator 
     if (!isLoggedIn) 
-    { 
         cout << "\nPlease log in to access this feature.";
-    } 
     else 
-    { 
-        cout << "\nWelcome to GeeksforGeeks!" ; 
-    } 
+        cout << "\nWelcome !" ; 
         return 0; 
 }
