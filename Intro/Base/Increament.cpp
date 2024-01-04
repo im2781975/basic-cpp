38ebc4d82cc1d13369f091c1edfb20d95aaaf7e5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-04 05:05:51 +0100

    Update and rename Incre & decreament to Increament.cpp

--- Intro/Base/Increament.cpp
+++ Intro/Base/Increament.cpp
@@ -1,17 +1,13 @@
-// increment and decrement operators 
 #include <iostream> 
 using namespace std; 
-  
 int main() 
 { 
     int x = 5,y=10;
+    
     cout << "x++ is : " << x++ ;
-  
     cout << "\n++x is : " << ++x;
     
     cout << "\ny-- is : " << y-- ;
-  
     cout << "\n--y is : " << --y ;
-  
     return 0; 
 }
