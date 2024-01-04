50d5893b677c6b20b9880f3f06708cd741f91fcc
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-04 21:04:34 +0100

    Update Register.cpp

--- Intro/Base/Register.cpp
+++ Intro/Base/Register.cpp
@@ -1,9 +1,14 @@
-//Registers are faster than memory to access, so the variables which are most frequently 
-//used in a program can be put in registers using the register keyword
+//Registers are faster than memory to access, so the variables which are most frequently used
+//in a program can be put in registers using the register keyword
 #include <iostream>
 #include <cstdio>
 using namespace std;
-
+void registerStorageClass()
+{
+    cout << "Demonstrating register class\n";
+    register char b = 'G';
+    cout << "\nValue of the variable 'b'"<< " declared as register: " << b;
+}
 void fastscan(int &number)
 {
     bool negative = false;
@@ -20,12 +25,10 @@ void fastscan(int &number)
         c = getchar();
     }
     // Keep on extracting characters if they are integers
-    // i.e ASCII Value lies from '0' (48) to '9' (57)
     for (; (c >= '0' && c <= '9'); c = getchar())
         number = number * 10 + c - '0';
 
-    // If scanned input has a negative sign, negate the
-    // value of the input number
+    // If scanned input has a negative sign, negate the value of the input number
     if (negative)
         number *= -1;
 }
@@ -34,6 +37,7 @@ int main()
     int number;
     fastscan(number);
     cout << number << "\n";
+    registerStorageClass();
     return 0;
 }
 
