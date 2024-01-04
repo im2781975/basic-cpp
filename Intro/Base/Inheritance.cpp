614cb6c27542fed43dcf752d1ec894df404664c1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-04 22:00:18 +0100

    Update and rename Intro/Scope<class> to Intro/Base/Inheritance.cpp

--- Intro/Base/Inheritance.cpp
+++ Intro/Base/Inheritance.cpp
@@ -1,7 +1,6 @@
 // multiple inheritance.
 #include<iostream>
 using namespace std;
- 
 class A
 {
     protected:
@@ -9,7 +8,6 @@ class A
     public:
     A() { x = 10; }
 };
- 
 class B
 {
     protected:
@@ -17,15 +15,14 @@ class B
     public:
     B() { x = 20; }
 };
-
 class C: public A, public B
 {
     public:
     void fun()
-{
-    cout << "A's x is " << A::x;
-    cout << "\nB's x is " << B::x;
-}
+    {
+        cout << "A's x is " << A::x;
+        cout << "\nB's x is " << B::x;
+    }
 };
 int main()
 {
