d5a88ff6557c46b318ba0e3531fbef23bad0ea5a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-04 20:14:35 +0100

    Update and rename Intro/MSD to Intro/Base/MSD.cpp

--- Intro/Base/MSD.cpp
+++ Intro/Base/MSD.cpp
@@ -2,14 +2,14 @@
 using namespace std;
  
 int MSD(int n){
-      if(n == 0)
+    if(n == 0)
         return 0;
-      int k = log10(n);
+    int k = log10(n);
     int x = pow(10,k);
+    
     int ans = n/x;
       return ans;
 }
- 
 int main() {
     int n;
     cin >> n;
