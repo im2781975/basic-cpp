1f0d00a232741afcc2c12b48a6bb13a135c2c16c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-04 22:02:16 +0100

    Update and rename Intro/Scope<class 0> to Intro/Base/Class scope.cpp

--- Intro/Base/Class scope.cpp
+++ Intro/Base/Class scope.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
- 
 string name1 = "GFG";
 string favlang = "python";
 string companyName = "GFG_2.0";
@@ -13,11 +12,8 @@ public:
     string company = "GFG";
  
     Developer(string favlang,string company): favLang(favlang)
-        , company(companyName)
-    {
-    }
+        , company(companyName){}
 };
- 
 int main()
 {
     Developer obj = Developer("python", "GFG");
