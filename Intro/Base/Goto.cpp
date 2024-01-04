5fb46367f46075fb56f9d86fd2da323844b615ff
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-04 22:45:10 +0100

    Update Goto.cpp

--- Intro/Base/Goto.cpp
+++ Intro/Base/Goto.cpp
@@ -1,1 +1,19 @@
-m
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if (n % 2 == 0) 
+        goto label1;
+    else
+        goto label2;
+ 
+    label1:
+        cout << "Even" << "\n";
+        return 0;
+    label2:
+        cout << "Odd" << "\n";
+}
+
+
