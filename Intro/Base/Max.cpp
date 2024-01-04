f1a5100d7ffb37a763da23eaaa68f29cfa402037
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-04 16:45:37 +0100

    Update and rename Intro/Find max<recursion> to Intro/Base/Max.cpp

--- Intro/Base/Max.cpp
+++ Intro/Base/Max.cpp
@@ -9,7 +9,6 @@ int findMax(int arr[], int size) {
         return maxInRest > arr[size - 1] ? maxInRest : arr[size - 1];
     }
 }
-
 int main() {
     int n;
     cin >> n;
@@ -18,10 +17,18 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
     int maxElement = findMax(arr, n);
-    cout << maxElement << endl;
-
+    cout << maxElement ;
+    
+    int num, test = 40; 
+    num = test < 10 ? 10 : test + 10; 
+  
+    cout << "\nNum - Test : " << num - test; 
+  
+    int A = 39, B = 10, C = 23; 
+    int maxNum 
+        = (A > B) ? ((A > C) ? A : C) : ((B > C) ? B : C); 
+    cout << "\nLargest number is " << maxNum ; 
     return 0;
 }
 
