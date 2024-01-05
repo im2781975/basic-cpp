2df5621c29ec63af4cb8199e04dd18cc5dccafe3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-05 18:42:15 +0100

    Update and rename Class<move ptr> to Unique ptr.cpp

--- Pointer/Unique ptr.cpp
+++ Pointer/Unique ptr.cpp
@@ -1,33 +1,31 @@
-// C++ program to illustrate the use of unique_ptr
+// unique_ptr
 #include <iostream>
 #include <memory>
 using namespace std;
-  
 class A {
 public:
-    void show() { cout << "A::show()" << endl; }
+    void show(){
+        cout << "A::show()\n" ;
+    }
 };
-  
 int main()
 {
     unique_ptr<A> p1(new A);
     p1->show();
-  
     // returns the memory address of p1
-    cout << p1.get() << endl;
+    cout << p1.get() << "\n";
   
     // transfers ownership to p2
     unique_ptr<A> p2 = move(p1);
     p2->show();
-    cout << p1.get() << endl;
-    cout << p2.get() << endl;
+    cout << p1.get() << "\n";
+    cout << p2.get() << "\n";
   
     // transfers ownership to p3
     unique_ptr<A> p3 = move(p2);
     p3->show();
-    cout << p1.get() << endl;
-    cout << p2.get() << endl;
-    cout << p3.get() << endl;
-  
+    cout << p1.get() << "\n";
+    cout << p2.get() << "\n";
+    cout << p3.get() << "\n";
     return 0;
 }
