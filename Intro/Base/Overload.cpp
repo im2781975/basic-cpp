db6cf34ee08c29e5ef2721d698a685d6dd4adcad
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-05 15:46:26 +0100

    Update Overload.cpp

--- Intro/Base/Overload.cpp
+++ Intro/Base/Overload.cpp
@@ -1,16 +1,53 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-void foo(char c)
+void test(int var)
 {
-   cout<<"From foo: char"<<"\n";
+    cout<<"\nInteger number: "<<var;
 }
-void foo(int i)
+void test(float var)
 {
-   cout<<"From foo: int"<<"\n";
+    cout <<"\nFloat number: "<< var;
+}
+void test(int var1, float var2)
+{
+    cout << "\nInteger & float number is: " << var1 <<" , " <<var2;
+}
+void test(char var)
+{
+    cout<<"\nChar value: "<<var ;
+}
+void print(int i)
+{
+    cout <<"\nHere is int " << i;
+}
+void print(double f)
+{
+    cout << "\nHere is float " << f;
+}
+void print(char const* c)
+{
+    cout << "\nHere is char* " << c;
+}
+void print(float s,float t)
+{
+    cout << "\nFunction with float called ";
+}
+void print(int s,int t)
+{
+    cout << "\nFunction with int called ";
 }
 int main()
 {
-   foo('V');
-   foo(89);
-   return 0;
+    int a = 5;
+    float b = 5.5;
+    char c='a';
+    test(a);
+    test(b);
+    test(a, b);
+    test(c);
+    print(10);
+    print(10.10);
+    print("ten");
+    print (3.7f,9.8f);
+    return 0;
 }
