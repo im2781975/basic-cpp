976ce30647a83888550d3db4bcc8e5c1bb3dc02a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-06 06:13:05 +0100

    Update Reference.cpp

--- Pointer/Reference.cpp
+++ Pointer/Reference.cpp
@@ -1,13 +1,23 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
+void fun(const int &i)
+{
+    cout << "fun(const int &) called ";
+}
+void fun(int &i)
+{
+    cout << "fun(int &) called " ;
+}
 int main()
 {
+    const int i = 10;
+    fun(i);
     int x = 10;
     int& ref = x;
     ref = 20;
-    cout << "x = " << x << '\n';
+    cout << "\nx = " << x ;
     // Value of x is now changed to 30
     x = 30;
-    cout << "ref = " << ref << '\n';
+    cout << "\nref = " << ref ;
     return 0;
 }
