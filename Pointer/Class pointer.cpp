844490b2d60db00cb96d7ee4080ea36616ba9747
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-06 15:18:29 +0100

    Update and rename Pointer<delete this> to Class pointer.cpp

--- Pointer/Class pointer.cpp
+++ Pointer/Class pointer.cpp
@@ -9,17 +9,13 @@ public:
         delete this;
     }
 };
-
 int main()
 {
     A *ptr = new A;
     ptr->fun();
     ptr = nullptr; // Make ptr nullptr to make sure that it's not used again.
-
-    /* And following is Invalid: Undefined Behavior */
     A a;
-    a.fun(); // This will cause undefined behavior, don't call fun() on a locally allocated object.
-
+    a.fun();
     return 0;
 }
 
