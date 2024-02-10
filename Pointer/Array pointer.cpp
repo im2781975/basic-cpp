1162bfd25b702674f2e35dc87264c19836e76532
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-10 05:34:53 +0100

    Update Array pointer.cpp

--- Pointer/Array pointer.cpp
+++ Pointer/Array pointer.cpp
@@ -2,37 +2,43 @@
 using namespace std;
 int main()
 {
-    int *p; 
-    int (*ptr)[5]; 
+    int *p;
+    int *ptr[5];
     int arr[5];
     p = arr;
-    
-    ptr = &arr; 
-    
-    cout<< "Address is: \n";
-    for(int i= 0; i< 5; i++)
+    for(int i=0; i<5; i++)
+        ptr[i] = &arr[i];
+    for(int i=0; i<5; i++)
     {
-        cout << "p =" << p <<", ptr = "<< ptr<< "\n";
-        p++; 
-        ptr++;
+        cout << p << " " << ptr << "\n";
+        p++;
     }
-    cout << "sizeof(p) : "<< sizeof(p) <<
-            ", sizeof(*p) : " << sizeof(*p) ;
-    cout << "\nsizeof(ptr): "<< sizeof(ptr) <<", sizeof(*ptr): " << sizeof(*ptr) ;
-     
+    cout << "sizeof(p): " << sizeof(p) << "\n";
+    cout << "sizeof(*p): " << sizeof(*p) << "\n";
+    cout << "sizeof(ptr): " << sizeof(ptr) << "\n";
+    cout << "sizeof(*ptr): " << sizeof(*ptr) << "\n";
+    
     int ar[3][4] = { {10, 11, 12, 13}, {20, 21, 22, 23}, 
         {30, 31, 32, 33} };
     int (*pr)[4];
     pr = ar;
-    cout<< "\nAddress of pointer: \n";
-    cout << pr<< " "<< pr + 1<< " "<< pr + 2 ;
+    cout << "pr: " << pr << "\n";
+    cout << "pr + 1: " << pr+1 << "\n";
+    cout << "pr + 2: " << pr+2 << "\n";
+    cout << "pr + 3: " << pr+3 << "\n";
+    cout << "*pr: " << *pr << "\n";
+    cout << "*(pr + 1): " << *(pr+1) << "\n";
+    cout << "*(pr + 2): " << *(pr+2) << "\n";
+    cout << "*(pr + 3): " << *(pr+3) << "\n";
+    //First value of every row
+    cout << "**pr: " << **pr << "\n";
+    cout << "**(pr + 1): " << **(pr+1) << "\n";
+    cout << "**(pr + 2): " << **(pr + 2) << "\n";
     
-    cout<<"\nAddress of pointer: \n";
-  cout << *pr<< " "<< *(pr + 1)<< " "<< *(pr + 2) ;
-  
-  cout<<"\nElements of pointer: ";
-  cout << **pr<< " "<< *(*(pr + 1) + 2)<< " "<< *(*(pr + 2) + 3) ;
-  cout<<"\nElements of pointer specific posotion: ";
-  cout << pr[0][0]<< " "<< pr[1][2]<< " "<< pr[2][3]<< endl;
-    return 0;
+    cout << "*(*(pr +0) +2): " << *(*(pr +0) + 2) << "\n";
+    cout << "**(pr + 2): " << **(pr + 2) << "\n";
+    
+    cout << "pr[0][0]: " << pr[0][0] << "\n";
+    cout << "pr[1][1]: " << pr[1][1] << "\n";
+    cout << "pr[2][2]: " << pr[2][2] << "\n";
 }
