2654a06dae15a0f0771a91d44a6a953731c05e51
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-11 19:17:44 +0100

    Update and rename String/Function/Getline to String/GetLine.cpp

---
+++ String/GetLine.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char name[100], add[100], about[100];
+    cout << "\nName is: ";
+    cin.getline(name, 100);
+    cout << "\nAddress is: ";
+    cin.getline(add, 100);
+    cout << "\nDescribe yourself: ";
+    cin.getline(about, 100);
+    
+    cout << "\nName is: " << name;
+    cout << "\nAddress is: " << add;
+    cout << "\nAbout is: " << about ;
+}
