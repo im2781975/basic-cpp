6a5d361c2fbd709d528d6c39143ee59b25e43087
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-11 20:17:12 +0100

    Update and rename String/Function/Stoi to String/Stoi.cpp

--- String/Stoi.cpp
+++ String/Stoi.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    string str = "2009, GeeksforGeeks_founded";
+    string str = "2009 GeeksforGeeks_founded";
     string str1 = "0x6C1";
     string str2 = "-10010010101";
 
@@ -11,9 +11,8 @@ int main()
     int num_hex = stoi(str1, nullptr, 16);
     int num_bin = stoi(str2, nullptr, 2);
  
-    cout<<str<<": "<< num <<"\n";
-    cout<<str1<<":"<<num_hex<<"\n";
-    cout << str2 << ": " << num_bin <<"\n";
- 
+    cout << str << ": " << num << "\n";
+    cout << str1 << ":" << num_hex<< "\n";
+    cout << str2 << ": " << num_bin << "\n";
     return 0;
 }
