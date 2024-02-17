26c8700df02851cafa96b3585be69ce4a146cbcf
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-17 15:59:18 +0100

    Update Forward.cpp

--- STL/List/Forward.cpp
+++ STL/List/Forward.cpp
@@ -1,4 +1,4 @@
-//copy() | merge() | push_front() |insert_after() |  pop_front() | emplace_front() | remove() | remove_if() | erase_after() | clear() | sort() | unique() | splice_after()
+//copy() | merge() | push_front() |insert_after() |  pop_front() | emplace_front() | remove() | remove_if() | erase_after() | clear() | sort() | unique() | splice_after() | assign()
 #include<bits/stdc++.h>
 using namespace std;
 void print(forward_list<int>fl)
@@ -15,6 +15,15 @@ int main()
     forward_list<int>list;
     forward_list<int>mist;
     forward_list<int>::iterator it;
+    forward_list<int> list1;
+    list1.assign({ 1, 2, 3 });
+    
+    forward_list<int> list2;
+    list2.assign(5, 10);
+    
+    forward_list<int> list3;
+    list3.assign(list1.begin(), list1.end());
+
     for(int i=1; i<=10; i++)
     {
         list.push_front(i);
