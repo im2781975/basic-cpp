30614bf3be92165597ccd9d64a7b9890796a7278
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-13 00:17:51 +0200

    Update Pair sort.cpp

--- Vector/Pair sort.cpp
+++ Vector/Pair sort.cpp
@@ -1,39 +1,26 @@
 //sorting in vector of pair according to 2nd element of pair in descending order
 #include<bits/stdc++.h>
 using namespace std;
-
-bool sortbysecdesc(const pair<int,int> &a,const pair<int,int> &b)
+bool sortby(const pair<int, int> a, const pair <int, int> b)
 {
-       return a.second > b.second;
+    return a.second < b.second;
 }
-bool Sortbysecdesc(const pair<int,int> &a,const pair<int,int> &b)
+bool sortby(const pair<int, int> a, const pair <int, int> b)
 {
-       return a.second < b.second;
+    return a.second > b.second;
 }
 int main()
 {
-    vector< pair <int,int> > vect;
- 
-    int arr[] = {5, 20, 10, 40 };
-    int arr1[] = {30, 60, 20, 50};
-    int n = sizeof(arr)/sizeof(arr[0]);
- 
-    for (int i=0; i<n; i++)
-        vect.push_back( make_pair(arr[i],arr1[i]) );
- 
-    cout << "Before sort vector is : ";
-    for (int i=0; i<n; i++)
-    {
-        cout <<vect[i].first << " "
-            <<vect[i].second<<"\n";
-    }
-    //sort(vect.begin(), vect.end(), sortbysecdesc);
-    sort(vect.begin(), vect.end(), Sortbysecdesc);
-
-    cout << "After sort vector is: " ;
-    for (int i=0; i<n; i++)
-    {
-        cout << vect[i].first <<" "<<vect[i].second <<"\n";
-    }
-    return 0;
+    vector <pair <int, int> >vect;
+    int a[]{1, 2, 3, 4};
+    int b[]{4, 3, 2, 1};
+    int n = sizeof(a)/sizeof(a[0]);
+    for(int i = 0; i < n; i++)
+        vect.push_back(make_pair(a[i], b[i]));
+        
+    vect.resize(n);
+    sort(vect.begin(), vect.end(), sortby);
+    
+    for(int i = 0; i < vect.size(); i++)
+        cout << vect[i].first << " " << vect[i].second << "\n";
 }
