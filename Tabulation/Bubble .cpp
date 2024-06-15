181a35afeb513d09c1b9430b4ba99fa37a171f7b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-15 08:01:24 +0200

    Update and rename Bubble 2.cpp to Bubble .cpp

--- Tabulation/Bubble .cpp
+++ Tabulation/Bubble .cpp
@@ -3,13 +3,12 @@ using namespace std;
 int main()
 {
     int n, temp;
-    cin>> n;
+    cin >> n;
     int arr[n+5];
-    for(int i =1; i<=n; i++){
+    for(int i = 1; i<= n; i++)
         cin >> arr[i];
-    }
-    for(int i=1; i<n; i++){
-        for(int j = i+1; j<=n; j++){
+    for(int i = 1; i < n; i++){
+        for(int j = i+1; j<= n; j++){
             if(arr[i] < arr[j]){
                 temp = arr[i];
                 arr[i] = arr[j];
@@ -17,8 +16,7 @@ int main()
             }
         }
     }
-    for(int i=1; i<=n; i++){
+    for(int i = 1; i<= n; i++)
         cout <<  arr[i] << " ";
-    }
     return 0;
 }
