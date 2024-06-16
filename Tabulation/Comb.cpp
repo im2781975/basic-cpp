4dee05b128c09c2233f471764ecf359349a80212
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-16 22:50:33 +0200

    Update Comb.cpp

--- Tabulation/Comb.cpp
+++ Tabulation/Comb.cpp
@@ -1,11 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-// To find gap between elements
 int getNextGap(int gap)
 {
-    // Shrink gap by Shrink factor
     gap = (gap*10)/13;
- 
     if (gap < 1)
         return 1;
     return gap;
@@ -17,64 +14,58 @@ void combSort(int a[], int n)
     // Keep running while gap is more than 1 and last iteration caused a swap
     while (gap != 1 || swapped == true)
     {
-        // Find next gap
         gap = getNextGap(gap);
         // Initialize swapped as false so that we can check if swap happened or not
         swapped = false;
- 
-        // Compare all elements with current gap
-        for (int i=0; i<n-gap; i++)
+        for(int i = 0; i < n - gap; i++)
         {
-            if (a[i] > a[i+gap])
+            if (a[i] > a[i + gap])
             {
-                swap(a[i], a[i+gap]);
+                swap(a[i], a[i + gap]);
                 swapped = true;
             }
         }
     }
 }
-int main()
+void CocktailSort(int a[], int n)
 {
-    int a[] = {8, 4, 1, 56, 3, -44, 23, -6, 28, 0};
-    int n = sizeof(a)/sizeof(a[0]);
- 
-    combSort(a, n);
- 
-    cout<<"\nSorted array is: ";
-    for (int i=0; i<n; i++)
-        cout<<arr[i]<<" ";
- 
-    return 0;
-}
-#include<bits/stdc++.h>
-using namespace std;
-int gapnxt(int gap)
-{
-    gap = (gap * 10)/13;
-    if(gap < 1)
-        return 1;
-    return gap;
-}
-void perform(int *arr, int n)
-{
-    int gap = n;
-    bool swaped = true;
-    while(gap!=1 || swaped)
-    {
-        gap = gapnxt(gap);
-        swaped = false;
-        for(int i =  0; i < n - gap)
-        {
-            if(arr[i] > arr[i + gap])
-                swap(arr[i], arr[i + gap]);
-                swaped = true;
+    bool swapped = true;
+    int start = 0;
+    int end = n - 1;
+    while (swapped) {
+        // reset the swapped flag on entering the loop, because it might be true from a previous iteration.
+        swapped = false;
+        for (int i = start; i < end; ++i) {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+                swapped = true;
+            }
         }
-        
+        if (!swapped)
+            break;
+        // otherwise, reset the swapped flag so that it can be used in the next stage
+        swapped = false;
+        // move the end point back by one, because item at the end is in its rightful spot
+        --end;
+        // from right to left, doing the same comparison as in the previous stage
+        for (int i = end - 1; i >= start; --i) {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+                swapped = true;
+            }
+        }
+        // increase the starting point, because the last stage would
+        //have moved the next smallest number to its rightful spot.
+        ++start;
     }
 }
 int main()
 {
-    int arr[]{2, 3, 1, 5, 4};
+    int arr[] = {8, 4, 1, 56, 3, -44, 23, -6, 28, 0};
     int n = sizeof(arr)/sizeof(arr[0]);
-    perform(arr, n);
+    combSort(arr, n);
+    CocktailSort(arr, n);
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
 }
