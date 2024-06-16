a7fc6076ccc3d439d6d4d1837586edd3ba2d30c5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-16 23:55:10 +0200

    Update Merge 3.cpp

--- Tabulation/Merge 3.cpp
+++ Tabulation/Merge 3.cpp
@@ -1,7 +1,6 @@
 // perform 3 way Merge Sort
 #include <bits/stdc++.h>
 using namespace std;
-//Merge the sorted ranges [low, mid1), [mid1,mid2) & [mid2, high) mid1 is first midpoint 
 void merge(vector<int> &gArray, int low, int mid1, int mid2, int high, vector<int> &destArray) 
 { 
     int i = low, j = mid1, k = mid2, l = low; 
@@ -11,61 +10,41 @@ void merge(vector<int> &gArray, int low, int mid1, int mid2, int high, vector<in
         if(gArray[i] < gArray[j])
         {
             if(gArray[i] < gArray[k])
-            {
                 destArray[l++] = gArray[i++];
-            }
             else
-            {
                 destArray[l++] = gArray[k++];
-            }
         }
         else
         {
             if(gArray[j] < gArray[k])
-            {
                 destArray[l++] = gArray[j++];
-            }
             else
-            {
                 destArray[l++] = gArray[k++];
-            }
         }
     } 
     // Case where first and second ranges have remaining values 
     while ((i < mid1) && (j < mid2)) 
     { 
         if(gArray[i] < gArray[j])
-        {
             destArray[l++] = gArray[i++];
-        }
         else
-        {
             destArray[l++] = gArray[j++];
-        }
     }
     // case where second and third ranges have remaining values 
     while ((j < mid2) && (k < high)) 
     { 
         if(gArray[j] < gArray[k])
-        {
             destArray[l++] = gArray[j++];
-        }
         else
-        {
             destArray[l++] = gArray[k++];
-        } 
     } 
     // Case where first and third ranges have remaining values 
     while ((i < mid1) && (k < high)) 
     { 
         if(gArray[i] < gArray[k])
-        {
             destArray[l++] = gArray[i++];
-        }
         else
-        {
             destArray[l++] = gArray[k++];
-        } 
     } 
     // Copy remaining values from the first range 
     while (i < mid1) 
@@ -79,10 +58,8 @@ void merge(vector<int> &gArray, int low, int mid1, int mid2, int high, vector<in
 } 
 void mergeSort3WayRec(vector<int> &gArray, int low,int high, vector<int> &destArray) 
 { 
-    // If array size is 1 then do nothing 
     if (high - low < 2) 
         return; 
-    // Splitting array into 3 parts 
     int mid1 = low + ((high - low) / 3); 
     int mid2 = low + 2 * ((high - low) / 3) + 1; 
  
@@ -93,25 +70,17 @@ void mergeSort3WayRec(vector<int> &gArray, int low,int high, vector<int> &destAr
  
     merge(destArray, low, mid1, mid2, high, gArray); 
 }
- 
 void mergeSort3Way(vector<int> &gArray, int n) 
 { 
     if (n == 0) 
         return; 
- 
-    // creating duplicate of given array 
     vector<int> fArray(n); 
- 
-    // copying elements of given array into 
-    // duplicate array 
     for (int i = 0; i < n; i++) 
         fArray[i] = gArray[i]; 
- 
     // sort function 
     mergeSort3WayRec(fArray, 0, n, gArray); 
  
-    // copy back elements of duplicate array 
-    // to given array 
+    // copy back elements of duplicate array to given array
     for (int i = 0; i < n; i++) 
         gArray[i] = fArray[i]; 
 } 
@@ -121,8 +90,6 @@ int main()
     mergeSort3Way(data,10);
     cout << "After 3 way merge sort: ";
     for (int i = 0; i < 10; i++)
-    {
         cout << data[i] << " ";
-    }
     return 0;
 }
