4b5bd1d7efeb2effd782df290a37f9a023c75440
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-16 23:34:13 +0200

    Update Merge 1.cpp

--- Tabulation/Merge 1.cpp
+++ Tabulation/Merge 1.cpp
@@ -3,27 +3,20 @@ using namespace std;
 vector<int> merge_sort(vector<int> a)
 {
     if (a.size() <= 1)
-    {
         return a;
-    }
 
     int mid = a.size() / 2;
-    vector<int> b;
-    vector<int> c;
-
+    vector<int> b, c;
     for (int i = 0; i < mid; i++)
         b.push_back(a[i]);
 
     for (int i = mid; i < a.size(); i++)
         c.push_back(a[i]);
-
     vector<int> sorted_b = merge_sort(b);
     vector<int> sorted_c = merge_sort(c);
 
     vector<int> sorted_a;
-    int idx1 = 0;
-    int idx2 = 0;
-
+    int idx1 = 0, idx2 = 0;
     for (int i = 0; i < a.size(); i++)
     {
         if (idx1 == sorted_b.size())
@@ -53,9 +46,7 @@ int main()
 {
     vector<int> a = {5, 3, 7, 1, 8, 9};
     vector<int> ans = merge_sort(a);
-
     for (int i = 0; i < ans.size(); i++)
         cout << ans[i] << " ";
-
     return 0;
 }
