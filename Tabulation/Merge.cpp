514312bf77ba57c34366e8c7a8681ce165552ac2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-16 23:32:48 +0200

    Update Merge.cpp

--- Tabulation/Merge.cpp
+++ Tabulation/Merge.cpp
@@ -3,8 +3,7 @@ using namespace std;
 vector<int>merged(vector<int>a, vector<int>b)
 {
     vector<int>output;
-    int idx1 = 0;
-    int idx2 = 0;
+    int idx1 = 0, idx2 = 0;
     int n = a.size() + b.size();
 
     for(int i=0; i<n; i++)
@@ -34,41 +33,31 @@ vector<int>merged(vector<int>a, vector<int>b)
 }
 vector<int>devide(vector<int>p)
 {
-    vector<int>b;
-    vector<int>c;
-
+    vector<int>b, c;
     if(p.size() <= 1)
         return p;
-
     int len = p.size();
     for(int i =0; i<len/2; i++)
-    {
         b.push_back(p[i]);
-    }
+        
     for(int i=len/2; i<len; i++)
-    {
         c.push_back(p[i]);
-    }
-
     vector<int>Sorted_b = devide(b);
     vector<int>Sorted_c = devide(c);
-
     return merged(Sorted_b, Sorted_c);
 }
 int main()
 {
     int r, x;
-    vector<int>p;
+    vector <int> p;
     cin >> r;
-    for(int i=0; i<r; i++)
+    for(int i = 0; i < r; i++)
     {
         cin >> x;
         p.push_back(x);
     }
     vector<int>ans = devide(p);
     for(int i = 0; i<ans.size(); i++)
-    {
         cout << ans[i] << " ";
-    }
     return 0;
 }
