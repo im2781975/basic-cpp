484b55cc6f08b50c00810b82fae70933bca36feb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-17 09:48:29 +0200

    Update Redix.cpp

--- Tabulation/Redix.cpp
+++ Tabulation/Redix.cpp
@@ -43,6 +43,28 @@ void radixsort(int arr[], int n)
     for (int exp = 1; m / exp > 0; exp *= 10)
         countSort(arr, n, exp);
 }
+void stoogesort(int arr[], int l, int h) 
+{ 
+    if (l >= h) 
+        return; 
+  
+    // If first element is smaller than last, swap them 
+    if (arr[l] > arr[h]) 
+        swap(arr[l], arr[h]); 
+  
+    // If there are more than 2 elements in the array 
+    if (h - l + 1 > 2) { 
+        int t = (h - l + 1) / 3; 
+    //Recursively sort first 2/3 elements 
+        stoogesort(arr, l, h - t); 
+  
+    // Recursively sort last 2/3 elements 
+        stoogesort(arr, l + t, h); 
+    // Recursively sort first 2/3 elements 
+    // again to confirm 
+        stoogesort(arr, l, h - t); 
+    } 
+} 
 void print(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -55,5 +77,8 @@ int main()
  
     radixsort(arr, n);
     print(arr, n);
+    stoogesort(arr, 0, n - 1); 
+    for (int i = 0; i < n; i++) 
+        cout << arr[i] << " "; 
     return 0;
 }
