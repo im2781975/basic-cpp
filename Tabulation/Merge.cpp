2c10e6a216a85be1f0dee1dd63e45b32e0db39cb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-17 09:11:25 +0200

    Update Merge.cpp

--- Tabulation/Merge.cpp
+++ Tabulation/Merge.cpp
@@ -46,6 +46,54 @@ vector<int>devide(vector<int>p)
     vector<int>Sorted_c = devide(c);
     return merged(Sorted_b, Sorted_c);
 }
+vector<int> merge_sort(vector<int> a)
+{
+    if (a.size() <= 1)
+        return a;
+
+    int mid = a.size() / 2;
+    vector<int> b, c;
+    for (int i = 0; i < mid; i++)
+        b.push_back(a[i]);
+
+    for (int i = mid; i < a.size(); i++)
+        c.push_back(a[i]);
+    vector<int> sorted_b = merge_sort(b);
+    vector<int> sorted_c = merge_sort(c);
+
+    vector<int> sorted_a;
+    int idx1 = 0, idx2 = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (idx1 == sorted_b.size())
+        {
+            sorted_a.push_back(sorted_c[idx2]);
+            idx2++;
+        }
+        else if (idx2 == sorted_c.size())
+        {
+            sorted_a.push_back(sorted_b[idx1]);
+            idx1++;
+        }
+        else if (sorted_b[idx1] < sorted_c[idx2])
+        {
+            sorted_a.push_back(sorted_b[idx1]);
+            idx1++;
+        }
+        else
+        {
+            sorted_a.push_back(sorted_c[idx2]);
+            idx2++;
+        }
+    }
+    return sorted_a;
+}
+void print(vector <int> ans)
+{
+    for(int i = 0; i < ans.size(); i++)
+        cout << ans[i] << " ";
+    cout << "\n";
+}
 int main()
 {
     int r, x;
@@ -56,8 +104,8 @@ int main()
         cin >> x;
         p.push_back(x);
     }
-    vector<int>ans = devide(p);
-    for(int i = 0; i<ans.size(); i++)
-        cout << ans[i] << " ";
+    //vector<int>ans = devide(p);
+    vector <int> ans = merge_sort(p);
+    print(ans);
     return 0;
 }
