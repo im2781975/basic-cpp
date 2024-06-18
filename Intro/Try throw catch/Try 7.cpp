0ed286a61237b169ff6a0cf2988b0204c77ec94b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-18 08:58:57 +0200

    Update Try 7.cpp

--- Intro/Try throw catch/Try 7.cpp
+++ Intro/Try throw catch/Try 7.cpp
@@ -28,3 +28,26 @@ int main()
     cout << "Again creating object \n";
     demo(1);
 }
+#include<bits/stdc++.h>
+using namespace std;
+class demo{
+    int num;
+    public:
+    demo(int x){
+        try{
+            if(x == 0)
+                throw "Zero not allowed";
+            num = x;
+            show();
+        }
+        catch(const char *ex){
+            cout << ex;
+        }
+    }
+    void show(){
+        cout << num;
+    }
+};
+int main(){
+    demo(-1);
+}
