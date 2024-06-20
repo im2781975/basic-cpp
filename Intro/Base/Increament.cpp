fb325f76f37cfddd92068a04a0bcefd54446aa17
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-20 12:29:11 +0200

    Update Increament.cpp

--- Intro/Base/Increament.cpp
+++ Intro/Base/Increament.cpp
@@ -14,6 +14,7 @@ int main()
         return 0;
     label2:
         cout << "Odd" << "\n";
+    //increament
     int z=5;
     int x = 5,y=10;
     
@@ -25,6 +26,17 @@ int main()
     
     cout <<"\nlocal access of z is: " << z;
     cout <<"\nGlobal access of z is: " <<::z;
+    //Build in
+    int a, b, c, d;
+    cout << "\nEnter elements: ";
+    cin >> a >> b >> c >> d;
+
+    swap(a, b);
+    cout << "\nafter swap : " << a << " " << b << " " << c << " " << d ;
+    
+    int maximum = max({a, b, c, d});
+    cout << "\nMaximum is: " << maximum ;
+
+    int minimum = min({a, b, c, d});
+    cout << "\nMinimum is: " << minimum ;
 }
-
-
