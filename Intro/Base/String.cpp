b0863d27c8efbb367ab8f84cfade98c11ac9ec7d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-20 12:32:04 +0200

    Update String.cpp

--- Intro/Base/String.cpp
+++ Intro/Base/String.cpp
@@ -53,5 +53,31 @@ int main()
     c.erase(c.begin()+ c.size() -1);
     cout<< "\nAfter erase back string is: " << c;
     c.pop_back()
+    
+// Build 
+int x; 
+    char str[80]; 
+    cout << "Enter a number and string:\n"; 
+    cin >> x; 
+  
+    // clear buffer before taking  new line 
+    cin.ignore();
+    cin.getline(str, 80); 
+    cout << "You have entered:\n"; 
+    cout << x << "\n"; 
+    cout << str << "\n"; 
+    
+    double pi = 3.14159783;
+    cout.precision(5);
+    cout << pi << "\n";
+ 
+    cout.precision(7);
+    cout << pi << "\n";
+    
+    char gfg[]="ibrahim";
+    char ch='e';
+    cout.write(gfg, 6);
+    cout.put(ch);
+    cout<<"\n"<<gfg;
     return 0;
 }
