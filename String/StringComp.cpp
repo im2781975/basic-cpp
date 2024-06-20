d45a70c7d6827fe83f54cac72cbd45e50e563ea5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-20 07:38:06 +0200

    Update StringComp.cpp

--- String/StringComp.cpp
+++ String/StringComp.cpp
@@ -19,5 +19,16 @@ int main()
         cout << "The first 5 characters are same.";
     else
         cout << "The first 5 characters are not equal.";
+   char name[100], add[100], about[100];
+    cout << "\nName is: ";
+    cin.getline(name, 100);
+    cout << "\nAddress is: ";
+    cin.getline(add, 100);
+    cout << "\nDescribe yourself: ";
+    cin.getline(about, 100);
+    
+    cout << "\nName is: " << name;
+    cout << "\nAddress is: " << add;
+    cout << "\nAbout is: " << about ;
     return 0;
 }
