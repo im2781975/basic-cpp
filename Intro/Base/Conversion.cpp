7ad65af6473e58f453a9a29e0cbf917f6db6dc3f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-20 12:34:42 +0200

    Update Conversion.cpp

--- Intro/Base/Conversion.cpp
+++ Intro/Base/Conversion.cpp
@@ -4,11 +4,20 @@ using namespace std;
 int main()
 {
     int number = 100; 
- 
     cout << "Hex Value :" << " " << hex << number;
     cout << "\nOctal Value=" << " " << oct << number ;
  
     cout << "\nSetbase Value=" << " " << setbase(8) << number;
-    cout << "\nSetbase Value=" << " " << setbase(16) << number ;
+    cout << "\nSetbase Value=" << " " << setbase(16) << number;
+    //typeid
+    auto int_=32;
+    auto bool_=true;
+    auto float_=21.89;
+    auto ptr= &float_ ;
+    
+    cout<< typeid(int_).name() <<" \n";
+    cout<<typeid(bool_).name() <<" \n";
+    cout<<typeid(float_).name() <<" \n";
+    cout<<typeid(ptr).name() <<" \n";
     return 0;
 }
