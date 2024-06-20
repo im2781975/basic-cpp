3b756176d588e788d91cce91f4a5ecf4b8bdab95
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-20 12:26:48 +0200

    Update Increament.cpp

--- Intro/Base/Increament.cpp
+++ Intro/Base/Increament.cpp
@@ -1,8 +1,19 @@
-#include <iostream> 
-using namespace std; 
-int z=10;
-int main() 
-{ 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if (n % 2 == 0) 
+        goto label1;
+    else
+        goto label2;
+ 
+    label1:
+        cout << "Even" << "\n";
+        return 0;
+    label2:
+        cout << "Odd" << "\n";
     int z=5;
     int x = 5,y=10;
     
@@ -14,5 +25,6 @@ int main()
     
     cout <<"\nlocal access of z is: " << z;
     cout <<"\nGlobal access of z is: " <<::z;
-    return 0; 
 }
+
+
