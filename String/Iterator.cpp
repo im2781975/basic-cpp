0efb451f37e5a88ab851bda584b51db78eca6d5c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-20 06:15:28 +0200

    Update Iterator.cpp

--- String/Iterator.cpp
+++ String/Iterator.cpp
@@ -34,5 +34,20 @@ int main()
     for(auto it1 = str.crbegin(); it1!=str.crend(); it1++)
         cout<<*it1;
     
+    string::iterator itr;
+    string::reverse_iterator rit;
+    string s = "GeeksforGeeks";
+    
+    itr = s.begin();
+    cout << "Pointing to the start of the string:"<< *itr<<"\n";
+    
+    itr = s.end() - 1;
+    cout << "Pointing to the end of the string: "<<*itr<<"\n";
+    
+    rit = s.rbegin();
+    cout << "Pointing to the last character of the string: " << *rit <<"\n";
+    
+    rit = s.rend() - 1;
+    cout << "Pointing to the first character of the string: " << *rit <<"\n";
     return 0;
 }
