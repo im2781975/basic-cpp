662c38429be0f79471cd3fa70a4bb4b1b102a258
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-20 12:13:09 +0200

    Update String.cpp

--- Intro/Base/String.cpp
+++ Intro/Base/String.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-string Erase_First_Last(string s)
-{
+string Erase_First_Last(string s){
     s.erase(s.begin());
     s.pop_back();
     return s;
 }
-string EraseFirstLast(string &s)
-{
+string EraseFirstLast(string &s){
     s.erase(s.begin());
     s.pop_back();
     return s;
 }
-void Swap(int &x, int &y)
-{
+void Swap(int &x, int &y){
     int z = x;
     x = y;
     y = z;
@@ -34,5 +31,27 @@ int main()
     cin >> a >> b;
     Swap(a, b);
     cout << "\nAfter swap() integers are: " << a << " " << b;
+    
+    string a ="ibrahim";
+    string b ="Molla";
+    cout<<a[0] <<" " <<a[1];
+    string c= a + b;
+    cout<<"\nAfter concatenate string is: " << c ;
+    cout<< "\nSizeof string is: " << a.size();
+    for(int i = 0; i < a.size(); i++)
+        a[i] = a[i] +1;
+        //'a' + 1 = 'b'
+    
+    cout<< "\nAfter add string is: " <<a;
+    a.pop_back();
+    cout<< "\nAfter pop_back string is: " << a ;
+    
+    b.erase(b.begin()+ 0);
+    cout <<"\nAfter erase B is: " << b;
+    c.erase(c.begin());
+    cout <<"\nAfter erase front string is: " << c;
+    c.erase(c.begin()+ c.size() -1);
+    cout<< "\nAfter erase back string is: " << c;
+    c.pop_back()
     return 0;
 }
