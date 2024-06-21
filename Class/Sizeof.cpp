a496a240fdaf2fd1e50cf5345e91d84921dc620c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-21 07:17:25 +0200

    Update Sizeof.cpp

--- Class/Sizeof.cpp
+++ Class/Sizeof.cpp
@@ -1,26 +1,20 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-class cls
-{   
-    int x;   
-}; 
-struct gfg 
-{ 
-  int z; 
-  float d; 
-  char s[20]; 
-}s; 
-union un
-{ 
-    int z; 
-    double d; 
-}u; 
-int main()   
-{   
+#include<bits/stdc++.h>
+using namespace std;
+class cls{
+    int x; int y; double d;
+    char ch[20];
+};
+struct str{
+    int x; float y; double d;
+    char ch[20];
+}s;
+union uni{
+    int x; float y; double d;
+    char ch[20];
+}u;
+int main(){
     cls c;   
     cout <<"Size of class std is in bytes  : " << sizeof(c);
     cout << "\nSize of structure is :" <<  sizeof(s) ;
     cout << "\nSize of union is: "<<sizeof(u) ;
-  return 0;   
 }
-
