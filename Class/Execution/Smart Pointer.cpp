f5accb9f1fdaed9d2c19e10a0879348c8690dbcb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-21 17:55:24 +0200

    Update and rename Class/Pointer<smart> to Class/Execution/Smart Pointer.cpp

--- Class/Execution/Smart Pointer.cpp
+++ Class/Execution/Smart Pointer.cpp
@@ -1,31 +1,26 @@
 // Smart Pointer
 #include <iostream>
 using namespace std;
- 
 class SmartPtr {
     int* ptr; 
     public:
-
     explicit SmartPtr(int* p = NULL) { ptr = p; }
  
        ~SmartPtr() { 
        cout<<"call";
-       delete (ptr); }
- 
+       delete (ptr); 
+    }
     // Overloading dereferencing operator
     int& operator*()
     { return *ptr; }
 };
- 
 int main()
 {
     SmartPtr ptr(new int());
     *ptr = 20;
     cout << *ptr;
- 
     // We don't need to call delete ptr: when the object
     // ptr goes out of scope, the destructor for it is automatically
     // called and destructor does delete ptr.
- 
     return 0;
 }
