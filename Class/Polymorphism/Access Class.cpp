2b4c48f66559dd46a847b5279c573d094b02af25
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-22 22:47:28 +0200

    Update Access Class.cpp

--- Class/Polymorphism/Access Class.cpp
+++ Class/Polymorphism/Access Class.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Test{
+    public:
+    void main(int s){
+        cout << s << "\n";
+    }
+    void main(const char *s){
+        cout << s << "\n";
+    }
+    void main(int m, int s){
+        cout << s << " " << m << "\n";
+    }
+};
 class Animal{
     public:
     string color = "Black";
@@ -17,4 +29,9 @@ int main(){
     cout << x.color << "\n";
     Animal:Dog d;
     cout << d.color << "\n";
+    
+    Test t;
+    t.main(56);
+    t.main("I love c++");
+    t.main(7, 9);
 }
