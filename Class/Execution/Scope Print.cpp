6aed4a461b13c665ceda88fb5f00c91a12a1ecbb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-24 20:46:07 +0200

    Update and rename Print<loop> to Scope Print.cpp

---
+++ Class/Execution/Scope Print.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class base{
+    public:
+    void print(){
+        cout << "Here I am\n";
+    }
+};
+class student{
+    public:
+    static int total;
+    student(){
+        total += 1;
+        
+    }
+};
+int student::total = 0;
+int main(){
+//If we Declare all variable consecutively it will print same value.
+    //For that Have to declare separetly
+    student a;
+    cout << "Number of Student: " << a.total << "\n";
+    student b;
+    cout << "Number of Student: " << b.total << "\n";
+    student c;
+    cout << "Number of Student: " << c.total << "\n";
+    
+    
+    base b;
+    for(int i = 0; i < 5; i++)
+        b.print();
+}
