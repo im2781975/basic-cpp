f68b44e86c50404c63c87b40be5bbcd46e315217
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-25 16:49:22 +0200

    Update Shape.cpp

--- Class/Execution/Shape.cpp
+++ Class/Execution/Shape.cpp
@@ -29,6 +29,47 @@ class circle:public shape{
         return 3.1416 * width * width;
     }
 };
+//area of shapes USING VIRTUAL FUNCTION
+class shape{
+    public:
+    virtual void calculate(){
+        cout << "\nArea of the shape";
+    }
+    // usage of virtual Destuctor to avoid memory leak
+    // Declaring the destructor in the Shape class as virtual ensures that 
+    //the destructors of derived classes (Rectangle and Square) are called correctly.
+     //When an object of a derived class is destroyed, the destructor of the derived class
+     //is called first, followed by the destructor of the base class.
+    virtual ~shape(){
+        cout << "\nshape Destructor Called";
+    }
+};
+class rectangle:public shape{
+    public:
+    int weidth, height, area;
+    void calculate(){
+        cout << "\nEnter Values for Weidth & Height: ";
+        cin >> weidth >> height;
+        int area = weidth * height;
+        cout << "\nArea Of Rectangle is: " << area;
+    }
+    ~rectangle(){
+        cout <<"\nRectangle Destructor called";
+    }
+};
+class Square:public shape{
+    public:
+    int side;
+    void calculate(){
+        cout << "\nEnter Values for side: ";
+        cin >> side;
+        int area = side * side;
+        cout << area;
+    }
+    ~Square(){
+        cout << "\nsquare Destruct called";
+    }
+};
 int main(){
     shape *s;
     Rectangle r(2, 9);
@@ -40,4 +81,13 @@ int main(){
     cout << "Area of Rectangle: " << rect.area() << "\n";
     circle cir(5);
     cout << "Area of Circle: " << cir.area();
+    ////////
+    shape *s;
+    rectangle r;
+    s = &r;
+    s->calculate();
+    
+    Square sq;
+    s = &sq;
+    s->calculate();
 }
