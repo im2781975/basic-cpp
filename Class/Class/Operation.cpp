40a93045397315fe7a03fab74673cbac32caee9e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-25 18:11:12 +0200

    Update Operation.cpp

--- Class/Class/Operation.cpp
+++ Class/Class/Operation.cpp
@@ -1,20 +1,35 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 class A{
-    int a, b;
+    int a, b, c;
     public:
     void add(int x, int y){
         a = x; b = y;
-        cout << "Sum is: " << a + b;
+        cout << "Sum is: " << a + b << "\n";
+    }
+    void add(int x, int y, int z){
+        a = x; b = y; c = z;
+        cout << "Sum is: " << a + b + c << "\n";
+    }
+    virtual void print(){
+        cout << "Class A\n";
     }
 };
 class B:public A{
     public:
-    void print(int x, int y){
+    void print(){
+        cout << "Class B\n";
+    }
+    void sum(int x, int y){
         add(x, y);
     }
 };
 int main(){
+    A a;
+    a.add(6, 8, 9);
+    a.print();
     B b;
-    b.print(9, 7);
+    b.add(2, 7);
+    b.sum(11, 19);
+    b.print();
 }
