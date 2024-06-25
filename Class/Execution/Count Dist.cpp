547cbf7f1982ce52bc1b0f8a031fd022cbd9e1c0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-25 23:30:11 +0200

    Update Count Dist.cpp

--- Class/Execution/Count Dist.cpp
+++ Class/Execution/Count Dist.cpp
@@ -24,9 +24,39 @@ Distance operator+(Distance &d1, Distance &d2){
     }
     return d;
 }
+//Without Friend Func
+class Dist{
+    float feet, inch;
+    public:
+    Dist(){}
+    Dist(float inch, float feet){
+        this->inch = inch;
+        this->feet = feet;
+    }
+    void print(){
+        cout << "\nFeet: " << feet << " Inch: " << inch;
+    }
+    Dist operator +(Dist &d){
+        Dist c;
+        c.feet = this->feet + d.feet;
+        c.inch = this->inch + d.inch;
+        if(c.inch > 12){
+            c.feet += static_cast<int>(c.inch/12);
+            c.inch -= static_cast<int>(c.inch/12)*12;
+        }
+        return c;
+    }
+};
 int main(){
     Distance d1(6.7f, 2.9f);
     Distance d2(5.5f, 11.3f);
     Distance d3 = d1 + d2;
     d3.print();
+    
+    Dist d(3.9, 4.7);
+    Dist e(8.9, 7.9);
+    Dist f = d + e;
+    d.print();
+    e.print();
+    f.print();
 }
