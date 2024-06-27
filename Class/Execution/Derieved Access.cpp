893448e33c9d641ba3609bb5847e50127096a958
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-27 12:57:44 +0200

    Update Derieved Access.cpp

--- Class/Execution/Derieved Access.cpp
+++ Class/Execution/Derieved Access.cpp
@@ -15,6 +15,35 @@ class Derieved:public base{
         cout << "Derieved call\n";
     }
 };
+//Local class
+void func(){
+    class test{
+        public:
+        static void method(){
+            cout << "\nclass method";
+        }
+    };
+    cout << "Yes\n";
+    test::method();
+    test t;
+    t.method();
+}
+//Nested Class
+class parent {
+    int x;
+    public:
+    class child {
+        int y;
+        public:
+        child(int val): y(val) {}
+        int GetY() {
+            return y;
+        }
+    };
+    void fun(child *n) {
+        cout << n->GetY();
+    }
+};
 int main()
 {
     base b;
@@ -24,4 +53,10 @@ int main()
     d.print();
     Derieved::base x;
     x.call();
+    //
+    func();
+    //
+    parent::child c(20);
+    parent b;
+    b.fun(&c);
 }
