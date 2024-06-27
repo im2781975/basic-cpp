37325e793bf47a02333f1524f3da6a7c459d0991
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-27 10:10:58 +0200

    Update MultiHeritance.cpp

--- Class/Execution/MultiHeritance.cpp
+++ Class/Execution/MultiHeritance.cpp
@@ -25,6 +25,29 @@ class C:public A, virtual B{
         cout << "product is: " << t;
     }
 };
+// Hierarchical Inheritance
+class A{
+    public:
+    void showA(){
+        cout << "\nClass A";
+    }
+};
+class B:public A{
+    public:
+    void showB(){
+        cout << "\nClass B";
+    }
+};
+class C:public A{
+    public:
+    void showC(){
+        cout << "\nClass C";
+    }
+};
 int main(){
     C x;
+
+    B b;
+    b.showB();
+    b.showA();
 }
