0d9c500d2da022bfddc429f356625687fb20d965
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-27 13:05:34 +0200

    Update and rename RTTI to RTTI.cpp

--- Class/Class/RTTI.cpp
+++ Class/Class/RTTI.cpp
@@ -1,27 +1,23 @@
 //Run Time Type Identification(RTTI).
 #include <iostream>
 using namespace std;
-
 class B {
 public:
-    virtual void someFunction() {}
+    virtual void Func() {}
 };
-
 class D : public B {};
-
 int main() {
-    // B *b = new D; creates a pointer b to an object of class B but initializes it with an instance of class D. This is possible because of polymorphism; you can point a base class pointer to a derived class object.
+    // creates a pointer b to an object of class B but initializes it 
+    //with an instance of class D. This is possible because of polymorphism;
+    //you can point a base class pointer to a derived class object.
     B* b = new D;
     //to cast the base class pointer b to a derived class pointer d.
     D* d = dynamic_cast<D*>(b);
-    //D*d=dynamic_casr<D*>(d);
     //without virtual
-
     if (d != nullptr)
         cout << "works";
     else
         cout << "cannot cast B*  D*";
-
     delete b; 
     return 0;
 }
