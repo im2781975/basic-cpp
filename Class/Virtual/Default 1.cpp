29589456689f0b4de5991b6741ecbc78cc452303
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-28 15:56:10 +0200

    Update and rename Default arguments to Default 1.cpp

--- Class/Virtual/Default 1.cpp
+++ Class/Virtual/Default 1.cpp
@@ -1,28 +1,23 @@
 // how default arguments & virtual function are used together
 #include <iostream>
 using namespace std;
- 
 class Base {
 public:
-    virtual void fun(int x = 0)
-    {
+    virtual void fun(int x = 0){
         cout << "Base::fun(), x = " << x << endl;
     }
 };
- 
+//default argument will be print as base class
 class Derived : public Base {
-public:
-    virtual void fun(int x = 10) 
-    {
+    public:
+    virtual void fun(int x = 10) {
         cout << "Derived::fun(), x = " << x << endl;
     }
 };
- 
 int main()
 {
     Derived d1; 
     Base* bp = &d1;
- 
     bp->fun(); 
     return 0; 
 }
