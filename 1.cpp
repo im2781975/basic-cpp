4cec8c3810127a6efc8e5f8afa13bd8f282fb158
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-06 21:25:14 +0600

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -240,3 +240,125 @@ void Hanoi(int n, int a, int b, int c){
         Hanoi(n - 1, b, c, a);
     }
 }
+/**/
+using namespace std;
+int Fib(int n){
+    if(n == 1 || n == 2)
+        return 1;
+    int first = 1, sec = 1, fib;
+    for(int i = 2; i <= n; i++){
+        fib = first + sec;
+        first = sec;
+        sec = fib;
+    }
+    return fib;
+}
+/**/
+using namespace std;
+int Fact(int n){
+    int fact = 1;
+    for(int i = 2; i <= n; i++)
+        fact *= i;
+    return fact;
+}
+/**/
+using namespace std;
+int RecursiveMin(int arr[], int len, int l, int r){
+    if(l == r)
+        return arr[l];
+    else{
+        int mid= (l + r)/2;
+        return min(RecursiveMin(arr, len, l, mid - 1), RecursiveMin(arr, len, mid + 1, r) );
+    }
+}
+/**/
+using namespace std;
+int LinearSearch(int arr[], int len, int val){
+    int i = 0;
+    while(i < len){
+        if(arr[i] == val)
+            return i;
+        i++;
+    }
+    return -1;
+}
+/***/
+using namespace std;
+int BinarySearch(int arr[], int l, int r, int val){
+    if(l > r)
+        return -1;
+    else{
+        int mid = (l + r)/2;
+        if(arr[mid] == val)
+            return mid;
+        if(mid == 0)
+            return -1;
+        elif(val < arr[mid])
+            return BinarySearch(arr, l, mid - 1, val);
+        else
+            return BinarySearch(arr, mid + 1,  r, val);
+    }
+}
+using namespace std;
+void Hanoi(int n, int a, int b, int c){
+    if(n == 1)
+        cout << a << " " << c << "\n";
+    else{
+        Hanoi(n - 1, a, b, c);
+        cout << a << " " << c << "\n";
+        Hanoi(n - 1, b, c, a);
+    }
+}
+####
+def linearSearch(arr, val):
+    i = 0
+    while i < len(arr):
+        if arr[i] == val:
+            return i
+        i += 1
+    return -1
+arr = [1, 2, 7, 5, 9]
+val = 5
+if linearSearch(arr, val):
+    print("Yes")
+else:
+    print("No")
+###
+def BinarySearch(arr, l, r, val):
+    if l > r:
+        return -1
+    else:
+        mid = (l + r)//2
+        if arr[mid] == val:
+            return mid
+        elif val < arr[mid]:
+            return BinarySearch(arr, l, mid -1, val)
+        else:
+            return BinarySearch(arr, mid + 1, r, val)
+arr = [2, 3, 4, 7, 6]
+print(BinarySearch(arr, 0, len(arr) - 1, 7))
+###
+def recursiveMin(arr, l, r):
+    if l == r:
+        return arr[l]
+    else:
+        mid = (l + r) //2
+        return min(recursiveMin(arr, l, mid), recursiveMin(arr, mid + 1, r))
+arr = [2, 3, 1, 5, 4]
+print(recursiveMin(arr, 0, len(arr) - 1))
+###
+def Hanoi(n, a, b, c):
+    if n == 1:
+        print(a, c)
+    else:
+        Hanoi(n - 1, a, c, b)
+        print(a, c)
+        Hanoi(n - 1, b, a, c)
+Hanoi(3, 1, 3, 2)
+###
+def Fact(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i;
+    return fact
+print(Fact(5))
