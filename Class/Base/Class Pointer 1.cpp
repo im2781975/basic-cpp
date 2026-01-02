ce4d0fb0ab086595c6aa0ddcdb31f6940e539e1f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-02 11:05:18 +0100

    Update Class Pointer 1.cpp

--- Class/Base/Class Pointer 1.cpp
+++ Class/Base/Class Pointer 1.cpp
@@ -1,31 +1,44 @@
-// unique_ptr
-#include <iostream>
-#include <memory>
+#include<iostream>
+#include<memory>
 using namespace std;
-class A {
-public:
+class A{
+    public:
     void show(){
-        cout << "A::show()\n" ;
+        cout << "A :: show()" << "\n";
     }
 };
-int main()
-{
-    unique_ptr<A> p1(new A);
-    p1->show();
-    // returns the memory address of p1
-    cout << p1.get() << "\n";
-  
-    // transfers ownership to p2
-    unique_ptr<A> p2 = move(p1);
-    p2->show();
-    cout << p1.get() << "\n";
-    cout << p2.get() << "\n";
-  
-    // transfers ownership to p3
-    unique_ptr<A> p3 = move(p2);
-    p3->show();
-    cout << p1.get() << "\n";
-    cout << p2.get() << "\n";
-    cout << p3.get() << "\n";
-    return 0;
+class B{
+    public:
+    void show(){
+        cout << "B :: show()" << "\n";
+    }
+};
+class C{
+    public:
+    void show(){
+        cout << "C :: show()" << "\n";
+        delete this;
+    }
+};
+int main(){
+    // unique_ptr can own an object at a time (no copying allowed).
+    unique_ptr <A> x(new A); x->show();
+    cout << x.get() << "\n"; // returns the memory address of x
+    unique_ptr <A> y = move(x); y->show();
+    cout << y.get() << " " << x.get() << "\n";
+    unique_ptr <A> z = move(y); z->show();
+    cout << z.get() << " " << y.get() << " " << x.get() << "\n";
+    
+    shared_ptr <B> e(new B); e -> show();
+    cout << e.get() << "\n";
+    shared_ptr <B> f(e); f->show();
+    cout << e.get() << " " << f.get() << "\n";
+    cout << e.use_count() << " " << f.use_count() << "\n";
+    e.reset();
+    cout << e.get() << " " << e.use_count() << " " << f.use_count() << "\n";
+    
+    C *ptr = new C; ptr -> show();
+    //ptr = nullptr; // Make ptr nullptr to make sure that it's not used again.
+    ptr -> show();
+    C c; c.show();
 }
