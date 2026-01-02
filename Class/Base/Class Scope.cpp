5fdf7fd545d3c2fd4ee2c178db5f254271a6fb6a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-02 16:17:37 +0100

    Update Class Scope.cpp

--- Class/Base/Class Scope.cpp
+++ Class/Base/Class Scope.cpp
@@ -11,9 +11,31 @@ class Devo{
     Devo(){}
     Devo(string lang, string company):Lang(lang), comp(company){}
 };
+class out {
+    public:    
+    int x;
+    out(int val = 0) { x = val;}
+    void Outer() { cout << x << " " ;}
+    class in {
+        public:   
+        int x; static int y;
+        in(int val = 0) { x = val; }
+        int func() { return x + y; }
+        static void show(){
+            cout << y << endl;
+        }
+    };
+};
+int out :: in :: y = 5;
 int main(){
-    Devo ob;
-    cout << ob.Lang << " " << ob.comp << "\n";
+    out ob(10); ob.Outer();
+    out::in inner(20);
+    cout << "Inner x = " << inner.x << endl;
+    cout << inner.func() << endl;
+    out::in::show();
+    
+    Devo obj;
+    cout << obj.Lang << " " << obj.comp << "\n";
     Devo dv = Devo("Java", "XYQR");
     cout << dv.Lang << " " << dv.comp << " " << dv.name;
 }
