c3d69d11139c5d3d777a05ecf1102ad5b9f6aa1c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-04 22:29:16 +0100

    Update shape.cpp

--- Class/Base/shape.cpp
+++ Class/Base/shape.cpp
@@ -10,24 +10,60 @@ class shape{
     virtual double area() { return len * wid; }
     virtual ~shape(){}
 };
-class square : public shape{
+class box : public shape{
     public:
-    square(int l = 0, int w = 0):shape(l, w){}
+    box(int l = 0, int w = 0):shape(l, w){}
     double area() override{ return len * wid;}
 };
-class rect : public shape{
+class rectangle : public shape{
     public:
-    rect(int l = 0, int w = 0):shape(l, w){}
+    rectangle(int l = 0, int w = 0):shape(l, w){}
     double area() override{ return len * wid;}
 };
-class Triangle : public shape{
+class triangle : public shape{
     public:
-    Triangle(int l = 0, int w = 0):shape(l, w){}
+    triangle(int l = 0, int w = 0):shape(l, w){}
     double area() override{ return 0.5 * (len * wid);}
 };
+class circle {
+    private:    double radius;
+    public:    /*
+    void area(double r) {
+        radius = r;
+        cout << radius << " " << 3.1416 * radius * radius << "\n";
+    } */
+    void getradius() { cin >> radius; }
+    void findarea() { cout << 3.1416 * radius * radius << "\n";}
+};
+class Triangle {
+    private:    int base, height;
+    public:
+    void area(int h, int b) {
+        base = b; height = h;
+        cout << 0.5 * base * height << "\n";
+    }
+};
+class square {
+    private:    int x;
+    public:
+    void area(int a) {
+        x = a; cout << x * x << "\n";
+    }
+};
+class rect{
+    public: double area;
+    rect(): area(0){}
+    rect(int x, int y) { area = x * y; }
+    void disp() { cout << area << "\n";}
+};
 int main(){
-    shape *sp; square sq(2, 9);
-    sp = &sq; cout << sp->area();
-    Triangle tri(6, 9);
-    sp = &tri; cout << sp->area();
+    shape *sp; box sq(2, 9);
+    sp = &sq; cout << sp->area() << "\n";
+    triangle tri(6, 9);
+    sp = &tri; cout << sp->area() << "\n";
+    
+    circle cir; cir.getradius(); cir.findarea(); // cir.area(8.5)
+    Triangle tr; tr.area(8, 9);
+    square sqr; sqr.area(8);
+    rect rt(8, 9); rt.disp();
 }
