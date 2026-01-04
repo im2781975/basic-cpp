94db92233fefb6e74b6a8cfaf58c464db29efe96
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-04 19:45:57 +0100

    Update Derieved.cpp

--- Class/Base/Derieved.cpp
+++ Class/Base/Derieved.cpp
@@ -1,5 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Animal {
+    public:
+    virtual void speak(){ cout << "Animal Speak\n"; }
+};
+class Dog : public Animal {
+    public:
+    void speak() override{ cout << "Dog Speak\n"; }
+};
+class Cat : public Animal {
+    public:
+    void speak() override{ cout << "Cat Speak\n"; }
+};
+class Monk : public Animal {
+    public:
+};
 class A{
     int x;
     public:
@@ -17,4 +32,15 @@ int main(){
     B b; 
     // A a = b.getA();
     A a = B :: getA();
+    
+    Animal *w = new Dog(); w-> speak();
+    Animal *v = new Monk(); v-> speak();
+    Animal *x = new Cat(); x-> speak();
+    Dog *y = dynamic_cast <Dog*> (w);
+    if(y)    y -> speak();
+    else    cout << "Failed To Cast\n";
+    Cat *z = dynamic_cast <Cat*> (x);
+    if(z)    z -> speak();
+    else    cout << "Failed To Cast\n";
+    delete w; delete v; delete x;
 }
