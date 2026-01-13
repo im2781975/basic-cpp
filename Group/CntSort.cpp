dc0690b0841f95927ec2df00ac1a44628fdda48b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-13 18:28:07 +0100

    Update CntSort.cpp

--- Group/CntSort.cpp
+++ Group/CntSort.cpp
@@ -13,6 +13,32 @@ void cntsort(int *arr, int n, int x){
     for(int i = 0; i < n; i++)
         arr[i] = res[i];
 }
+int invmerge(int *arr, int *tmp, int l, int r){
+    int mid, cntinv = 0;
+    if(r > l){
+        mid = l + (l + r) / 2;
+        cntinv += invmerge(arr, tmp, l, mid);
+        cntinv += invmerge(arr, tmp, mid + 1, r);
+        cntinv += merge(arr, tmp, l, mid + 1, r);
+    }
+    return cntinv;
+}
+int merge(int *arr, int *tmp, int l, int mid, int r){
+    int cntinv = 0;
+    int i = l, j = mid, k = l;
+    while((i <= mid - 1) && (j <= r)){
+        if(arr[i] <= arr[j]) tmp[k++] = arr[i++];
+        else {
+            tmp[k++] = arr[j++];
+            cntinv += mid - i;
+        }
+    }
+    while(i <= mid - 1) tmp[k++] = arr[i++];
+    while(j <= right) tmp[k++] = arr[j++];
+    for(int i = l; i <= r; i++)
+        arr[i] = tmp[i];
+    return cntinv;   
+}
 void print(int *arr, int n){
     for(int i = 0; i < n; i++) cout << arr[i] << " ";
     cout << endl;
@@ -20,7 +46,9 @@ void print(int *arr, int n){
 int main(){
     int arr[] = {40, 12, 45, 32, 33, 1, 22};
     int n = sizeof(arr)/sizeof(arr[0]);
-    cntsort(arr, n, 1); 
-    cntsort(arr, n, n);
+    //cntsort(arr, n, 1); 
+    // cntsort(arr, n, n);
+    int tmp[n];
+    Invmerge(arr, tmp, 0, n - 1);
     print(arr, n);
 }
