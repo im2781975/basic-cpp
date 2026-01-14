18cab80be140d0e7d49f3a135187ffb8b9688337
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-15 00:17:47 +0100

    Update MinMax.cpp

--- Group/Execution/MinMax.cpp
+++ Group/Execution/MinMax.cpp
@@ -20,3 +20,31 @@ int main(){
     } 
     cout << tmp[0] << " " << tmp[n - 1] << endl;
 }
+using namespace std;
+int poslt(int *arr, int lt, int rt, int key){
+    while(rt - lt > 1){
+        int mid = lt + (rt - lt) / 2;
+        if(arr[mid] >= key)  rt = mid;
+        else    lt = mid;
+    }
+    return rt;
+}
+int posrt(int *arr, int lt, int rt, int key){
+    while(rt - lt > 1){
+        int mid = lt + (rt - lt) / 2;
+        if(arr[mid] <= key)  lt = mid;
+        else    rt = mid;
+    }
+    return lt;
+}
+int cntfreq(int *arr, int n, int key){
+    int lt = poslt(arr, -1, n - 1, key);
+    int rt = posrt(arr, 0, n - 1, key);
+    return (arr[lt] == key && key == arr[rt]) ? (rt - lt + 1) : 0;
+}
+int main(){
+    int arr[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 5;
+    cout << cntfreq(arr, n, key) << endl;
+}
