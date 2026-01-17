0432680b2e15012631e540c2d8c649d3ec1d56da
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-17 10:03:26 +0100

    Update MinMax.cpp

--- Group/Execution/MinMax.cpp
+++ Group/Execution/MinMax.cpp
@@ -1,11 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
 //find the minimum and maximum values that can be calculated by summing exactly 
 // four of the five integers. print the respective minimum and maximum values
-using namespace std;
-int main(){
-    int n; cin >> n;
-    int arr[n], sum;
-    for(int i = 0; i < n; i++) cin >> arr[i];
-    int tmp[n];
+void maxmin(int *arr, int n){
+    int tmp[n], sum;
     for(int i = 0; i < n; i++){
         sum = 0;
         for(int j = 0; j < n; j++){
@@ -20,7 +18,6 @@ int main(){
     } 
     cout << tmp[0] << " " << tmp[n - 1] << endl;
 }
-using namespace std;
 int poslt(int *arr, int lt, int rt, int key){
     while(rt - lt > 1){
         int mid = lt + (rt - lt) / 2;
@@ -42,9 +39,28 @@ int cntfreq(int *arr, int n, int key){
     int rt = posrt(arr, 0, n - 1, key);
     return (arr[lt] == key && key == arr[rt]) ? (rt - lt + 1) : 0;
 }
+// Deleteval
+int Binsearch(int *arr, int l, int r, int key){
+    if(r < l) return -1;
+    int mid = (l + r) / 2;
+    if(key == arr[mid]) return mid;
+    else if(key > arr[mid]) return  Binsearch(arr, mid + 1, r, key);
+    else return Binsearch(arr, l, mid - 1, key);
+}
+int deleteval(int *arr, int n, int key){
+    int pos = Binsearch(arr, 0, n - 1, key);
+    if(pos == -1) return n;
+    for(int i = pos; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+    return n - 1;
+}
 int main(){
     int arr[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int n = sizeof(arr) / sizeof(arr[0]);
     int key = 5;
     cout << cntfreq(arr, n, key) << endl;
+    maxmin(arr, n);
+    n = deleteval(arr, n, key);
+    for(int i = 0; i < n; i++)
+        cout<< arr[i] << " ";
 }
