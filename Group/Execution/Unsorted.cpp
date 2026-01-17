9ef763fe9529538c3962b6aec8683c01c6955497
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-17 20:26:31 +0100

    Update Unsorted.cpp

--- Group/Execution/Unsorted.cpp
+++ Group/Execution/Unsorted.cpp
@@ -1,7 +1,61 @@
 #include<bits/stdc++.h>
+using namespace std;
+//find the minimum and maximum values that can be calculated by summing exactly 
+// four of the five integers. print the respective minimum and maximum values
+void maxmin(int *arr, int n){
+    int tmp[n], sum;
+    for(int i = 0; i < n; i++){
+        sum = 0;
+        for(int j = 0; j < n; j++){
+            if(i == j) continue;
+            sum += arr[j];
+        }
+        tmp[i] = sum;
+    }
+    int duo = tmp[0];
+    for(int i = 1; i < n; i++){
+        if(duo < tmp[i]) duo = tmp[i];
+    } 
+    cout << tmp[0] << " " << tmp[n - 1] << endl;
+}
+int poslt(int *arr, int lt, int rt, int key){
+    while(rt - lt > 1){
+        int mid = lt + (rt - lt) / 2;
+        if(arr[mid] >= key)  rt = mid;
+        else    lt = mid;
+    }
+    return rt;
+}
+int posrt(int *arr, int lt, int rt, int key){
+    while(rt - lt > 1){
+        int mid = lt + (rt - lt) / 2;
+        if(arr[mid] <= key)  lt = mid;
+        else    rt = mid;
+    }
+    return lt;
+}
+int cntfreq(int *arr, int n, int key){
+    int lt = poslt(arr, -1, n - 1, key);
+    int rt = posrt(arr, 0, n - 1, key);
+    return (arr[lt] == key && key == arr[rt]) ? (rt - lt + 1) : 0;
+}
+// Deleteval
+int Binsearch(int *arr, int l, int r, int key){
+    if(r < l) return -1;
+    int mid = (l + r) / 2;
+    if(key == arr[mid]) return mid;
+    else if(key > arr[mid]) return  Binsearch(arr, mid + 1, r, key);
+    else return Binsearch(arr, l, mid - 1, key);
+}
+int deleteval(int *arr, int n, int key){
+    int pos = Binsearch(arr, 0, n - 1, key);
+    if(pos == -1) return n;
+    for(int i = pos; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+    return n - 1;
+}
 // find the Minimum length Unsorted Subarray, 
 // sorting which makes the complete array sorted
-using namespace std;
 void unsorted(int *arr, int n){
     int l = 0, r = n - 1;
     while(l < n - 1 && arr[l] <= arr[l + 1])    l++;
@@ -66,8 +120,14 @@ void findsurpassar(int *arr, int n){
         cout << (n - 1) - i - mp[arr[i]] << " ";
 }
 int main(){
-    int arr[]{10, 12, 20, 30, 25,40, 32, 31, 35, 50, 60};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 5;
+    cout << cntfreq(arr, n, key) << endl;
+    maxmin(arr, n);
+    n = deleteval(arr, n, key);
+    for(int i = 0; i < n; i++)
+        cout<< arr[i] << " ";
     unsorted(arr, n);
     findsurpassar(arr, n);
 }
