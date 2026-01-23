85d0915b0253bc6699de0320d2be06ba77742ea2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-23 21:26:00 +0600

    Update Nth sort.cpp

--- Group/Execution/Nth sort.cpp
+++ Group/Execution/Nth sort.cpp
@@ -49,6 +49,24 @@ int ksmallest(int arr[], int n, int k) {
     delete[] count;
     return num - 1;
 }
+void nthlargest(int *arr, int n) {
+    priority_queue <int> maxheap;
+    for(int i = 0; i < k; i++) maxheap.push(arr[i]);
+    for (int i = k; i < n; i++) {
+        if (arr[i] > maxheap.top()) {
+            maxheap.pop();
+            maxheap.push(arr[i]);
+        }
+    }
+    vector<int> largestK;
+    while (!maxheap.empty()) {
+        largestK.push_back(maxheap.top());
+        maxheap.pop();
+    }
+    for (int i = k - 1; i >= 0; i--) {
+        cout << largestK[i] << " ";
+    }
+}
 bool cmp(int a, int b){
     return a < b;
 }
