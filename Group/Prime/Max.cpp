5e0f2c78a47101038606a1620290daabd74fe44f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-24 12:52:47 +0600

    Update Max.cpp

--- Group/Prime/Max.cpp
+++ Group/Prime/Max.cpp
@@ -30,3 +30,39 @@ void sumrange(vector <int> vec, int n) {
         cout << pref[r] - pref[l - 1] << endl;
     }
 }
+using namespace std;
+void func(int n) {
+    if (n == 1) cout << "One";
+    else if (n == 2) cout << "Two";
+    else if (n == 3) cout << "Three";
+    else if (n == 4) cout << "Four";
+    else if (n == 5) cout << "Five";
+    else if (n == 6) cout << "Six";
+    else if (n == 7) cout << "Seven";
+    else if (n == 8) cout << "Eight";
+    else cout << "Nine";
+}
+void tion(int n) {
+    if (n == 1) cout << "Ten";
+    else if (n == 2) cout << "Twenty";
+    else if (n == 3) cout << "Thirty";
+    else if (n == 4) cout << "Forty";
+    else if (n == 5) cout << "Fifty";
+    else if (n == 6) cout << "Sixty";
+    else if (n == 7) cout << "Seventy";
+    else if (n == 8) cout << "Eighty";
+    else cout << "Nine";
+}
+int main() {
+    vector <int> vec;
+    int n; cin >> n;
+    while(n != 0) {
+        int x = n % 10; n /= 10;
+        vec.push_back(x);
+    }
+    for(int i = vec.size() - 1; i >= 0; i--) {
+        if(vec[i] == 0 && i == 0) cout << "zero";
+        else if(i == 0) func(vec[i]);
+        else if(i == 1) tion(vec[i]);
+    }
+}
