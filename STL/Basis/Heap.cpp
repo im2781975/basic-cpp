09c44719708e5c58b3d9793bcc38b35a91b67ce0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-24 22:13:01 +0600

    Update Heap.cpp

--- STL/Basis/Heap.cpp
+++ STL/Basis/Heap.cpp
@@ -121,3 +121,38 @@ int main( ) {
       cout << *Itera1 << " "; 
    cout << ")" << endl; 
 } 
+// min heap for priority queue
+#include <iostream>
+#include <queue>
+using namespace std;
+  
+void showpq(
+    priority_queue<int, vector<int>, greater<int> > g)
+{
+    while (!g.empty()) {
+        cout << ' ' << g.top();
+        g.pop();
+    }
+}
+void showArray(int* arr, int n)
+{
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+}
+int main()
+{
+    int arr[6]={10, 2, 4, 8, 6, 9};
+    priority_queue<int, vector<int>, greater<int> > gquiz(
+        arr, arr + 6);
+    //int: The data type of elements in the priority q.
+//vector<int>:underlying container used for the priority queue 
+//greater<int>: The comparison function or functor that defines the order of elements.
+    cout << "\nArray: ";
+      showArray(arr, 6);
+  
+    cout << "\nPriority Queue : ";
+    showpq(gquiz);
+  
+    return 0;
+}
