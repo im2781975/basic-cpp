cff9d589fa9c15fb96a330f5e70742db13873671
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-24 20:54:41 +0600

    Update Merge 1.cpp

--- Group/Tabu/Merge 1.cpp
+++ Group/Tabu/Merge 1.cpp
@@ -1,5 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std; 
+void merge() {
+    int n, x, m, flag; cin >> n;
+    vector<int> a, b, k;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        a.push_back(x);
+        k.push_back(x);
+    }
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> x; b.push_back(x);
+    }
+    for (int i = 0; i < m; i++) {
+        flag = 0;
+        for (int j = 0; j < n; j++) {
+            if (b[i] == a[j]) flag++;
+        }
+        if (flag == 0) 
+            k.push_back(b[i]);
+    }
+    for (int i = 0; i < k.size(); i++)
+        cout << k[i] << " ";
+    return 0;
+}
 void rotate(int *arr, int n, int idx){
     for (int i = 0; i < idx / 2; i++)
         swap(arr[i], arr[idx - 1 - i]);
