4160d847588364f186fb6877b2ba82d7ab6c8e63
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-24 18:05:59 +0600

    Update For each.cpp

--- Intro/Loop/For each.cpp
+++ Intro/Loop/For each.cpp
@@ -10,11 +10,5 @@ int main() {
     for (int v : value) {
         cout << v << " ";
     }
-
-    vector<int> vec{0, 1, 2, 3, 4};
-    for_each(vec.begin(), vec.end(), [](int &i){ i += 2; });
-    
-    for_each(vec.begin(), vec.end(), [](int i){ cout << i << " "; });
-
     return 0;
 }
