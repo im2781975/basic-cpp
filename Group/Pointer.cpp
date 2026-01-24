8a586ee8f10a6f419b219fb187c184c09d3d8418
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-24 15:07:19 +0600

    Add pointer and array manipulation examples

--- Group/Pointer.cpp
+++ Group/Pointer.cpp
@@ -28,4 +28,36 @@ int main(){
     int *tr = func();
     for (int i = 0; i < 5; i++) cout << *tr++ << " ";
     cout << endl;
+    
+    int n = 5;
+    int *grp = new int[n];
+    for(int i = 0; i < n; i++) cin >> grp[i];
+    cout << grp << endl;
+    for(int i = 0; i < n; i++) cout << grp[i] << " ";
+    cout << endl;
+    cout << grp << endl;
+    delete []grp;
+    
+    int *p, *ter[5];
+    int ray[5] = {10, 20, 30, 40, 50};
+    p = ray;
+    for(int i = 0; i < 5; ++i) ter[i] = &ray[i];
+    for(int i = 0; i < 5; ++i) {
+        cout << p << " " << ter[i] << endl;
+        p++;
+    }
+    cout << sizeof(p) << " " << sizeof(*ter) << " " << sizeof(*p) << " " << sizeof(ter);
+    
+    int rr[3][4] = { {10, 11, 12, 13}, {20, 21, 22, 23}, {30, 31, 32, 33} };
+    int (*pr)[4]; pr = rr;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 4; j++) {
+            cout << *(*(pr + i) + j) << " ";
+        }
+        cout << endl;
+    }
+    for(int i = 0; i < 4; i++) cout << pr + i << " " << *(pr + i) << endl;
+    // first value of row
+    cout << **pr << " " << **(pr + 1) << **(pr + 2) << endl;
+    cout << pr[0][0] << " " << pr[1][1] << " " << pr[2][2] << endl;
 }
