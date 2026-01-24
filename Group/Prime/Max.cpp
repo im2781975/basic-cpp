24b491f5278a348dc6010f9178681516c133e663
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-24 20:02:10 +0600

    Update Max.cpp

--- Group/Prime/Max.cpp
+++ Group/Prime/Max.cpp
@@ -7,6 +7,13 @@ int findMax(int arr[], int n) {
         return maxInRest > arr[size - 1] ? maxInRest : arr[size - 1];
     }
 }
+int getavg(int *arr, int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+        sum += arr[i];
+    float avg = sum / n;
+    return avg;
+}
 // Odd occuring element
 int findOdd(int arr[], int n){
     int res = 0, i;
@@ -29,6 +36,17 @@ void sumrange(vector <int> vec, int n) {
         int l, r; cin >> l >> r;
         cout << pref[r] - pref[l - 1] << endl;
     }
+}
+using namespace std;
+//how much coin need for complete n
+vector <int> coins {3, 4, 5};
+int coin_change(int n) {
+    int mini = *min_element(coins.begin(), coins.end());
+    if(n < mini) return 0;
+    int ans = 10000;
+    for(int i = 0; i < coins.size() -1 ; i++)
+        ans = min(ans, 1 + coin_change(n - coins[i]));
+    return ans;
 }
 using namespace std;
 void func(int n) {
@@ -64,5 +82,24 @@ int main() {
         if(vec[i] == 0 && i == 0) cout << "zero";
         else if(i == 0) func(vec[i]);
         else if(i == 1) tion(vec[i]);
+    }
+}
+// sorting basis of a column & size
+using namespace std;
+bool sortcol(const vector <int> &a, const vector <int> &b) {
+    return a[1] < b[1];
+}
+bool sizecom(const vector <int> &a, const vector <int> &b) {
+    return a.size() < b.size();
+}
+int main() {
+    vector <vector <int>> vec {{ 3, 5, 1 },{ 4, 8, 6 },{ 7, 2, 9}};
+    sort(vec[0].rbegin(), vec[0].rend());
+    //sort(vect[0].begin(), vect[0].end());
+    sort(vec.begin(), vec.end(), sizecom); // sizecol
+    for(int i = 0; i < vec.size(); i++){
+        for(int j = 0; j < vec[i].size(); j++)
+            cout << vec[i][j] << " ";
+        cout << endl;
     }
 }
