5a5415b601e0013d360da5696c5328095af906dd
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-09 11:12:05 +0600

    Create Arr.cpp

---
+++ Group/Arr.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    typedef int arr[3];
+    arr array {1,2,3};
+    cout << "Array output: "<<"\n";
+    
+    for (int i = 0; i < 3; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+//----------------------------------
+    arr matrix[3];
+    cout <<"Matrix output: "<<"\n";
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix[i][j] = i * j;
+            }
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
