8b017bd9adb15890c2abd69403e0b8197bd42299
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-11 11:38:58 +0600

    Update AdjList.cpp

--- Algo/Key/AdjList.cpp
+++ Algo/Key/AdjList.cpp
@@ -24,3 +24,32 @@ vector <vector <int>> ListTomatrix(vector <vector <int>> adjList) {
     }
     return matrix;
 }
+void grid(int n) {
+    vector <vector <int>> vec(n, vector <int>(n));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) vec[i][j] = i + j;
+    }
+    vector <int> tor[n];
+    tor[1].push_back(12); tor[1].push_back(14);
+    tor[1].push_back(16); tor[2].push_back(18);
+    tor[2].push_back(20); tor[3].push_back(22);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < tor[i].size(); j++)
+            cout << tor[i][j] << " ";
+        cout << endl;
+    }
+}
+void graph() {
+    int nd, edg; cin >> nd >> edg;
+    vector <int> adj[nd + 1];
+    for(int i = 0; i < edg; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    for(int i = 0; i < nd; i++) {
+        for(int j = 0; j < adj[i].size(); j++)
+            cout << adj[i][j] << " ";
+        cout << endl;
+    }
+}
