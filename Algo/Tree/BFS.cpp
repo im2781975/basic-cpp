3c6d95a5a66e4f2e637f5467923a546948f58ffb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-11 18:42:17 +0600

    Update BFS.cpp

--- Algo/Tree/BFS.cpp
+++ Algo/Tree/BFS.cpp
@@ -85,3 +85,118 @@ int main() {
     int start = 0;
     BFS(start, graph);
 }
+#define x 100
+int graph[x][x]; bool vis[x];
+void path(int parent[], int start, int end) {
+    if(start == end) {
+        cout << start << " "; return;
+    }
+    path(parent, start, parent[end]);
+}
+void BFS(int start, int end, int node) {
+    queue <int> q;  bool flag = false;
+    int parent[x];
+    q.push(start);
+    for(int i = 0; i < node; i++) {
+        parent[i] = -1; vis[i] = false;
+    }
+    vis[start] = true; q.push(start);
+    while(!q.empty()) {
+        int u = q.front(); q.pop();
+        for(int i = 0; i < node; i++) {
+            if(graph[u][i] == 1 && !vis[i]) {
+                vis[i] = true; parent[i] = u;
+                q.push(i);
+                if(i == end) {
+                    flag = true; break;
+                }
+            }
+        }
+        if(flag) break;
+    }
+    if(found) cout << path(parent, start, end);
+    else cout << "No path Exits";
+}
+void BfS(vector <int> adj[], vector <int> &vis, int src, int *lev) {
+    queue <int> q; q.push(src);
+    vis[src] = 1; lev[src] = 0;
+    while(!q.empty()) {
+        int u = q.front(); q.pop();
+        cout << u << " ";
+        for(int v : adj[u]) {
+            if(vis[v] == 0) {
+                vis[v] = 1; q.push(v);
+                lev[v] = lev[u] + 1;
+            }
+        }
+    }
+}
+vector <vector <pair <int, int>>> graph;
+void Join(int u, int v, int w) {
+    graph[u].push_back(make_pair(v, w));
+    graph[v].push_back(make_pair(u, w));
+}
+void BFs(int src, int trg, int node) {
+    vector <bool> vis(node, false);
+    priority_queue <pair <int, int>, vector <pair <int, int>>, greater <pair <int, int>>> pq;
+    pq.push(make_pair(0, src));
+    int u = src; vis[u] = true;
+    while(!pq.empty()) {
+        int v = pq.top().second;
+        cout << v << " "; pq.pop();
+        if(v == trg) break;
+        for(int i = 0; i < graph[v].size(); i++){
+            if(!vis[graph[v][i].second]){
+                vis[graph[v][i].second] = true;
+                pq.push(make_pair(graph[v][i].first, graph[v][i].second));
+            }
+        }
+    }
+}
+int main(){
+    int node, edge; cin >> node >> edge;
+    vector <int> visited(node, 0);
+    vector <int> adj[node], int lev[node];
+    for(int i = 0; i < edge; i++){
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    BfS(adj, visited, 0, lev);
+    for(int i = 0; i < node; i++) cout << i << " " << lev[i] << endl;
+    graph.resize(node);
+    Join(0, 1, 3);    Join(0, 2, 6);
+    Join(0, 3, 5);    Join(1, 4, 9);
+    Join(1, 5, 8);
+    int src = 0, trg = 9;
+    BFs(src, trg, node);
+}
+const int maxn = 1005;
+vector <int> adj[maxn];
+int dist[maxn];
+void BFS(int src){
+    queue <int> q;
+    q.push(src); dist[src] = 0;
+    while(!q.empty()){
+        int u = q.front(); q.pop();
+        for(int v : adj[u]){
+            if(dist[v] == -1){
+                dist[v] = dist[u] + 1;
+                q.push(v);
+            }
+        }
+    }
+}
+int main(){
+    int node, edge; cin >> node >> edge;
+    for(int i = 0; i < edge; i++){
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    memset(dist, -1, sizeof(dist));
+    int src; cin >> src;
+    BFS(src);
+    for(int i = 1; i <= node; i++)
+        cout << dist[i] << " ";
+}
