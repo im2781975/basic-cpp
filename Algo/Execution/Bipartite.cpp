3f6da80017442560d2c5d0536b85e378dc5d4e58
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-12 19:42:54 +0600

    Update Bipartite.cpp

--- Algo/Execution/Bipartite.cpp
+++ Algo/Execution/Bipartite.cpp
@@ -1,3 +1,83 @@
+//Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<vector<int>> constructadj(int V, vector<vector<int>> &edges){
+    
+    vector<vector<int>> adj(V);
+    for(auto it:edges){
+        adj[it[0]].push_back(it[1]);
+        adj[it[1]].push_back(it[0]);
+    }
+    return adj;
+}
+
+//Driver Code Ends
+// Helper function for DFS to check bipartite graph
+bool dfs(int u, int color, vector<int> &colors,
+                           vector<vector<int>> &adj) {
+
+    // Assign color to the current u
+    colors[u] = color; 
+
+    // Iterate through all adjacent vertices
+    for(auto &v : adj[u]) {
+        if(colors[v] == -1) {
+
+            // Assign alternate color to the adjacent u
+            if(!dfs(v, 1 - color, colors, adj))
+                return false;
+        }
+        else if(colors[v] == color) {
+          
+            // If the adjacent u has the same color,
+            // it's not bipartite
+            return false;
+        }
+    }
+    return true;
+}
+
+// Function to check if the graph is Bipartite using DFS
+bool isBipartite(int V, vector<vector<int>> &edges) {
+
+    // Initialize all vertices as uncolored
+    vector<int> colors(V, -1); 
+    
+    // create adjacency list
+    vector<vector<int>> adj = constructadj(V,edges);
+
+    // Check each component of the graph
+    for(int i = 0; i < V; i++) {
+
+        // If the vertex is uncolored
+        if(colors[i] == -1) { 
+
+            // If DFS fails, the graph is not bipartite
+            if(!dfs(i, 0, colors, adj))
+                return false; 
+        }
+    }
+
+    // All vertices can be colored bipartitely
+    return true;
+}
+//Driver Code Starts
+
+int main() {
+    
+    int V = 4;
+    vector<vector<int>> edges = {{0, 1}, {0, 2}, {1, 2}, {2, 3}};
+    if(isBipartite(V, edges))
+        cout << "true";
+    else
+        cout << "false";
+    
+    return 0;
+}
+
+//Driver Code Ends
 #include <bits/stdc++.h>
 using namespace std;
 
