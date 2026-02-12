03ffe14fa2cb27a19fdc3170fa2dfa32a0e1eff9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-12 17:13:44 +0600

    Update Cycle.cpp

--- Algo/Execution/Cycle.cpp
+++ Algo/Execution/Cycle.cpp
@@ -199,4 +199,25 @@ int main(){
     }
     cout << (CycleExists ? "Cycle Exists" : "No Cycle") << endl; 
 }
+using namespace std;
+vector <int> parent;
+int find(int x) {
+    return parent[x] == -1 ? x : parent[x] = find(parent[x]); 
+}
+void union_set(int a, int b) {
+    parent[find(a)] = find(b);
+}
+int main() {
+    int n, m; cin >> n >> m;
+    parent.assign(n, -1);
+    bool hasCycle = false;
+    for(int i = 0; i < m; i++) {
+        int u, v; cin >> u >> v;
+        if(find(u) == find(v)) {
+            hasCycle = true; break;
+        }
+        union_set(u, v);
+    }
+    cout << (has_Cycle ? "Yes" : "No");
+}
 
