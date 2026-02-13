256f6bf6de7c2517e1724e372549ad6bb1a3d210
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-13 12:40:22 +0600

    Update RobHouse.cpp

--- Algo/Execution/RobHouse.cpp
+++ Algo/Execution/RobHouse.cpp
@@ -154,6 +154,24 @@ int countPaths(int n, vector<vector<int>>& trap) {
         }
     } */
 }
+// Min path sum from top-left to bottom-right (right/down moves only)
+int minPathsum(vector <vector <int>> grid) {
+    int n = grid.size();
+    int m = grid[0].size();
+    vector <vector <int>> dp(n, vector <int> (m, 0));
+    dp[0][0] = grid[0][0];
+    for (int i = 1; i < m; i++) 
+        dp[0][i] = dp[0][i - 1] + grid[0][i];
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = dp[i - 1][0] + grid[i][0];
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        }
+    }
+    return dp[n - 1][m - 1];
+}
 // Max money from circular houses without robbing adjacent ones (first+last adjacent).
 int robRange(vector<int>& nums, int start, int end) {
     int prev1 = 0;  
