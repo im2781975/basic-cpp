5df8ae0a2e084456de06d51e8710373f115cda98
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-13 11:46:14 +0600

    Update Permutation.cpp

--- Algo/Execution/Permutation.cpp
+++ Algo/Execution/Permutation.cpp
@@ -129,3 +129,40 @@ string rev(string str) {
     string res = str.substr(1);
     return rev(res) + ch;
 }
+string LCS() {
+    string str, ing; cin >> str >> ing;
+    int n = str.size(), m = ing.size();
+    vector <vector <int>> dp(n + 1, vector <int>(m + 1, 0));
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            if(str[i - 1] == ing[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    string res;
+    for(int i = n, j = m; i && j;) {
+        if(str[--i] == ing[--j]) res.push_back(str[i]);
+        else if(dp[i][j - 1] > dp[i - 1][j]) j--;
+        else i--;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+// find the longest palindromic subsequence's length in str
+int LPS(string str) {
+    int n = str.size();
+    vector <vector <int>> dp(n, vector <int> (n));
+    for(int len = 1; len <= n; len++) {
+        for(int i = 0; i + len <= n; i++) {
+            int j = i + len - 1;
+            if(i == j) dp[i][j] = 1;
+            else if(str[i] == str[j])
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            else dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[0][n - 1];
+}
