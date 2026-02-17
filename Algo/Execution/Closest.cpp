5753c4d696a193db52c11d859e5f808c28f188d2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-17 12:58:30 +0600

    Update Closest.cpp

--- Algo/Execution/Closest.cpp
+++ Algo/Execution/Closest.cpp
@@ -172,6 +172,49 @@ int cntOnes(bool arr[], int low, int high) {
     }
     return 0;
 }
+//pair sum
+//Function to return largest pair sum. Assumes that there are at-least two elements in arr[] 
+int findLargestSumPair(int arr[], int n)
+{
+    int maxSum = INT_MIN;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int sum = arr[i] + arr[j];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+    }
+    return maxSum;
+}
+// return largest pair sum. Assumes that there are at-least  two elements in arr[] 
+int findLargestSumPair(int arr[], int n)
+{
+    // Initialize first and second largest element
+    int first, second;
+    if (arr[0] > arr[1]) {
+        first = arr[0];
+        second = arr[1];
+    }
+    else {
+        first = arr[1];
+        second = arr[0];
+    }
+ 
+    // Traverse remaining array and find first and second largest elements in overall array
+    for (int i = 2; i < n; i++) {
+        /* If current element is greater than first then
+          update both first and second */
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        }
+        //If arr[i] is in between first and second then update second  
+        else if (arr[i] > second && arr[i] != first)
+            second = arr[i];
+    }
+    return (first + second);
+}
 int countOnes(bool arr[], int n) {
     /* auto ptr = upper_bound(arr, arr + n, 1, greater <int> ());
     cout << (ptr - arr); */
