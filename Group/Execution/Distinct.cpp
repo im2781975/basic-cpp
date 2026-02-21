8b2f85608bb14f07dc1c3ebe486f8d4c7fbd5eda
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-21 14:32:42 +0600

    Update Distinct.cpp

--- Group/Execution/Distinct.cpp
+++ Group/Execution/Distinct.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std; /*
 vector <int> distinct(vector <int> vec) {
     vector <int> unq, dup;
     for(int i = 0; i < vec.size(); i++) {
@@ -7,25 +7,24 @@ vector <int> distinct(vector <int> vec) {
         else dup.push_back(vec[i]);
     }
     return unq;
-}
+} */
 void distinct(int *arr, int n) {
-    for(int i = 0; i < n; i++){
-        int j
-        for(j = 0; j < i; j++) {
+    for(int i = 0; i < n; i++) {
+        int j;
+        for(int j = 0; j < i; j++) {
             if(arr[i] == arr[j]) break;
         }
-        if(i == j) cout << arr[i] << " ;"
-    }/*
+        if(i == j) cout << arr[i] << " ";
+    } /*
     sort(arr, arr + n);
     for(int i = 0; i < n; i++) {
         while(i < n - 1 && arr[i] == arr[i + 1]) i++;
         cout << arr[i] << " ";
-    } *//*
+    }
     unordered_set <int> st;
     for(int i = 0; i < n; i++) {
         if(!st.count(arr[i])) {
             st.insert(arr[i]); cout << arr[i] << " ";
-        }
     } */
 }
 void printdup(int *arr, int n) {
@@ -38,47 +37,53 @@ void printdup(int *arr, int n) {
     for(it = dup.begin(); it != dup.end(); ++it) cout << *it << " ";
     cout << endl;
 }
-void firstdup(vector <int> vec) {
+void firstdup(vector <int> arr) {
     map <int, int> mp;
-    for(int i = 0; i < vec.size(); ++i) {
-        if(mp.find(vec[i]) != mp.end()) {
-            cout << vec[i] << " " << mp[vec[i]] << endl;
-            break;
+    for(int i = 0; i < arr.size(); ++i) {
+        if(mp.find(arr[i]) != mp.end()) {
+            cout << arr[i] << " " << mp[arr[i]] << endl; break;
         }
-        mp[vec[i]] = i;
+        mp[arr[i]] = i;
     }
 }
 int dig(int n) {
     int cnt = 0;
-    while(n != 0) { n /= 10; cnt++; }
+    while(n != 0) {
+        n /= 10; cnt++;
+    }
     return cnt;
 }
-void minopr(vector <int> vec, vector <int> tor) {
-    int n = vec.size();
+int func(vector <int> vec, vector <int> tor) {
     priority_queue <int> x, y;
-    for(int i = 0; i < n; i++) x.push(vec[i]);
-    for(int i = 0; i < n; i++) y.push(tor[i]);
+    for(int i = 0; i < vec.size(); i++) x.push(vec[i]);
+    for(int i = 0; i < tor.size(); i++) y.push(tor[i]);
     int res = 0;
     while(x.size() && y.size()) {
         int a = x.top(), b = y.top();
-        if (a == b) { x.pop(); y.pop(); continue; }
+        if(a == b) {
+            x.pop(); y.pop(); continue;
+        }
         ++res;
-        if(a > b) { x.pop(); x.push(dig(a)); }
-        else { y.pop(); y.push(dig(b)); }
+        if(a > b) {
+            x.pop(); x.push(dig(a));
+        }
+        else {
+            y.pop(); y.push(dig(b));
+        }
     }
     cout << res;
 }
-// Find the smallest signal range needed so every house is within range of at least one transmitter placed on a house.
-void smallrange(vector <int> vec, int n) {
+// Find the smallest signal range needed so every house is within range of at least one transmitter placed on a house
+void smallrange(vector <int> arr, int n) {
     bool flag = true; int tmp = INT_MAX;
-    for(int i = n; i < n; ++i) {
+    for(int i = 1; i < n; i++) {
         if(arr[i] < arr[i - 1]) {
             flag = false; break;
         }
     }
     if(flag) {
-        for(int i = 1; i < n; ++i)
-            tmp = min(tmp, vec[i] - vec[i - 1])
+        for(int i = 1; i < n; ++i) 
+            tmp = min(tmp, vec[i] - vec[i - 1]);
         cout << (tmp / 2) + 1;
     }
     cout << 0;
@@ -87,49 +92,41 @@ void pattern(vector <int> vec, int n) {
     int l, r; cin >> l >> r;
     if(l >= 1 && r <= vec.size() && l <= r)
         vec.erase(vec.begin() + l - 1, vec.begin() + r);
-    int sum = 0;
+    int res = 0;
     while(n > 0) {
         sum += n; vec.push_back(sum); n /= 2;
     }
-    for(int i = 0; i < vec.size(); i++) cout << vec[i] << " ";
+    for(int i = 0; i < vec.size(); ++i) cout << vec[i] << " ";
 }
-void cntcoin(vector <int> vec, int n, int x) {
-    vector <int> cnt(x + 1, INT_MAX);
-    cnt[0] = 0;
-    for(int i = 0; i <= x; i++){
-        for(int j = 0; j < coin.size(); j++) {
-            if(i - coin[j] >= 0) cnt[i] = min(cnt[i], cnt[i - cnt[j]] + 1);
+void cntcoin(vector <int> coin, int n, int amt) {
+    vector <int> cnt(amt + 1, 0); cnt[0] = 0;
+    for(int i = 0; i <= amt; i++) {
+        for(int j = 0; j <= n; j++) {
+            if(i - coin[j] >= 0) 
+                cnt[i] = min(cnt[i], cnt[i - coin[j]] + 1);
         }
     }
-    cout << cnt[x];
+    cout << cnt[amt];
 }
-// find equilibrium index of an array
 int equilibrium(int *arr, int n) {
-    int left = 0, right = 0;
-    for(int i = 0; i < n; ++i) {
-        for(int j = 0; j < i; j++) left += arr[j];
-        for(int j = i; j < n; j++) right += arr[j];
-        if(left == right) return i;
+    int left = 0, total = 0;
+    for(int i = 0; i < n; i++) total += arr[i];
+    for(int i = 0; i < n; i++) {
+        total -= arr[i];
+        if(left == total) return i;
+        left += arr[i];
     }
     return -1;
 }
-int Equilibrium(int *arr, int n) {
-    int res = INT_MIN;
-    for(int i = 0; i < n; i++) {
-        int prefix = arr[i]; int suffix = arr[i];
-        for(int j = 0; j < i; j++) prefix += arr[j];
-        for(int j = n - 1; j > 0; j--) suffix += arr[j];
-        if(suffix == prefix) res = max(res, prefix);
-    }
-    return res;
-}
 int equilibrium(int *arr, int n) {
-    int sum = 0, left = 0;
-    for(int i = 0; i < n; i++) sum += arr[i];
+    vector <int> suffix(n + 1);
+    suffix[n - 1] = arr[n - 1];
+    for(int i = n - 2; i >= 0; i--) suffix[i] = suffix[i + 1] + arr[i];
     for(int i = 0; i < n; i++) {
-        sum -= arr[i];
-        if(left == sum) return i;
-        left += arr[i];
+        int prefix = 0;
+        for(int j = 0; j < i; j++) prefix += arr[j];
+        int right = suffix[i + 1];
+        if(prefix == right) return i;
     }
     return -1;
 }
@@ -145,17 +142,16 @@ int equilibrium(int *arr, int n) {
     }
     return -1;
 }
-int equilibrium(vector <int> vec) {
-    int left = 0, right = 0, pivot = 0;
-    for(int i = 1; i < vec.size(); i++) right += vec[i];
-    while(pivot < vec.size() - 1 && right != left) {
-        pivot++; right -= vec[pivot];
-        left += vec[pivot];
+int equilibrium(vector <int> arr) {
+    int left = 0, right = 0, piv = 0;
+    for(int i = 1; i < n; i++) right += arr[i];
+    while(piv < arr.size() - 1 && right != left) {
+        piv++; left += arr[piv - 1];
+        right += arr[piv];
     }
-    return (left == right) ? pivot : -1;
+    return (left == right) ? piv : -1;
 }
-// Function to find maximum equilibrium sum
-void maxsum(int *arr, int n) {
+void equilibrium(int *arr, int n) {
 	int prefix[n], suffix[n];
     int res = INT_MIN; prefix[0] = arr[0];
     for(int i = 1; i < n; i++) prefix[i] = prefix[i - 1] + arr[i];
@@ -167,10 +163,10 @@ void maxsum(int *arr, int n) {
     }
     return res;
 }
-int maxsum(int *arr, int n){
+int maxsum(int *arr, int n) {
     int sum = accumulate(arr, arr + n, 0);
     int prefix = 0, res = INT_MIN;
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
         prefix += arr[i];
         if(prefix == sum) res = max(res, prefix);
         sum -= arr[i];
@@ -181,63 +177,53 @@ int maxsum(int *arr, int n){
 int maxdiff(int *arr, int n) {
     unordered_map <int, int> freq;
     for(int i = 0; i < n; i++) freq[arr[i]]++;
-    int res = 0;
+    int res = INT_MIN;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(freq[arr[i]] > freq[arr[j]] && arr[i] > arr[j])
-                res = max(res, freq[arr[i]] - freq[arr[j]])
-            else if(freq[arr[i]] < freq[arr[j]] && arr[i] < arr[j])
-                res = max(res, freq[arr[j]] - freq[arr[i]])
+                res = max(res, freq[arr[i]] - freq[arr[j]]);
+            else if(freq[arr[j]] > freq[arr[i]] && arr[j] > arr[i])
+                res = max(res, freq[arr[j]] - freq[arr[i]]);
         }
     }
     return res;
 }
 int maxdiff(int *arr, int n) {
-    unordered_map <int, int> freq;
-    int dist[n], j = 0;
+    unordered_map <int, int> mp;
+    int tmp[n], j = 0;
     for(int i = 0; i < n; i++) {
-        if(freq.find(arr[i]) == freq.end()) dist[j++] = arr[i];
+        if(freq.find(arr[i]) == freq.end()) tmp[j++] = arr[i];
         freq[arr[i]]++;
     }
-    sort(dist, dist + j);
+    sort(tmp, tmp + j);
     int mini = n + 1, res = 0;
-    for(int i = 0; i < j; i++) {
-        int cur = freq[dist[i]];
+    for(int i = 0; i < n; ++i) {
+        int cur = freq[tmp[i]];
         res = max(res, cur - mini);
-        mini = min(cur, mini);
+        mini = min(mini, cur);
     }
     return res;
 }
 // Returns min diff between max & min values of distribution n choco in m students
 int minDiff(int *arr, int n, int m) {
     if(m == 0 || n == 0) return 0;
-    sort(arr, arr + n);
-    if(n < m) return -1;
+    sort(arr, arr + n); if(n < m) return -1;
     int mini = INT_MAX, maxi = INT_MIN;
     for(int i = 0; i + m - 1 < n; i++) {
         int diff = arr[i + m - 1] - arr[i];
-        mini = min(mini, diff);
-        maxi = max(maxi, diff);
+        mini = min(mini, diff); maxi = max(maxi, diff);
     }
     cout << mini << " " << maxi << endl;
 }
-//find minimum diff of max & min of K number. 
+//find minimum diff of max & min of m numbers
 int mindiff(int *arr, int n, int m) {
-    int res = INT_MAX;
+    if(n < m) return INT_MAX;
     sort(arr, arr + n);
-    for(int i = 0; i <= n - m; i++) 
-        res = min(res, arr[i + k - 1] - arr[i]);
+    int res = INT_MAX;
+    for(int i = 0; i <= n - m; i++) {
+        res = min(res, arr[i + m - 1] - arr[i]);
+    }
     return res;
-}
-int save[100];
-int fib(int n) {
-    if(n == 0) return 0;
-    if(n == 1) return 1;
-    if(save[n] != 0) return save[n];
-    int x = fib(n - 1);
-    int y = fib(n - 2);
-    save[n] = x + y;
-    return x + y;
 }
 int fibsearch(int *arr, int n, int x) {
     int fib1 = 0, fib2 = 1; fib3 = fib1 + fib2;
@@ -261,4 +247,12 @@ int fibsearch(int *arr, int n, int x) {
     }
     if (fib2 == 1 && arr[j + 1] == x) return j + 1;
     else return -1;
+}
+int save[100];
+int fib(int n) {
+    if(n == 0) return 0; if(n == 1) return 1;
+    if(save[n] != 0) return save[n];
+    int x = fib(n - 1); int y = fib(n - 2);
+    save[n] = x + y;
+    return x + y;
 }
