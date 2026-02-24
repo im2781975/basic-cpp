b03bc662ab1113d5f75bde6f757c756b085ec98b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-24 12:20:02 +0600

    Update Shiftval.cpp

--- Group/Execution/Shiftval.cpp
+++ Group/Execution/Shiftval.cpp
@@ -1,39 +1,73 @@
 #include<bits/stdc++.h>
 using namespace std;
-//find position to insert current element of arr using binary search
-int Bsearch(float *arr, int val, int l, int r) {
-    if(l >= r) return (val > arr[l] ? (l + 1): l);
+int Bsearch(float *arr, float val, int l, int r) {
+    if(l >= r) return (val > arr[l] ? l + 1 : l);
     int mid = (l + r) >> 1;
     if(val == arr[mid]) return mid + 1;
     if(val > arr[mid]) return Bsearch(arr, val, mid + 1, r);
     return Bsearch(arr, val, l, mid - 1);
 }
-void valmedian(int *arr, int n) {
-    int j, pos, cnt = 1; float median;
+void valmedian(float *arr, int n) {
+    int j, pos; float median;
     for(int i = 1; i < n; i++) {
-        j = i - 1; num = arr[i];
+        j = i - 1; flat num = arr[i];
         pos = Bsearch(arr, num, 0, j);
         while(j >= pos) {
             arr[j + 1] = arr[j]; j--;
         }
-        arr[j + 1] = num; cnt++;
-        if(cnt % 2 != 0) median = arr[cnt / 2];
-        else median = (arr[(cnt / 2) - 1] + arr[cnt / 2]) / 2;
+        arr[pos] = num;
     }
-    cout << median << endl;
+    float median = (n % 2 == 1)? arr[n / 2] : (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+    cout << fixed << setprecision(1) << median << endl;
 }
-void valmedian(int *arr, int n) {
+void valmedian(vector <float> arr, int n) {
+    float median;
+    for (int i = 1; i < n; i++) {
+        float num = arr[i];
+        int pos = Bsearch(arr, num, 0, i - 1);
+        for (int j = i - 1; j >= pos; j--) arr[j + 1] = arr[j];
+        arr[pos] = num;
+        int cnt = i + 1;  
+        if (cnt % 2 == 1) median = arr[cnt / 2];
+        else median = (arr[cnt / 2 - 1] + arr[cnt / 2]) / 2.0;
+        cout << fixed << setprecision(1) << median << endl;
+    }
+}
+void valmedian(float *arr, int n) {
     priority_queue <int> x, y;
     for(int i = 0; i < n; i++) {
         x.push(arr[i]);
         int tmp = x.top(); x.pop();
         y.push((-1) * tmp);
+        if(x.size() < y.size()) {
+            tmp = y.top(); y.pop();
+            x.push((-1) * tmp);
+        }
+        if(x.size() == y.size())
+            cout << (double)(x.top() + (-1 * y.top())) / 2 << endl;
+        else
+            cout << (double)x.top() << endl;
     }
-    if(x.size() != y.size()) cout << (double) x.top();
-    else cout << (double)((x.top() - y.top()) / 2);
 }
-void pushzero(vector <int> vec) {
-    stable_partition(vec.begin(), vec.end(), [](int n) {return n != 0;});
+void valmedian(float *arr, int n) {
+    priority_queue <int> maxHeap; // Lower Half
+    priority_queue <int, vector <int>, greater <int>> minHeap; // Upper Half
+    for(int i = 0; i < n; i++) {
+        maxHeap.push((int)arr[i]);
+        int tmp = maxHeap.top(); maxHeap.pop();
+        minHeap.push(tmp);
+        if(maxHeap.size() < minHeap.size()) {
+            tmp = minHeap.top(); minHeap.pop();
+            maxHeap.push(tmp);
+        }
+        if(maxHeap.size() == minHeap.size())
+            cout << (double)(maxHeap.top() + minHeap.top()) / 2 << endl;
+        else
+            cout << (double)maxHeap.top() << endl;
+    }
+}
+void pushzero(vector <int> arr) {
+    stable_partition(arr.begin(), arr.end(), [](int n){ return n != 0;});
 }
 void multarr(int *arr, int n) {
     if(n == 0) return 1;
@@ -60,7 +94,7 @@ vector <int> segregate(vector <int> arr) {
 }
 void rearrange(int *arr, int n) {
     for(int i = 0; i < n - 2; i += 2) {
-        if(arr[i] < arr[i + 1]) swap(arr[i], arr[i + 1]);
+        if(arr[i] < arr[i + 1]) swap(arr, arr[i + 1]);
     }
 }
 void rearrange(int *arr, int n) {
@@ -69,9 +103,9 @@ void rearrange(int *arr, int n) {
     for(int i = 0; i < n; i++) {
         if(flag) tmp[i] = arr[l++];
         else tmp[i] = arr[r--];
-        flag = false;
+        flag != flag;
     }
-    for(int i = 0; i < n; i++) arr[i] = tmp[i];
+    for(int i = 0; i < n; i++) cout << arr[i] << " ";
 }
 void assign(int *arr, int n) {
     sort(arr, arr + n); int res[n];
@@ -82,7 +116,8 @@ void assign(int *arr, int n) {
     }
     for(int i = 0; i < n; i++) cout << res[i] << " ";
 }
-void fix(int *arr, int n) {
+// rearrange an array such that arr[i] = i
+void rearrange(int *arr, int n) {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(arr[j] == i){
@@ -95,7 +130,7 @@ void fix(int *arr, int n) {
     }
     for(int i = 0; i < n; i++) cout << arr[i] << " ";
 }
-void fix(int *arr, int n) {
+void rearrange(int *arr, int n) {
     for(int i = 0; i < n; i++) {
         if(arr[i] != i && arr[i] != -1) {
             int x = arr[i];
@@ -106,24 +141,23 @@ void fix(int *arr, int n) {
     }
     for(int i = 0; i < n; i++) cout << arr[i] << " ";
 }
-//rearrange an array such that arr[i] = i.
-int fixarr(int *arr, int n) {
+int rearrange(int *arr, int n) {
     vector <int> vec(n, -1);
     for(int i = 0; i < n; i++) {
         if(arr[i] != -1) vec[arr[i]] = arr[i];
     }
-    for(int i = 0; i < n; i++) arr[i] = vec[i] << " ";
+    for(int i = 0; i < n; i++) arr[i] = vec[i];
 }
-void fixarr(int *arr, int n) {
-    int i = 0;
+int rearrange(int *arr, int n) {
+    int i = 0; 
     while(i < n) {
-        int val = arr[i]; 
+        int val = arr[i];
         if(arr[i] != -1 && arr[i] != arr[val]) swap(arr[i], arr[val]);
-        else i++;
+        i++;
     }
     return arr;
 }
-void fixarr(int *arr, int n) {
+void rearrange(int *arr, int n) {
     unordered_set <int> st;
     for(int i = 0; i < n; i++) {
         if(arr[i] != -1) st.insert(arr[i]);
@@ -137,8 +171,8 @@ void shiftzero(vector <int> vec) {
     int cnt = 0, n = vec.size();
     for(int i = 0; i < n; i++) {
         if(vec[i] == 0) {
-            cnt++; vec.erase(vec.begin() + i); 
-            i--; n--;
+            vec.erase(vec.begin() + i);
+            cnt++; i--; n--;
         }
     }
     for(int i = 0; i < cnt; i++) vec.push_back(0);
@@ -175,4 +209,23 @@ void slidewindow(vector <int> vec, int n, int k) {
     }
     cout << res;
 }
-
+int maxsubarrsum(vector <int> vec, int k) {
+    vector <int> prefix(n + 1, 0);
+    for(int i = 1; i <= n; i++) prefix[i] = prefix[i - 1] + vec[i - 1];
+    int res = INT_MIN;
+    for(int i = k; i <= n; i++) {
+        res = max(res, prefix[i] - prefix[i - k]);
+    }
+    return res;
+}
+int maxslidingwindow(vector <int> vec, int k) {
+    deque <int> dq;
+    int res = INT_MIN;
+    for(int i = 0; i < vec.size(); i++) {
+        if(!dq.empty() && dq.front() == i - k) dq.pop_front();
+        while(!dq.empty() && vec[dq.back()] < vec[i]) dq.pop_back();
+        dq.push_back(i);
+        if(i >= k - 1) res = max(res, vec[dq.front()]);
+    }
+    return res;
+}
