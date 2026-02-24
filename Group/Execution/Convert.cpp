136ec8b40b5d2fa51925dda594dd33da7228a255
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-25 04:43:30 +0600

    Update Convert.cpp

--- Group/Execution/Convert.cpp
+++ Group/Execution/Convert.cpp
@@ -1,5 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+void printSpiralMatrix(int rows, int cols, int matrix[][4]) {
+    int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) {
+            cout << matrix[top][i] << " ";
+        }
+        top++;
+        for (int i = top; i <= bottom; i++) {
+            cout << matrix[i][right] << " ";
+        }
+        right--;
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                cout << matrix[bottom][i] << " ";
+            }
+            bottom--;
+        }
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                cout << matrix[i][left] << " ";
+            }
+            left++;
+        }
+    }
+}
 //find an element that can be used to operate on an arr & get equal
 void equalize(int *arr, int n) {
     set <int> st;
