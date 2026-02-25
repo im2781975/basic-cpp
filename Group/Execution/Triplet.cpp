055dcb0c0e9e5269c88d90d6f539d68ab82ec644
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-25 09:56:30 +0600

    Update Triplet.cpp

--- Group/Execution/Triplet.cpp
+++ Group/Execution/Triplet.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 //find the min and max values that can be calculated by summing exactly four of the five integers
-void maxmin(int *arr, int n) {
+void maxmin(int *arr, int n){
     int tmp[n], sum;
     for(int i = 0; i < n; i++) {
         sum = 0;
@@ -11,12 +11,12 @@ void maxmin(int *arr, int n) {
         }
         tmp[i] = sum;
     }
-    int mn = INT_MAX, mx = INT_MIN;
-    for(int i = 0; i < n; i++) {
-        if(tmp[i] < mn) mn = tmp[i];
-        if(tmp[i] > mx) mx = tmp[i];
+    int mn = tmp[0], mx = tmp[0];
+    for(int i = 1; i < n; i++) {
+        if(mn > tmp[i]) mn = tmp[i];
+        if(mx < tmp[i]) mx = tmp[i];
     }
-    cout << mx << " " << mn;
+    cout << mn << " " << mx << endl;
 }
 int left(int *arr, int l, int r, int key) {
     while(r - l > 1) {
@@ -39,15 +39,15 @@ int cntfreq(int *arr, int n, int key) {
     int r = right(arr, 0, n - 1, key);
     return (arr[l] == key && key == arr[r]) ? (r - l + 1) : -1;
 }
-int Bsearch(int *arr, int l, int r, int key){
-    if(r < l) return -1;
-    int mid = l + (r - l) / 2;
-    if(key == arr[mid]) return mid;
-    else if(key > arr[mid]) return  Bsearch(arr, mid + 1, r, key);
-    else return Bsearch(arr, l, mid - 1, key);
+void Bsearch(int *arr, int low, int high, int key) {
+    if(high < low) return -1;
+    int mid = low + (high - low) / 2;
+    if(arr[mid] == key) return mid;
+    else if(key > arr[mid]) return Bsearch(arr, mid + 1, high, key);
+    else return Bsearch(arr, low, mid - 1, key);
 }
-int Deleteval(int *arr, int n, int key){
-    int pos = Bsearch(arr, 0, n - 1, key);
+int DeleteVal(int *arr, int n, int key) {
+    pos = Bsearch(arr, 0, n - 1, key);
     if(pos == -1) return n;
     for(int i = pos; i < n - 1; i++)
         arr[i] = arr[i + 1];
@@ -56,18 +56,18 @@ int Deleteval(int *arr, int n, int key){
 // find the Minimum length of Unsorted Subarray
 void unsorted(int *arr, int n) {
     int l = 0, r = n - 1;
-    while(l < n - 1 && arr[l] <= arr[l + 1])    l++;
+    while(l < n - 1 && arr[l] <= arr[l + 1]) l++;
     if(l == n - 1) return;
-    while (r > 0 && arr[r] >= arr[r - 1]) r--;
+    while(r > 0 && arr[r] >= arr[r - 1]) r--;
     int submin = arr[l], submax = arr[l];
-    for (int i = l + 1; i <= r; i++) {
+    for(int i = l + 1; i <= r; i++) {
         submin = min(submin, arr[i]);
         submax = max(submax, arr[i]);
     }
-    for(int i = 0; i < l; i++){
+    for(int i = 0; i < l; i++) {
         if(arr[i] > submin) l = i; break;
     }
-    for(int i = n - 1; i > r; i--){
+    for(int i = n - 1; i > r; i--) {
         if(arr[i] < submax) r = i; break;
     }
     cout << l << " " << r << endl;
@@ -87,18 +87,16 @@ void cntsurpasser(int *arr, int n) {
         for(int j = i + 1; j < n; j++) {
             if(arr[j] > arr[i]) ++cnt;
         }
-        cout << cnt << " ";
     }
+    cout << cnt;
 }
-// Returns length of smallest subarray with sum greater than x. 
+// Returns length of smallest subarray with sum greater than x
 int subarrlen(int *arr, int n, int x) {
     int cur = 0, minlen = n + 1, l = 0, r = 0;
-    while(r < n) {
-        while(cur <= x && r < n) cur += arr[r++];
-        while(cur > x && l < n) {
-            if(r - l < minlen) minlen = r - l;
-            cur -= arr[l++];
-        }
+    while(cur <= x && r < n) cur += arr[r++];
+    while(cur > n && l < n) {
+        if(r - l < minlen) minlen = r - l;
+        cur -= arr[l++];
     }
     return minlen;
 }
@@ -115,17 +113,28 @@ int subarrlen(int *arr, int n, int x) {
     return minlen;
 }
 int subarrlen(vector <int> arr, int x) {
-    int n = arr.size(), res = INT_MAX - 1;
+    int n = arr.size(), res = INT_MIN;
     if(n == 0) return 1;
     vector <int> sum(n + 1, 0);
     for(int i = 1; i <= n; i++) sum[i] = sum[i - 1] + arr[i - 1];
     for(int i = 1; i <= n; i++) {
         int val = x + sum[i - 1];
-        auto find = lower_bound(sum.begin(), sum.end(), val);
-        if(find != sum.end() && *find != val) {
+        auto find = lower_bound(arr.begin(), arr.end(), val);
+        if(find != sum.end() && *find == val) {
             int len = find - (sum.begin() + i - 1);
             res = min(res, len);
         }
+    }
+    return res;
+}
+int subarrlen(vector <int> arr, int n) {
+    unordered_map <int, int> mp;
+    mp[0] = 0;
+    int sum = 0, res = INT_MIN;
+    for(int i = 1; i <= arr.size(); i++) {
+        sum += arr[i - 1]; int trg = sum - x;
+        if(mp.count(trg)) res = min(res, i - mp[trg]);
+        if(mp.find(sum) == mp.end()) mp[sum] = i;
     }
     return res;
 }
@@ -133,34 +142,37 @@ int subarrlen(vector <int> arr, int x) {
 void subarrsum(int *arr, int n, int sum) {
     for(int i = 0; i < n; i++) {
         int cur = arr[i];
-        if(cur == sum) { cout << i << " "; return; }
-        else {
-            for(int j = i + 1; j < n; j++) {
-                cur += arr[j];
-                if(cur == sum) cout << i << " " << j << endl; return;
+        if(cur == sum) {
+            cout << i << " "; return;
+        }
+        for(int j = i + 1; j < n; j++) {
+            cur += arr[j];
+            if(cur == sum) {
+                cout << i << " " << j << endl; return;
             }
         }
     }
     return;
 }
 int subarrsum(int *arr, int n, int sum) {
-    int l = 0, cur = arr[0];
-    for(int i = 1; i <= n; i++) {
-        if(i < n) cur += arr[i];
+    int cur = 0, l = 0;
+    for(int i = 0; i < n; i++) {
+        cur += arr[i];
+        while(cur > sum && l <= i) cur -= arr[l++];
         if(cur == sum) {
-            cout << l << " " << i - 1 << endl; return 1;
+            cout << l << " " << i << endl;
+            return;
         }
-        while(cur > sum && l < i - 1) cur -= arr[l++];
     }
     return 0;
 }
 vector <int> subarrsum(vector <int> arr, int sum) {
-    unordered_map <int, int> mp;
-    int cur = 0;
+    unordered_map <int, int> mp; int cnt = 0;
     for(int i = 0; i < arr.size(); i++) {
         cur += arr[i];
-        if (mp.count(cur - sum))
+        if(mp.count(cur - sum)) {
             return vector <int> (arr.begin() + mp[cur - sum] + 1, arr.begin() + i + 1);
+        }
         mp[cur] = i;
     }
     return {};
@@ -182,94 +194,32 @@ void freq(vector <int> arr) {
             arr[i++] = it -> second;
     }
 }
-// Function to reorder elements of arr[] according to index[]
-bool comp(const pair<int, int>& x, const pair<int, int>& y) {
-    return x.second < y.second;
-}
-void reorder(int *arr, int *idx, int n) {
-    vector <pair <int, int>>vec(n);
-    for(int i = 0; i < n; i++) {
-        vec[i].first = arr[i];
-        vec[i].second = idx[i];
-    }
-    sort(vec.begin(), vec.end(), cmp);
-    for(int i = 0; i < n; i++) 
-        arr[i] = vec[i].first;
-}
-void reorder(int *arr, int *idx, int n) {
-    for(int i = 0; i < n; i++) {
-        while(idx[i] != i) {
-            int dig = idx[idx[i]];
-            char ch = arr[idx[i]];
-            
-            arr[idx[i]] = arr[i];
-            idx[idx[i]] = idx[i];
-            
-            idx[i] = dig;
-            arr[i]   = ch;
-        }
-    }
-}
-void reorder(int *arr, int *idx, int n) {
-    int tmp[n];
-    for(int i = 0; i < n; i++) tmp[idx[i]] = arr[i];
-    for(int i = 0; i < n; i++) {
-        arr[i] = tmp[i];
-        idx[i] = i;
-    }
-}
-void printSpiralMatrix(int rows, int cols, int matrix[][4]) {
-    int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
-
-    while (top <= bottom && left <= right) {
-        for (int i = left; i <= right; i++) {
-            cout << matrix[top][i] << " ";
-        }
-        top++;
-        for (int i = top; i <= bottom; i++) {
-            cout << matrix[i][right] << " ";
-        }
-        right--;
-        if (top <= bottom) {
-            for (int i = right; i >= left; i--) {
-                cout << matrix[bottom][i] << " ";
-            }
-            bottom--;
-        }
-        if (left <= right) {
-            for (int i = bottom; i >= top; i--) {
-                cout << matrix[i][left] << " ";
-            }
-            left++;
-        }
-    }
-}
 // find three elements whose sum is equal to zero
-void findTriplets(int arr[], int n){
+void findTriplets(int *arr, int n) {
     bool found = false;
-    for (int i = 0; i < n - 2; i++) {
-        for (int j = i + 1; j < n - 1; j++) {
-            for (int k = j + 1; k < n; k++) {
-                if (arr[i] + arr[j] + arr[k] == 0) {
+    for(int i = 0; i < n - 2; i++) {
+        for(j = i + 1; j < n - 1; j++) {
+            for(int k = j + 1; k < n; k++) {
+                if(arr[i] + arr[j] + arr[k] == 0) {
                     cout << arr[i] << " " << arr[j] << " " << arr[k] << endl;
-                    found = true;
+                    found = true; 
                 }
             }
         }
     }
-    if (found == false) cout << " not exist " << endl;
+    if(!found) cout << "not exits" < endl;
 }
 void findTriplets(int *arr, int n) {
-    bool found = false;
     sort(arr, arr + n);
+    bool found = false;
     for(int i = 0; i < n - 1; i++) {
         int l = i + 1, r = n - 1;
-        while (l < r) {
-            if (arr[i] + arr[l] + arr[r] == 0) {
+        while(l < r) {
+            if(arr[i] + arr[l] + arr[r] == 0) {
                 cout << arr[i] << " " << arr[l] << " " << arr[r] << endl;
                 l++; r--; found = true;
             }
-            else if (x + arr[l] + arr[r] < 0) l++;
+            else if(arr[i] + arr[l] + arr[r] > 0) l++;
             else r--;
         }
     }
@@ -278,21 +228,21 @@ void findTriplets(int *arr, int n) {
     bool found = false;
     for(int i = 0; i < n - 1; i++) {
         unordered_set <int> st;
-        for (int j = i + 1; j < n; j++) {
+        for(int j = i + 1; j < n; j++) {
             int x = -(arr[i] + arr[j]);
-            if (st.find(x) != st.end()) {
+            if(st.find(x) != st.end()) {
                 cout << x << " " << arr[i] << " " << arr[j] << endl;
                 found = true;
             }
-            else st.insert(arr[j]);
+            else st.insert(arr[i]);
         }
     }
     if(!found) cout << "No Triplet found";
 }
-void Triple(int *arr, int n, int sum) {
+bool Triplet(int *arr, int n, int sum) {
     for(int i = 0; i < n - 2; i++) {
         unordered_set <int> st;
-        int curr = sum - arr[i];
+        int cur = sum - arr[i];
         for(int j = i + 1; j < n; j++) {
             int req = cur - arr[j];
             if(st.find(req) != st.end()) {
@@ -304,22 +254,16 @@ void Triple(int *arr, int n, int sum) {
     }
     return false;
 }
-int maximum(int a, int b, int c) {
-    return max(max(a, b), c);
-}
-int minimum(int a, int b, int c) {
-   return min(min(a, b), c);
-}
 void TripletDiff(int *a, int *b, int *c, int n) {
     sort(a, a + n); sort(b, b + n), sort(c, c + n);
     int mini, maxi, mid, i = 0, j = 0, k = 0;
     int diff = INT_MAX;
-    while (i < n && j < n && k < n){
-        int sum = a[i] + b[j] + c[k];
-        int mx = maximum(a[i], b[j], c[k]);
-        int mn = minimum(a[i], b[j], c[k]);
-        if (mx == a[i]) i++;
-        else if (mn == b[j]) j++;
+    while(i < n && j < n && k < n) {
+        int sum = a[i] + b[i] + c[i];
+        int mx = maximum(a[i], b[i], c[i]);
+        int mn = minimum(a[i], b[i], c[i]);
+        if(mx == a[i]) i++;
+        else if(mn = b[j]) j++;
         else k++;
         if (diff > (mx - mn)){
             diff = mx - mn;
@@ -328,6 +272,8 @@ void TripletDiff(int *a, int *b, int *c, int n) {
             mini = mn;
         }
     }
-    cout << maxi << " " << mini << " " << mid << endl;
+    cout << mini << " " << mid << " " << maxi << endl;
 }
+
+
 
