53260415edc169b4b89dbd5ed759703c6fee5431
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-28 06:27:26 +0600

    Add functions for prime generation and counting zeros

--- Group/Execution/CntOccur.cpp
+++ Group/Execution/CntOccur.cpp
@@ -224,3 +224,53 @@ void print(vector <int> vec, vector <int> res) {
         cout << vec[i] << " ";
     }
 }
+void primes(int n) {
+    vector <int> prime;
+    vector <bool> flag(n + 1);
+    for(int i = 2; i <= n; i++) {
+        if(flag[i] == false) {
+            prime.push_back(i);
+            for(int x = 2; i * x <= n; x++){
+                flag[i * x] = true;
+            }
+        }
+    }
+    for(int i = 0; i < prime.size(); i++)
+        cout << prime[i] << " ";
+}
+int recusive(int *arr, int n, int l, int r) {
+    if(l == r) return arr[l];
+    else {
+        int mid = l + (r - l) / 2;
+        return min(recursive(arr, n, l, mid - 1), recursive(arr, n, mid + 1, r));
+    }
+}
+void cntzero(int *arr, int n, int q) {
+    int cnt = 0;
+    for(int i = 1; i <= n; i++) {
+        if(arr[i] != 0) cnt++;
+    }
+    while(q--) {
+        int type, idx; cin >> type >> idx;
+        if(type == 2) {
+            if(arr[idx] == 0) arr[idx]++;
+        }
+        else if(type == 1) {
+            if(arr[idx] > 0) {
+                arr[idx]--;
+                if(arr[idx] == 0) cnt--;
+            }
+        }
+    }
+    cout << cnt;
+}
+void pair() {
+    vector <pair <int, int>> vpr;
+    int arr[]{10, 20, 5, 40};
+    int ray[]{30, 60, 20, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i = 0; i < n; i++)
+        vec.push_back(make_pair(arr[i], ray[i]));
+    for(int i = 0; i < n; i++)
+        cout << vec[i].first << " " << vec[i].second << endl;
+}
