50c5f9762d50bba611024eb608fa8391e3df6ba5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-03-02 21:43:17 +0600

    Update Jump.cpp

--- Group/Execution/Jump.cpp
+++ Group/Execution/Jump.cpp
@@ -237,3 +237,16 @@ void maxoverlap(vector <int>start, vector <int> end) {
     }
     cout << maxi << " " << cur
 }
+int recursiveMin(const vector<int> &arr, int l, int r) {
+    if (l == r) return arr[l];
+    else {
+        int mid = (l + r) / 2;
+        return min(recursiveMin(arr, l, mid), recursiveMin(arr, mid + 1, r));
+    }
+}
+void initialize() {
+    typedef vector <int> vec;
+    vec v;
+    for(int i = 1; i <= 10; i += 2) v.push_back(i * 2);
+    for(int x : v) cout << x << " ";
+}
