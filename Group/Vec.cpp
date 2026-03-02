ace9bc2730d8d8e9ffec7b7a3c89c56a7904a52f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-03-02 19:48:42 +0600

    Update Vec.cpp

--- Group/Vec.cpp
+++ Group/Vec.cpp
@@ -1,36 +1,8 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    typedef vector<int> vInt;
-    // vec1 is a vectorof type int
-    vInt v;
-    v.push_back(190);
-    v.push_back(180);
-    v.push_back(10);
-    v.push_back(10);
-    v.push_back(27);
-    cout<<"array element is: ";
-    for (auto X : v)
-    {
-        cout << X << " ";
-    }
-    cout<<"\n";
-//---------------------------------
-    typedef unsigned long long int ulli;
-    ulli a{ 1232133 };
-    cout <<"after typedef element is: "<< a<<"\n";
-//---------------------------------
-//typedef ptr
-    int x = 10;
-    int y = 20;
-    typedef int* iPtr;
-  
-    iPtr pointer_to_a = &x;
-    iPtr pointer_to_b = &y;
-  
-    cout << "x is: " << *pointer_to_a << "\n";
-    cout << "y is: " << *pointer_to_b << "\n";
-  
-    return 0;
+void initialize() {
+    typedef vector <int> vec;
+    vec v;
+    for(int i = 1; i <= 10; i += 2) v.push_back(i * 2);
+    for(int x : v) cout << x << " ";
 }
