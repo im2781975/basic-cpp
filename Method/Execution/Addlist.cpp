ac5c7520ba453d39f470d42b1199d168b22b1beb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-03-20 21:30:55 +0600

    Update Addlist.cpp

--- Method/Execution/Addlist.cpp
+++ Method/Execution/Addlist.cpp
@@ -107,3 +107,174 @@ void printlist(node *head) {
         head = head -> next;
     }
 }
+class node {
+    public:
+    int data; node *next;
+};
+void push(node **head, int val) {
+    node *newnode = new node{val, *head};
+    *head = newnode;
+}
+// Insert at beginning of circular list
+void pushcircular(node *&head, int val) {
+    node *newnode = new node{data, head};
+    if(!head) head = newnode;
+    else {
+        node *tail = head;
+        while(tail -> next != head) tail = tail -> next;
+        tail -> next = newnode;
+    }
+    head = newnode;
+}
+void deletelist(node **head) {
+    node *curr = *head;
+    while(curr) {
+        node *tmp = curr -> next;
+        delete curr; curr = curr -> next;
+    }
+    *head = nullptr;
+}
+void deletenode(node **head, int pos) {
+    if(!*head || pos < 0) return;
+    if(pos == 0) {
+        node *tmp = *head;
+        *head = (*head) -> next;
+        delete tmp; return;
+    }
+    node *curr = head;
+    for(int i = 0; i < pos - 1 && curr -> next; ++i) curr = curr -> next;
+    if(curr -> next) {
+        node *tmp = curr -> next;
+        curr -> next = tmp -> next;
+        delete tmp;
+    }
+}
+void deletenode(node *head) {
+    if(head == NULL) return;
+    else {
+        node *tmp = head -> next;
+        head -> data = tmp -> data;
+        head -> next = tmp -> next;
+        tmp = NULL;
+    }
+}
+void deleteOddnode(node *head) {
+    if(!head) return;
+    node *curr = head;
+    node *tmp = curr -> next;
+    while(curr && tmp) {
+        curr -> next = tmp -> next; delete(tmp);
+        curr = curr -> next;
+        if(curr) tmp = curr -> next;
+    }
+}
+void deleteAlt(Node *head) {
+    if (!head || !head->next) return;
+    
+    Node* prev = head;
+    while (prev && prev->next) {
+        prev->next = prev->next->next;
+        prev = prev->next;
+    }
+}
+void deleteOddnode(node *head) {
+    if(!head || !head -> next) return;
+    node *curr = head;
+    while(curr && curr -> next) {
+        curr -> next = curr -> next -> next;
+        curr = curr -> next;
+    }
+}
+void deleteOddnode(node *head) {
+    if(!head || !head -> next) return;
+    head -> next = head -> next -> next;
+    delete head -> next;
+    deleteOddnode(head -> next);
+}
+void deletekey(node **head, int key) {
+    if(!*head) return;
+    if((*head) -> data == key) {
+        node *tmp = *head;
+        *head = tmp -> next;
+        delete tmp; return;
+    }
+    node *curr = *head;
+    while(curr -> next && curr -> next -> data != key) curr = curr -> next;
+    if(curr -> next) {
+        node *tmp = curr -> next;
+        curr -> next = tmp -> next;
+        delete tmp;
+    }
+}
+node *deletekthnode(node *head, int k) {
+    if(!head || k <= 0) return nullptr;
+    if(k == 1) {
+        while(head) {
+            node *tmp = head;
+            head = head -> next; delete tmp;
+        }
+        return nullptr;
+    }
+    node *curr = head, *prv = nullptr;
+    /* int cnt = 0;
+    while(curr != NULL) {
+        cnt++;
+        if(k == cnt) {
+            delete prv -> next;
+            prv -> next = curr -> next;
+            cnt = 0;
+        }
+        if(cnt != 0) prv = curr;
+        curr = prv -> next;
+    } */
+    while(curr) {
+        for(int i = 1; i < k && curr -> next; i++) {
+            prv = curr; curr = curr -> next;
+        }
+        if(!curr -> next) break;
+        prv -> next = curr -> next -> next;
+        delete curr -> next;
+        curr = prv -> next;
+    }
+    return head;
+}
+void append(node **head, int val) {
+    node *newnode = new node(data, nullptr);
+    if(!*head) {
+        *head = newnode; return;
+    }
+    node *curr = *head;
+    while(curr -> next) curr = curr -> next;
+    curr -> next = newnode;
+}
+void makecircular(node *head) {
+    node *curr = head;
+    while(curr -> next != NULL) curr = curr -> next;
+    curr -> next = head;
+}
+node *swapfirstlast(node *head) {
+    if(!head || head -> next == head) return head;
+    node *curr = head, *tmp = curr -> next;
+    while(tmp -> next != head) {
+        curr = tmp; tmp = tmp -> next;
+    }
+    if(curr == head) {
+        tmp -> next = head; head -> next = tmp;
+        return tmp;
+    }
+    curr -> next = head;
+    head -> next = tmp -> next;
+    tmp -> next = head;
+    return tmp;
+}
+bool isCircular(node *head) { 
+    if (head == nullptr) return false;
+    node *slow = head;
+    node *fast = head -> next;
+    while(fast != nullptr && fast -> next != nullptr) {
+        if(slow == fast) return true;
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    return false;
+}
