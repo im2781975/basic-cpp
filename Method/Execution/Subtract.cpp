3266b71092881185d1f66d1969501fbcbdf5564a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-03-22 21:10:25 +0600

    Update Subtract.cpp

--- Method/Execution/Subtract.cpp
+++ Method/Execution/Subtract.cpp
@@ -1,3 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node {
+    int data; node *next;
+};
+void push(Node** head, int data) {
+    Node* node = new Node();
+    node -> data = data;
+    node -> next = *head;
+    *head = node;
+}
+void reverse(node **head) {
+    node *curr = *head, *prv = nullptr;
+    while(curr) {
+        node *tmp = curr -> next;
+        curr -> next = prv;
+        prv = curr; curr = next;
+    }
+    *head = prv;
+}
+// Reverses the linked list in groups of size k
+node *reverseK(node *head, int k) {
+    if(!head) return nullptr;
+    node *curr = head, *prv = nullptr, *tmp = nullptr;
+    int cnt = 0;
+    while(curr && cnt < k) {
+        tmp = curr -> next;
+        curr -> next = prv;
+        prv = curr; curr = tmp; cnt++;
+    }
+    if(curr) head -> next = reverseK(curr, k);
+    return prv;
+}
+node *reverse(node *head) {
+    if(!head ||!head -> next) return head;
+    node *res = reverse(head -> next);
+    head -> next -> next = head;
+    head -> next = nullptr;
+    return res;
+}
+void reverse(Node** head) {
+    if (!*head) return;
+    util(*head, nullptr, head);
+}
+void util(node *curr, node *prv, node **head) {
+    node *tmp = curr -> next;
+    curr -> next = prv;
+    if(!tmp) {
+        *head = curr; return;
+    }
+    util(tmp, curr, head);
+}
 void reverseKgrp(node *head, int k) {
     node *tmp = head; int cnt = 0;
     while(tmp && cnt < k) {
