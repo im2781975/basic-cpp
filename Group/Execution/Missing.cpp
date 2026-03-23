e0c7406a72c2b142daeee122b10d5bd08b4b2d97
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-03-23 09:42:06 +0600

    Update Missing.cpp

--- Group/Execution/Missing.cpp
+++ Group/Execution/Missing.cpp
@@ -106,6 +106,7 @@ void cyclesum(int *arr, int n) {
     }
     cout << maxi;
 }
+
 // Returns max possible value of i * arr[i]
 void maxsum(int *arr, int n) {
     int sum = 0, cur = 0;
@@ -118,6 +119,94 @@ void maxsum(int *arr, int n) {
         if(cur > maxi) maxi = cur;
     }
     cout << maxi;
+}
+// find Two elements whose sum is closest to zero
+void minsum(int *arr, int n) {
+    if(n < 2) return;
+    sort(arr, arr + n, [](int a, int b){
+        return abs(a) < abs(b); });
+    int mini = INT_MAX, x = -1, y = -1;
+    for(int i = 1; i < n; i++) {
+        if(abs(arr[i - 1] + arr[i]) <= mini) {
+            mini = abs(arr[i - 1] + arr[i]);
+            x = i - 1; y = i;
+        }
+    }
+    cout << arr[x] << " " << arr[y] << endl;
+}
+void minsum(int *arr, int n) {
+    int tmp = abs(arr[0] + arr[1]);
+    int minL = 0, minR = 1;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            int sum = abs(arr[i] + arr[j]);
+            if (sum < tmp) {
+                tmp = sum; minL = i; minR = j;
+            }
+        }
+    }
+    cout << arr[minR] << " " << arr[minL] << endl;
+}
+void minsum(int *arr, int n) {
+    if(n < 2) return;
+    sort(arr, arr + n);
+    int tmp = INT_MAX, minL = 0, minR = 1;
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int current = abs(arr[left] + arr[right]);
+        if (current < minSum) {
+            tmp = current;
+            minL = left;
+            minR = right;
+        }
+        if (arr[left] + arr[right] < 0) left++;
+        else right--;
+    }
+    cout << arr[minL] << " " << arr[minR] << endl;
+}
+// find the pair from two sorted arrays such that the sum of pair is closest to a given number x
+void printClosestPair(int arr[], int ray[], int m, int n, int x) {
+    int l = 0, r = n - 1;
+    int minDiff = INT_MAX;
+    int resL = -1, resR = -1;
+    while (l < m && r >= 0) {
+        int sum = arr[l] + ray[r];
+        int diff = abs(sum - x);
+        if (diff < minDiff) {
+            minDiff = diff;
+            resL = l; resR = r;
+        }
+        if (sum > x) r--;
+        else l++;
+    }
+    cout << arr[resL] << " " << ray[resR] << endl;
+}
+int Bsearch(int *arr, int l, int r, int val) {
+    if(l > r) return -1;
+    int mid = l + (r - l) >> 2;
+    if(arr[mid] == val) return mid;
+    else if (arr[mid] > val) return Bsearch(arr, l, mid - 1, val);
+    else
+        return Bsearch(arr, mid + 1, r, val);
+}
+void printClosest(int arr[], int ray[], int m, int n, int x) { /*
+    for(int i = 0; i < m; i++) {
+        int idx = Bsearch(ray, 0, n - 1, x - arr[i]);
+        if(idx >= 0 && idx < n && abs(arr[i] + ray[idx] - val) < abs(arr[i] + ray[idx - 1] - x)) printClosest(arr, ray, m, n, val);
+        else if(idx > 0 && abs(arr[i] + ray[idx - 1] - val) < abs(arr[i] + ray[idx] - x)) idx--;
+    } */
+    int diff = INT_MAX;
+    int resL, resR;
+    int l = 0, r = n - 1;
+    while (l < m && r >= 0) {
+        if (abs(arr[l] + ray[r] - x) < diff) {
+            resL = l; resR = r;
+            diff = abs(arr[l] + ray[r] - x);
+        }
+        if (ar1[l] + ar2[r] > x) r--;
+        else l++;
+    }
+    cout << arr[resL] << " " << ray[resR];
 }
 bool ispairsum(int *arr, int n, int trg) {
     for(int i = 0; i < n; i++) {
