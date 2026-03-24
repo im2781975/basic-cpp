86cebccf699ea6f104eb16752db77f07824af1b9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-03-24 20:23:03 +0600

    Create Disjoint.cpp

---
+++ Group/Algo/Disjoint.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector <vector <int>> edgeList(4);
+    edgeList[0].push_back(0);
+    edgeList[0].push_back(1);
+    edgeList[1].push_back(2);
+    edgeList[1].push_back(1);
+    edgeList[2].push_back(1);
+    edgeList[2].push_back(3);
+    edgeList[3].push_back(2);
+    edgeList[3].push_back(3);
+    for(int i = 0; i < edgeList.size(); i++){
+        for(int j = 0; j < edgeList[i].size(); j++)
+            cout << edgeList[i][j] << " ";
+        cout << "\n";
+    }
+}
+using namespace std;
+int main(){
+    vector <vector <int> >edgeList 
+    {{1, 2, 3}, {4, 5, 6}, 
+    {7, 8, 9}, {10, 11, 12},};
+    edgeList.push_back({13, 14, 15});
+    for(int i = 0; i < edgeList.size(); i++){
+        cout << edgeList[i][0] << " " << edgeList[i][1] << " " << edgeList[i][2] << " ";
+        cout << "\n";
+    }
+}
+using namespace std;
+int main(){
+    int node, edge; cin >> node >> edge;
+    cout << "Maximum number of edges that can be added to maintain DAG is: " << (node * (node - 1) /2) - edge;
+}
