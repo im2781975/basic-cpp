eaa024716584b08ed355f69f5432fd8e64a671ca
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-03-31 11:29:22 +0600

    Update LCS.cpp

--- Group/Execution/LCS.cpp
+++ Group/Execution/LCS.cpp
@@ -157,3 +157,47 @@ int main() {
     cout << endl;
     return 0;
 }
+using namespace std;
+bool DFS(vector <vector <char> >&matrix, string str, int i, int j, int n, int m, int idx){
+    if((i < 0 || i >= n || j < 0 || j >= m))
+        return false;
+    if(str[idx]!= matrix[i][j])
+        return false;
+    if(idx == str.size() - 1)
+        return true;
+    char tmp = matrix[i][j];
+    matrix[i][j] = '*';
+    //Explore 8 direction
+    bool a = DFS(matrix, str, i, j + 1, n, m, idx + 1);
+    bool b = DFS(matrix, str, i, j - 1, n, m, idx + 1);
+    bool c = DFS(matrix, str, i + 1, j, n, m, idx + 1);
+    bool d = DFS(matrix, str, i - 1, j, n, m, idx + 1);
+    bool e = DFS(matrix, str, i + 1, j + 1, n, m, idx + 1);
+    bool f = DFS(matrix, str, i + 1, j - 1, n, m, idx + 1);
+    bool g = DFS(matrix, str, i - 1, j + 1, n, m, idx + 1);
+    bool h = DFS(matrix, str, i - 1, j - 1, n, m, idx + 1);
+    matrix[i][j] = tmp;
+    return (a || b || c || d || e || f || g || h);
+}
+void FindWord(vector <vector <char> >&matrix, vector <string> &dic){
+    //int n = matrix.size(), m = matrix[0].size();
+    vector <string> ans;
+    set <string> store;
+    for(int i = 0; i < dic.size(); i++){
+        string str = dic[i];
+        for(int j = 0; j < matrix.size(); j++){
+            for(int k = 0; k < matrix[0].size(); k++){
+                if(DFS(matrix, str, j, k, matrix.size(), matrix[0].size(), 0))
+                    store.insert(str);
+            }
+        }
+    }
+    for(const string &word : store)
+        cout << word << " ";
+}
+int main(){
+    vector <vector <char> >Boggle { { 'G', 'I', 'Z' }, { 'U', 'E', 'K' }, { 'Q', 'S', 'E' }
+    };
+    vector <string> Dic{ "GEEKS", "FOR", "QUIZ", "GO" };
+    FindWord(Boggle, Dic);
+}
