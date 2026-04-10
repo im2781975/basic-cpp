efe5fd18c8a56d11326db38a90a4fb35288f6e8a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-10 21:04:41 +0600

    Update Sinknode.cpp

--- Group/Root/Sinknode.cpp
+++ Group/Root/Sinknode.cpp
@@ -15,6 +15,41 @@ int cntsinknode(int nd, int edg, int *src, int *dst) {
     } */
     return cnt;
 }
+//find transpose of a graph.
+using namespace std;
+void addEdge(vector <int> adj[], int u, int v){
+    adj[u].push_back(v);
+}
+void Display(vector <int> adj[], int node){
+    for(int i = 0; i < node; i++){
+        for(int j = 0; j < adj[i].size(); j++)
+            cout << adj[i][j] << " ";
+        cout << "\n";
+    }
+    cout << "\n";
+}
+void TransposeGraph(vector <int> transpose[], vector <int> adj[], int node){
+    for(int i = 0; i < node; i++){
+        for(int j = 0; j < adj[i].size(); j++)
+            addEdge(transpose, adj[i][j], i);
+    }
+}
+int main(){
+    int node = 5;
+    vector <int> adj[node];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 0, 3);
+    addEdge(adj, 2, 0);
+    addEdge(adj, 3, 2);
+    addEdge(adj, 4, 1);
+    addEdge(adj, 4, 3);
+    vector <int> transpose[node];
+    
+    TransposeGraph(transpose, adj, node);
+    //Display(adj, node);
+    Display(transpose, node);
+}
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
