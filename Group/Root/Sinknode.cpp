0878c6696627b7dbe0685cfb2e794a1c29e214b0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-10 22:14:23 +0600

    Update Sinknode.cpp

--- Group/Root/Sinknode.cpp
+++ Group/Root/Sinknode.cpp
@@ -1,3 +1,38 @@
+using namespace std;
+//find the shortest path using multistage graph
+using ump = pair <int, unordered_map<int, int> >;
+int shortestPath(vector <ump> &graph, int src, int trg, int k){
+    vector <int> dist(graph.size(), INT_MAX);
+    dist[trg] = 0;
+    //Loop over each stage from k-1 to 1
+    for(int i = k - 1; i >= 0; i--){
+        //Loop over all vertices in the current stage
+        for(int u = 0; u < graph.size(); u++){
+            if(graph[u].first != i)
+                continue;
+            for(const auto &v: graph[u].second){
+                if(dist[v.first] != INT_MAX)
+                     dist[u] = min(dist[u], v.second + dist[v.first]);
+            }
+        }
+    }
+    //Return the shortest path distance from source to target
+    return dist[src];
+}
+int main(){
+    //Each stage represented a level of nodes
+    vector <ump> graph = {
+        {0, {{1, 2}, {2, 9}}},
+        {1, {{3, 6}, {4, 3}}},  
+        {2, {{4, 1}}},          
+        {3, {{5, 7}, {6, 2}}},  
+        {4, {{5, 1}, {6, 5}}},  
+        {5, {{7, 4}}},          
+        {6, {{7, 6}}},          
+        {7, {}}
+    };
+    cout << shortestPath(graph, 0, 7, 4) << " ";
+}
 //find longest lath in DAG.
 #include <bits/stdc++.h>
 using namespace std;
