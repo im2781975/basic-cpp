eef6bd31f0833d0b1074f0f6f96bc87b0653a400
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-10 22:13:49 +0600

    Update Sinknode.cpp

--- Group/Root/Sinknode.cpp
+++ Group/Root/Sinknode.cpp
@@ -1,3 +1,68 @@
+//find longest lath in DAG.
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5;
+vector<int> parent_list[N];
+int dp[N];
+
+int max_distance(int node) {
+    if (node == root) return 0;
+    if (dp[node] != -1) return dp[node];
+
+    int ans = 0;
+    for (int parent : parent_list[node]) {
+        int parent_dist = max_distance(parent);
+        ans = max(ans, parent_dist);
+    }
+    ans++;
+    dp[node] = ans;
+    return dp[node];
+}
+int main() {
+    int node, edge;
+    cin >> node >> edge;
+    for (int i = 0; i < edge; i++) {
+        int u, v;
+        cin >> u >> v;
+        parent_list[v].push_back(u);
+    }
+    for (int i = 1; i <= node; i++) {
+        dp[i] = -1;
+    }
+    int root, target_node; 
+    cin >> root >> target_node;
+    cout << max_distance(target_node);
+    return 0;
+}
+//find shortest distance in a multistage graph.
+#include<bits/stdc++.h>
+using namespace std;
+#define INF INT_MAX
+#define N 8
+int shortestDist(int graph[N][N]){
+    int dist[N]; dist[N - 1] = 0;
+    for(int i = N - 2; i >= 0; i--){
+        dist[i] = INF;
+        for(int j = i; j < N; j++){
+            if(graph[i][j] == INF)
+                continue;
+            dist[i] = min(dist[i], graph[i][j] + dist[j]);
+        }
+    }
+    return dist[0];
+}
+int main(){
+    int graph[N][N] =
+      {{INF, 1, 2, 5, INF, INF, INF, INF},
+       {INF, INF, INF, INF, 4, 11, INF, INF},
+       {INF, INF, INF, INF, 9, 5, 16, INF},
+       {INF, INF, INF, INF, INF, INF, 2, INF},
+       {INF, INF, INF, INF, INF, INF, INF, 18},
+       {INF, INF, INF, INF, INF, INF, INF, 13},
+       {INF, INF, INF, INF, INF, INF, INF, 2},
+      {INF, INF, INF, INF, INF, INF, INF, INF}};
+    cout << shortestDist(graph);
+}
 using namespace std;
 
 using ll = long long;
