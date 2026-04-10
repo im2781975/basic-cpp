1c4395c033a49efb02ec48342c121b7a567677fc
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-10 20:16:09 +0600

    Update Minpath.cpp

--- Group/Root/Minpath.cpp
+++ Group/Root/Minpath.cpp
@@ -184,3 +184,67 @@ void BellFord(int nd, int edg) {
     for(int i = 1; i < nd; i++) 
         cout << (dist[i] == x ? -1 : dist[i]) << " ";
 }
+#include<bits/stdc++.h>
+using namespace std;
+//Find negcycle
+void negCycle(int node, int edge) {
+    vector <tuple <int, int, int>> adj;
+    for(int i = 0; i < edge; i++) {
+        int u, v, w; cin >> u >> v >> w;
+        adj.emplace_back(u, v, w);
+    }
+    vector <int> dist(node + 1, INT_MAX);
+    int src = 1; dist[src] = 0;
+    for(int i = 1; i < node; i++) {
+        for(auto [u, v, w] : adj) {
+            if(dist[u] != INT_MAX && dist[u] + w < dist[v])
+                dist[v] = dist[u] + w;
+        }
+    }
+    bool negcycle = false;
+    for(int i = 0; i < adj.size(); i++) {
+        int u = adj[i].first;
+        int v = adj[i].second.first;
+        int w = adj[i].second.second;
+        if(dist[u] != INT_MAX && dist[u] + w < dist[v])
+            negcycle = true;
+    }
+    if(negcycle) cout << "detected";
+    for(int i = 1; i <= nd; i++) cout << i << " " << src << " " << dist[i] << endl;
+}
+// Find a simple cycle in an undirected graph
+#define x 1004
+vector <int> adj, vector <int> path;
+vector <bool> vis;
+bool hascycle(int src, int parent) {
+    vis[src] = true;
+    path.push_back(src);
+    for(int child : adj[src]) {
+        if(child == parent) continue;
+        if(!vis[child]) {
+            if(hascycle(child, src) return true;
+        }
+        else if(find(path.begin(), path.end(), child) != path.end()) {
+            auto it = find(path.begin(), path.end(), child);
+            cout << path.end() - it << " ";
+            for(auto i = it; i != path.end(); ++i)
+                cout << *i << " ";
+            return true;
+        }
+    }
+    path.pop_back(); return false;
+}
+void cntcycle(int node, int edge) {
+    for(int i = 0; i < edge; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    memset(vis, 0, sizeof(vis));
+    for(int i = 1; i <= node && !path.size(); i++) {
+        if(!vis[i]) {
+            path.clear(); if(hascycle(i, -1)) return 0;
+        }
+    }
+    cout << "Immpossible";
+}
