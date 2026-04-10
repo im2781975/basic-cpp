88ad6201a1685f8cfc512d1d564943353656c42e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-10 21:25:13 +0600

    Update Sinknode.cpp

--- Group/Root/Sinknode.cpp
+++ Group/Root/Sinknode.cpp
@@ -1,5 +1,82 @@
 #include<bits/stdc++.h>
 using namespace std;
+//find longest lath in DAG.
+int max_distance(int node) {
+    if (node == root) return 0;
+    if (dp[node] != -1) return dp[node];
+
+    int ans = 0;
+    for (int parent : parent_list[node]) {
+        int parent_dist = max_distance(parent);
+        ans = max(ans, parent_dist);
+    }
+    ans++;
+    dp[node] = ans;
+    return dp[node];
+}
+int main() {
+    int node, edge;
+    cin >> node >> edge;
+    for (int i = 0; i < edge; i++) {
+        int u, v;
+        cin >> u >> v;
+        parent_list[v].push_back(u);
+    }
+    for (int i = 1; i <= node; i++) {
+        dp[i] = -1;
+    }
+    int root, target_node; 
+    cin >> root >> target_node;
+    cout << max_distance(target_node);
+    return 0;
+}
+//You play a game consisting of n rooms and m tunnels. Your initial score is 0, and each tunnel increases your score by x where x may be both positive or negative. You may go through a tunnel several times.
+//Your task is to walk from room 1 to room n. What is the maximum score you can get?
+const int MAX_N = 105;
+const int INF = 1e9;
+int n, m;
+vector<pair<int, int>> tunnels[MAX_N];
+int dp[MAX_N][MAX_N];
+
+int maxScore(int curRoom, int remainingRooms) {
+    if (remainingRooms == 0) {
+        if (curRoom == n) {
+            return 0;
+        } else {
+            return -INF; // It's not possible to reach room n in the given number of moves.
+        }
+    }
+    if (dp[curRoom][remainingRooms] != -1) {
+        return dp[curRoom][remainingRooms];
+    }
+    int maxScore = -INF;
+    for (auto tunnel : tunnels[curRoom]) {
+        int nextRoom = tunnel.first;
+        int score = tunnel.second;
+
+        int result = maxScore(nextRoom, remainingRooms - 1);
+        if (result != -INF) {
+            maxScore = max(maxScore, result + score);
+        }
+    }
+    return dp[curRoom][remainingRooms] = maxScore;
+}
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int u, v, x;
+        cin >> u >> v >> x;
+        tunnels[u].push_back({v, x});
+    }
+    memset(dp, -1, sizeof(dp));
+    int result = maxScore(1, n);
+    if (result == -INF) {
+        cout << -1 << endl;
+    } else {
+        cout << result << endl;
+    }
+    return 0;
+}
 // sink node is a node with no outgoing edges.
 int cntsinknode(int nd, int edg, int *src, int *dst) {
     int vis[nd] = {0}, cnt = 0;
