b213f7c5786f42ae8af420c7db620cf89ba6fd45
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-04-11 21:04:44 +0600

    Update #Header.cpp

--- #Header.cpp
+++ #Header.cpp
@@ -1,7 +1,52 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
-
+#include<bits/stdc++.h>
+#define abdo ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define tc ll t ; cin>>t; while(t--)
+#define pb push_back
+#define pob pop_back
+#define loopj(j,a,b) for(ll j = a ; j < b-1 ; j++)
+#define loopi(i,a,b) for (ll i = a; i < b; i++)
+#define vl vector<ll>
+#define vi vector<int>
+#define lb lower_bound
+#define ub upper_bound
+#define sumv(v) accumalate(v.begin(),v.end(),0)
+#define mii map<int,int>
+#define umii unordered_map<int,int>
+#define order(v) sort(v.begin(),v.end())
+#define ed '\n'
+#define el "\n"
+#define all(v) v.begin(),v.end()
+#define YES cout<<"YES"<<ed;
+#define NO cout<<"NO"<<ed;
+#define Yes cout<<"Yes"<<ed;
+#define No cout<<"No"<<ed;
+#define yEs cout<<"yEs"<<ed;
+#define yeS cout<<"yeS"<<ed;
+#define nO cout<<"nO"<<ed;
+#define yes cout<<"yes"<<ed;
+#define no cout<<"no"<<ed;
+#define ans1 ll ans = 0;
+#define cnt1 ll cnt = 0;
+#define sum(a , b) (a + b)
+#define product(a , b) (a * b)
+#define squarear(a) (a * a)
+#define rectanglear(a , b) (a * b)
+#define squarepre(a) (a * 4)
+#define rectanglpr(a , b) ((a + b)*2)
+typedef unsigned long long ull;
+typedef long double lld;
+using namespace std;
+const int N = 1e7;
+ll frq[N];
+vector<bool> seive(1e6 + 1, true);
+const int maxn = 1e5;
+bool isprime[maxn];
+const int mod = 998244353;
+ll fact[N];
 #include <algorithm>
 #include <bitset>
 #include <complex>
